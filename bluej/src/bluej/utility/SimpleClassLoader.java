ccb9c0b71d4684add77a80b3e0c43632b6238b8b
mik <mik@bluej.org>
1999-05-11 05:01:43 +0000

    modified for jdk 1.2
    (from subversion r72)

--- bluej/src/bluej/utility/SimpleClassLoader.java
+++ bluej/src/bluej/utility/SimpleClassLoader.java
@@ -7,93 +7,106 @@ import java.io.InputStream;
 import java.io.IOException;
 
 /**
- ** @version $Id: SimpleClassLoader.java 36 1999-04-27 04:04:54Z mik $
- ** @author Michael Cahill
  ** A simple ClassLoader for BlueJ
+ **
+ ** @author Michael Cahill
+ ** @author Michael Kolling
+ **
+ ** @version $Id: SimpleClassLoader.java 72 1999-05-11 05:01:43Z mik $
  **/
 public class SimpleClassLoader extends ClassLoader
 {
-	private ClasspathSearcher searcher;
+    private ClasspathSearcher searcher;
 	
-	public SimpleClassLoader(ClasspathSearcher searcher)
-	{
-		this.searcher = searcher;
-	}
+    public SimpleClassLoader(ClasspathSearcher searcher)
+    {
+	this.searcher = searcher;
+    }
 	
-	public SimpleClassLoader(String path)
-	{
-		this(new ClasspathSearcher(path));
-	}
+    public SimpleClassLoader(String path)
+    {
+	this(new ClasspathSearcher(path));
+    }
 	
-	public SimpleClassLoader()
-	{
-		this(new ClasspathSearcher());
-	}
+    public SimpleClassLoader()
+    {
+	this(new ClasspathSearcher());
+    }
 	
-	public void addSearchPath(String path)
-	{
-		searcher.addClasspath(path);
-	}
+    public void addSearchPath(String path)
+    {
+	searcher.addClasspath(path);
+    }
 	
-	public void removeSearchPath(String path)
-	{
-		searcher.removeClasspath(path);
-	}
+    public void removeSearchPath(String path)
+    {
+	searcher.removeClasspath(path);
+    }
 	
-	protected Class loadClass(String name, boolean resolve)
-		throws ClassNotFoundException
-	{
-		Class c = loadClass(name);
+    protected Class loadClass(String name, boolean resolve)
+	throws ClassNotFoundException
+    {
+	Class c = loadClass(name);
 		
-		if(resolve)
-			resolveClass(c);
+	if(resolve)
+	    resolveClass(c);
 
-		return c;
-	}
+	return c;
+    }
 	
-	public synchronized Class loadClass(String name)
-		throws ClassNotFoundException
- 	{
-		Class preloaded = findLoadedClass(name);
-		if(preloaded != null)
-			return preloaded;
+    public synchronized Class loadClass(String name)
+	throws ClassNotFoundException
+    {
+	Class preloaded = findLoadedClass(name);
+	if(preloaded != null)
+	    return preloaded;
 
-		byte[] bytes = findClass(name);
-		if(bytes != null)
-			return defineClass(name, bytes, 0, bytes.length);
-		else
-		{
-			Class ret = findSystemClass(name);
-			// Debug.message("Returning system class " + ret + " for class " + name);
-			return ret;
-		}
+	Class cl = findClass(name);
+
+	if(cl != null)
+	    return cl;
+	else
+	    return findSystemClass(name);
+    }
+
+    /**
+     * Read in a class file from disk. Return a class object.
+     */
+    protected Class findClass(String name)
+    {
+	byte[] bytes = loadClassData(name);
+	if(bytes != null)
+	    return defineClass(name, bytes, 0, bytes.length);
+	else
+	    return null;
+    }
+
+    /**
+     * Read in a class file from disk. Return the class code as a byte
+     * array.
+     */
+    protected byte[] loadClassData(String name)
+    {
+	byte[] classData = null;
+
+	try {
+	    String filename = name.replace('.', Config.slash) + ".class";
+	    InputStream in = searcher.getFile(filename);
+	    if(in != null) {
+		byte[] buffer = new byte[in.available()];
+		in.read(buffer);
+		classData = buffer;
+		in.close();
+	    }
+	} catch(Exception e) {
+	    Debug.reportError("cannot load class " + name + ": " + e);
+	    e.printStackTrace();
 	}
-
-	protected byte[] findClass(String name)
-		throws ClassNotFoundException
-	{
-		byte[] classData = null;
-
-		try {
-			String filename = name.replace('.', Config.slash) + ".class";
-			InputStream in = searcher.getFile(filename);
-			if(in != null)
-			{
-				byte[] buffer = new byte[in.available()];
-				in.read(buffer);
-				classData = buffer;
-				in.close();
-			}
-		} catch(Exception e) {
-			// ignore it
-			e.printStackTrace();
-		}
-			
-		return classData;
-	}
+	return classData;
+    }
 	
-	public boolean hasClass(String classname)
-	{
-		return (findLoadedClass(classname) != null);
-	}
+    public boolean hasClass(String classname)
+    {
+	return (findLoadedClass(classname) != null);
+    }
 }
