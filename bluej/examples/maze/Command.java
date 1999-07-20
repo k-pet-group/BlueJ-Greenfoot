a9a0a7a543fdbbcfa623498cf038c50fb86c701d
ajp <ajp@bluej.org>
1999-07-20 06:11:21 +0000

    Javadoc'ed the comments
    (from subversion r195)

--- bluej/examples/maze/Command.java
+++ bluej/examples/maze/Command.java
@@ -1,25 +1,29 @@
-/*
-Author:  Morten Knudsen & Kent Hansen
-         Ported from the Blue program "Maze" by Michael Kolling.
-Version: 1.0
-Date:    July 1998
-Short:   Command words for "Maze", a simple text based game.
-
- This class is part of Maze. Maze is a simple, text based adventure game.
-
- This enumeration lists all valid commands in the game.
-
-*/
-
 import java.util.Enumeration;
 import java.util.NoSuchElementException;
+
+/**
+ * Command words for "Maze", a simple text based game.
+ *
+ * Author:  Morten Knudsen & Kent Hansen
+ *          Ported from the Blue program "Maze" by Michael Kolling.
+ * Version: 1.0
+ * Date:    July 1998
+ *
+ * This class is part of Maze. Maze is a simple, text based adventure game.
+ *
+ * This enumeration lists all valid commands in the game.
+ */
 
 class Command implements Enumeration {
     private int index;
     private final static String validCommands[] = {"north","south","west"
         ,"east","back","quit","take","drop","solve","help"};
 
-    //    Compare this Command to either a String or another Command.
+    /**
+     * Compare this Command to either a String or another Command.
+     *
+     * @param anObject the Object to be compared with
+     */
     public boolean equals (Object anObject) {
         // Is it a String?
         if (anObject instanceof String)
@@ -32,7 +36,9 @@ class Command implements Enumeration {
             return (anObject.equals (this));
     }
 
-    //    Print all valid commands to System.out.
+    /**
+     * Print all valid commands to System.out.
+     */
     public void showAll () {
         int i = 0;
         while (i<validCommands.length-1)
@@ -41,19 +47,25 @@ class Command implements Enumeration {
             System.out.println (validCommands[i]);
     }
 
-    //    Needed for Enumerator. Does this list contain more elements?
+    /**
+     * Needed for Enumerator. Does this list contain more elements?
+     */
     public boolean hasMoreElements() {
         return (index < validCommands.length-1);
     }
 
-    //    Needed for Enumerator. Return the next element.
+    /**
+     * Needed for Enumerator. Return the next element.
+     */
     public Object nextElement() {
         if (!hasMoreElements ())
             throw (new NoSuchElementException ());
         return validCommands[++index];
     }
 
-    //    Reset the enumerator.
+    /**
+     * Reset the enumerator.
+     */
     public void gotoFirstElement () {
         index = -1;
     }
