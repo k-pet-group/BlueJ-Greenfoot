0454fe49c424c5bf19aeb64dacdc7615a2fb2503
mik <mik@bluej.org>
1999-08-20 06:44:34 +0000

    - fixed parameter list in popup menu to include names instead of types
    (from subversion r245)

--- bluej/test-examples/hello/hello.java
+++ bluej/test-examples/hello/hello.java
@@ -1,3 +1,4 @@
+
 /**
  ** hello.java
  **
@@ -8,27 +9,23 @@ public class hello
 {
     static final int cons = 42;
     static int stats = 0;
-    public Integer number = new Integer(17);
     private String name = "mik";
 
     /** Method that does the work **/
-    public void go()
+    public void go(int numm)
     {
         int nummer;
 
         nummer = 42;
         System.out.println("hello, world");
-        System.out.println("starting count...");
-	for(int i = 0; i < 50000000; i++) {
-	    nummer++;
-	}
-        System.out.println("answer = " + nummer);
     }
 	
-	/** main method for testing outside JavaBlue **/
-	public static void main(String[] args)
-	{
-		hello h = new hello();
-		h.go();
-	}
+    	/**
+     * main method for testing outside JavaBlue 
+     */
+    public static void main(String[] args)
+    {
+	       hello h = new hello();
+	       h.go(4);
+    }
 }
