002046d7703d00e1bcc769f4ca9e4d1e551399af
ajp <ajp@bluej.org>
1999-11-25 02:33:45 +0000

    Remove old classloader. Subclasses URLClassLoader to do work now.
    (from subversion r283)

--- bluej/src/bluej/runtime/RemoteClassMgr.java
+++ bluej/src/bluej/runtime/RemoteClassMgr.java
@@ -12,13 +12,15 @@ import java.net.*;
 import javax.swing.table.*;
 
 /**
- ** @version $Id: RemoteClassMgr.java 193 1999-07-20 05:46:07Z ajp $
- ** @author Andrew Patterson
- ** Class to maintain a global classpath environment.
- **/
+ * Class to maintain a global classpath environment.
+ *
+ * @author   Andrew Patterson
+ * @version  $Id: RemoteClassMgr.java 283 1999-11-25 02:33:45Z ajp $
+ */
 public class RemoteClassMgr
 {
 	/**
+         *
 	 */
 	private ClassPath otherLibraries = new ClassPath();
 
@@ -56,67 +58,6 @@ public class RemoteClassMgr
 
 //			this.classpath = classpath;
 		}
-
-		/**
-		 * Read in a class file from disk. Return a class object.
-		 */
-/*		protected Class findClass(String name) throws ClassNotFoundException
-		{
-			// Debug.message("remoteclasspathloader: finding " + name);
-
-			byte[] bytes = loadClassData(name);
-			if(bytes != null) {
-				// Debug.message("remoteclasspathloader: succeeded " + name);
-				return defineClass(name, bytes, 0, bytes.length);
-			}
-			else {
-				// Debug.message("remoteclasspathloader: failed " + name);
-				throw new ClassNotFoundException("RemoteClassPathLoader");
-			}
-		}
-*/
-		/**
-		 * Read in a class file from disk. Return the class code as a byte
-		 * array. The JDK class loader delegation model means that we are
-		 * only ever asked to look up a class if the parent system loader
-		 * has failed. Therefore we need only look in our userLibraries and
-		 * systemLibraries. The bootLibraries will have been searched by
-		 * the system loader.
-		 */
-/*		protected byte[] loadClassData(String name)
-		{
-			ByteArrayOutputStream classdata = new ByteArrayOutputStream();
-
-			try {
-				String filename = name.replace('.', Config.slash) + ".class";
-
-				InputStream in = null;
-
-				if(classpath != null)
-					in = classpath.getFile(filename);
-
-				if(in == null)
-					in = otherLibraries.getFile(filename);
-
-				if(in != null) {
-					BufferedInputStream bufin = new BufferedInputStream(in);
-					int b;
-					while ((b = bufin.read()) != -1) {
-						classdata.write(b);
-					}
-					// Debug.message("remoteclasspathloader: " + classdata.size() + " bytes");
-				}
-
-			} catch(Exception e) {
-				Debug.reportError("cannot load class " + name + ": " + e);
-				e.printStackTrace();
-				return null;
-			}
-			if (classdata.size() == 0)
-				return null;
-			else
-				return classdata.toByteArray();
-		} */
 	} 
 }
 
