96c6a42c4f35ec3023f03150ff33c8bdfcd3afcb
ajp <ajp@bluej.org>
2000-01-02 13:02:13 +0000

    Comment made javadoc compliant
    (from subversion r318)

--- bluej/src/bluej/utility/Comparer.java
+++ bluej/src/bluej/utility/Comparer.java
@@ -1,17 +1,18 @@
 package bluej.utility;
 
 /**
- ** @version $Id: Comparer.java 49 1999-04-28 03:01:02Z ajp $
- ** @author Michael Cahill
- ** A interface for compares two objects
- ** @see Utility#sort()
- **/
+ * A interface for compares two objects
+ *
+ * @author  Michael Cahill
+ * @version $Id: Comparer.java 318 2000-01-02 13:02:13Z ajp $
+ * @see     Utility#sort()
+ */
 public interface Comparer
 {
 	/**
-	 ** @return -1 if a < b
-	 ** @return 0 if a == b
-	 ** @return 1 if a > b
-	 **/
+	 * @return -1 if a < b
+	 * @return 0 if a == b
+	 * @return 1 if a > b
+	 */
 	int cmp(Object a, Object b);
 }
