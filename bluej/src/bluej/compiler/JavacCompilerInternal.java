fb6ef7e9e2740f3d593090e638bb3ea6726ebcf1
ajp <ajp@bluej.org>
2001-07-05 03:21:33 +0000

    Attempted fix to allow the 1.4 beta compiler to work
    (from subversion r963)

--- bluej/src/bluej/compiler/JavacCompilerInternal.java
+++ bluej/src/bluej/compiler/JavacCompilerInternal.java
@@ -9,6 +9,32 @@ import com.sun.tools.javac.Main;    // newer version
 
 import bluej.utility.*;
 
+/*
+public final static String CLASSIC_CLASS = "sun.tools.javac.Main";
+  public final static String MODERN_CLASS = "com.sun.tools.javac.Main";
+
+  private boolean modern = false;
+
+  public Javac() {
+
+    // Use reflection to be able to build on all JDKs
+    try {
+        Class.forName(MODERN_CLASS);
+        modern = true;
+    } catch (ClassNotFoundException e) {
+        log.warn("Javac()", e);
+        try {
+            Class.forName(CLASSIC_CLASS);
+            modern = false;
+        } catch (ClassNotFoundException ex) {
+            log.warn("No compiler found in your classpath. Make sure you added 'tools.jar'", ex);
+            throw new RuntimeException("No compiler found in your classpath. Make sure you added
+'tools.jar'");
+        }
+    }
+  }
+*/
+
 /**
  * JavacCompilerInternal class - an implementation for the BlueJ "Compiler"
  * class. This implementation provides an interface to Sun's javac
@@ -16,10 +42,12 @@ import bluej.utility.*;
  *
  * @author  Michael Cahill
  * @author  Michael Kolling
- * @version $Id: JavacCompilerInternal.java 921 2001-06-04 07:26:13Z mik $
+ * @version $Id: JavacCompilerInternal.java 963 2001-07-05 03:21:33Z ajp $
  */
 public class JavacCompilerInternal extends Compiler
 {
+    private ErrorStream firstStream = null;
+
 	private String destdir;
 	private String classpath;
 	private boolean debug;
@@ -72,29 +100,51 @@ public class JavacCompilerInternal extends Compiler
 
 //        args.addElement("-verbose");
 
-		for(int i = 0; i < sources.length; i++)
-			args.addElement(sources[i]);
+        for(int i = 0; i < sources.length; i++)
+            args.addElement(sources[i]);
 
-		int length = args.size();
-		String[] params = new String[length];
-		args.copyInto(params);
+        int length = args.size();
+        String[] params = new String[length];
+        args.copyInto(params);
 
-                PrintStream systemErr = System.err;
+        PrintStream systemErr = System.err;
+        ErrorStream output = new ErrorStream();
 
-		ErrorStream output = new ErrorStream();
-		System.setErr(output);      // redirect errors to our stream
+        /* there is a problem with the jdk1.4 beta. It seems to use a
+           PrintWriter wrapped around the system error stream. It also
+           seems to cache the creation of this, so on subsequent compiles,
+           the original output stream is still being used. To cope with
+           both 1.3 and 1.4, we check the results of both the first stream
+           and the newly created stream */
 
-		//Main javac = new Main(output, "javac"); // old version
-		Main javac = new Main();
+        if (firstStream == null)
+            firstStream = output;
 
-		int result = javac.compile(params);
+        System.setErr(output);      // redirect errors to our stream
+
+        //Main javac = new Main(output, "javac"); // old version
+        int result = com.sun.tools.javac.Main.compile(params);
+
+        // only one of 'output' or 'firstStream' should be receiving output so
+        // we will only generate one error message
 
 		if (output.hasError()) {
 			watcher.errorMessage(output.getFilename(),
 						output.getLineNo(),
 						output.getMessage(),true);
 		}
-                System.setErr(systemErr);   // restore
+
+        if (firstStream != null && firstStream != output && firstStream.hasError()) {
+			watcher.errorMessage(firstStream.getFilename(),
+						firstStream.getLineNo(),
+						firstStream.getMessage(),true);
+		}
+
+        System.setErr(systemErr);   // restore
+
+        // in case we are reusing the first stream, reset the hasError boolean
+        if (firstStream != null)
+            firstStream.reset();
 
 		//return result;
 		return result==0;
@@ -119,6 +169,11 @@ class ErrorStream extends PrintStream
         // non-null OutputStream
         // we pass it the system error stream
         super(System.err);
+    }
+
+    public void reset()
+    {
+        haserror = false;
     }
 
     public boolean hasError()
@@ -146,7 +201,7 @@ class ErrorStream extends PrintStream
 
     /**
      * Note: this class "cheats" by assuming that all output will be written by
-     * a call to print or println. It happens that this is true for the 
+     * a call to print or println. It happens that this is true for the
      * current version of javac but this could change in the future.
      *
      * We assume a certain error message format here:
@@ -159,54 +214,62 @@ class ErrorStream extends PrintStream
      */
     public void print(String msg)
     {
-	if (haserror)
-	    return;
+        if (haserror)
+            return;
 
-	// Debug.message("Compiler message: " + msg);
+        // Debug.message("Compiler message: " + msg);
 
-	int first_colon = msg.indexOf(':', 0);
-	if(first_colon == -1) {
-	    // cannot read format of error message
-	    DialogManager.showErrorWithText(null, "compiler-error", msg);
-	    return;
-	}
+        int first_colon = msg.indexOf(':', 0);
+        if(first_colon == -1) {
+            // cannot read format of error message
+            DialogManager.showErrorWithText(null, "compiler-error", msg);
+            return;
+        }
 
-	filename = msg.substring(0, first_colon);
+        filename = msg.substring(0, first_colon);
 
-	// Windows might have a colon after drive name. If so, ignore it
-	if(! filename.endsWith(".java")) {
-	    first_colon = msg.indexOf(':', first_colon + 1);
-	    if(first_colon == -1) {
-				// cannot read format of error message
-		DialogManager.showErrorWithText(null, "compiler-error", msg);
-		return;
-	    }
-	    filename = msg.substring(0, first_colon);
-	}
-	int second_colon = msg.indexOf(':', first_colon + 1);
-	if(second_colon == -1) {
-	    // cannot read format of error message
-	    DialogManager.showErrorWithText(null, "compiler-error", msg);
-	    return;
-	}
+        // Windows might have a colon after drive name. If so, ignore it
+        if(! filename.endsWith(".java")) {
+            first_colon = msg.indexOf(':', first_colon + 1);
+            if(first_colon == -1) {
+                    // cannot read format of error message
+            DialogManager.showErrorWithText(null, "compiler-error", msg);
+            return;
+            }
+            filename = msg.substring(0, first_colon);
+        }
+        int second_colon = msg.indexOf(':', first_colon + 1);
+        if(second_colon == -1) {
+            // cannot read format of error message
+            DialogManager.showErrorWithText(null, "compiler-error", msg);
+            return;
+        }
 
-	lineno = 0;
-	try {
-	    lineno = Integer.parseInt(msg.substring(first_colon + 1, second_colon));
-	} catch(NumberFormatException e) {
-	    // ignore it
-	}
+        lineno = 0;
+        try {
+            lineno = Integer.parseInt(msg.substring(first_colon + 1, second_colon));
+        } catch(NumberFormatException e) {
+            // ignore it
+        }
 
-	message = msg.substring(second_colon + 1);
+        message = msg.substring(second_colon + 1);
 
-	haserror = true;
+        haserror = true;
     }
 
     /**
-     * Map println tp print - we are not interested in the line break anyway. 
+     * Map println to print - we are not interested in the line break anyway.
      */
     public void println(String msg)
     {
         print(msg);
+    }
+
+    /**
+     * JDK 1.4 seems to use write rather than print
+     */
+    public void write(byte[] buf, int off, int len)
+    {
+        print(new String(buf, off, len));
     }
 }
