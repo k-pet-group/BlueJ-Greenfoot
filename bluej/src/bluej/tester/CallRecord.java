dc398b55e7b6eb8e992a8628816eae75212684a3
ajp <ajp@bluej.org>
2001-10-29 05:35:57 +0000

    Should compile now.
    (from subversion r1001)

--- bluej/src/bluej/tester/CallRecord.java
+++ bluej/src/bluej/tester/CallRecord.java
@@ -1,19 +1,22 @@
 package bluej.tester;
 
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
 import bluej.utility.Utility;
 import bluej.utility.Debug;
-import java.util.Enumeration;
+import bluej.views.*;
+import bluej.debugger.*;
+import bluej.runtime.*;
 
 /**
  * Records a single user interaction with the object construction/
  * method call mechanisms of BlueJ.
  *
  * @author  Andrew Patterson
- * @version $Id: CallRecord.java 1000 2001-10-29 03:06:09Z ajp $
+ * @version $Id: CallRecord.java 1001 2001-10-29 05:35:57Z ajp $
  */
 public class CallRecord
 {
@@ -31,7 +34,7 @@ public class CallRecord
     public static CallRecord getCallRecord(String instanceName, CallableView theCall,
                                             String[] args, Component[] objectBench)
     {
-        CallRecord newRecord = new CallRecord(theCall, args);
+        CallRecord newRecord = new CallRecord(instanceName, theCall, args, objectBench);
 
         callRecords.put(instanceName, newRecord);
 
@@ -42,13 +45,34 @@ public class CallRecord
     // ======= instance section =======
 
     private CallableView theCall;
-    private List args;
+    private List constructorArgs = new ArrayList();
 
     private CallRecord(String instanceName, CallableView theCall,
                          String[] args, Component[] objectBench)
     {
+	if (args != null) {
+	for(int a = 0; a < args.length; a++) {
+		CallRecord cr;
 
-
-
+		if((cr = CallRecord.getCallRecord(args[a])) != null) {
+			this.args.add(cr);
+		} else {
+			this.args.add(args[a]);
+		}
+	}
+	}
     }
+	
+	public String toString() {
+		String res = "CR ";
+
+		Iterator it = args.iterator();
+
+		while(it.hasNext()) {
+			res += it.next().toString();
+		}
+		
+		return res;		
+	}
+
 }
