ce17701825b4587d32815b794a779ea361c08978
ajp <ajp@bluej.org>
2002-10-18 05:28:55 +0000

    Fix for bug #421 and #439. Message labels for thread status have been internationalized and exceptions in packages now work
    (from subversion r1414)

--- bluej/src/bluej/debugger/jdi/JdiThread.java
+++ bluej/src/bluej/debugger/jdi/JdiThread.java
@@ -7,6 +7,7 @@ import bluej.debugger.DebuggerClass;
 import bluej.debugger.SourceLocation;
 import bluej.utility.Debug;
 import bluej.utility.JavaNames;
+import bluej.Config;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -20,20 +21,31 @@ import com.sun.jdi.request.*;
  * This class represents a thread running on the remote virtual machine.
  *
  * @author  Michael Kolling
- * @version $Id: JdiThread.java 1187 2002-03-15 15:10:23Z mik $
+ * @version $Id: JdiThread.java 1414 2002-10-18 05:28:55Z ajp $
  */
 public final class JdiThread extends DebuggerThread
 {
+    static final String statusFinished = Config.getString("debugger.threadstatus.finished");
+    static final String statusBreakpoint = Config.getString("debugger.threadstatus.breakpoint");
+    static final String statusStopped = Config.getString("debugger.threadstatus.stopped");
+    static final String statusMonitor = Config.getString("debugger.threadstatus.monitor");
+    static final String statusNotStarted = Config.getString("debugger.threadstatus.notstarted");
+    static final String statusRunning = Config.getString("debugger.threadstatus.running");
+    static final String statusSleeping = Config.getString("debugger.threadstatus.sleeping");
+    static final String statusUnknown = Config.getString("debugger.threadstatus.unknown");
+    static final String statusWaiting = Config.getString("debugger.threadstatus.waiting");
+    static final String statusZombie = Config.getString("debugger.threadstatus.zombie");
+
     /** a list of classes to exclude from source display */
     private static List excludes;
-   
+
     static private List getExcludes() {
         if (excludes == null) {
             setExcludes("java.*, javax.*, sun.*, com.sun.*");
         }
         return excludes;
     }
-   
+
     static void setExcludes(String excludeString) {
         StringTokenizer t = new StringTokenizer(excludeString, " ,;");
         List list = new ArrayList();
@@ -42,7 +54,7 @@ public final class JdiThread extends DebuggerThread
         }
         excludes = list;
     }
-   
+
     static void addExcludesToRequest(StepRequest request) {
         Iterator iter = getExcludes().iterator();
         while (iter.hasNext()) {
@@ -50,149 +62,149 @@ public final class JdiThread extends DebuggerThread
             request.addClassExclusionFilter(pattern);
         }
     }
-   
-   
+
+
     ThreadReference rt; // the reference to the remote thread
     Object userParam;   // an optional user parameter associated with this
     // thread
     int selectedFrame;  // stores a stack frame that was selected for this
     // thread (selection is done for debugging)
-   
+
     EventRequestManager eventReqMgr;
-   
+
     static ObjectReference terminateException;  // The exception object to use
     //   to terminate a thread
-   
+
     public static void setTerminateException(ObjectReference terminateExc)
     {
         terminateException = terminateExc;
     }
-   
+
     // ---- instance: ----
-   
+
     public JdiThread(ThreadReference rt, Object userParam)
     {
         this.rt = rt;
         this.userParam = userParam;
-      
+
         selectedFrame = 0;      // unless specified otherwise, assume we want
                                 //  to see the top level frame
     }
-   
+
     public JdiThread(ThreadReference rt)
     {
         this(rt, null);
     }
-   
+
     public String getName()
     {
         String name = null;
-      
+
         try {
             name = rt.name();
-        } 
+        }
         catch(Exception e) {
             // ignore it
         }
         return name;
     }
-   
+
     public void setParam(Object param)
     {
         userParam = param;
     }
-   
+
     public Object getParam()
     {
         return userParam;
     }
-   
+
     public ThreadReference getRemoteThread()
     {
         return rt;
     }
-   
+
     public String getStatus()
     {
         try {
             if(rt.isAtBreakpoint()) {
                 if(rt.frame(0).location().declaringType().name().equals(
                                             "bluej.runtime.ExecServer"))
-                    return "finished";
+                    return statusFinished;
                 else
-                    return "at breakpoint";
+                    return statusBreakpoint;
             }
-         
+
             if(rt.isSuspended())
-                return "stopped";
-         
+                return statusStopped;
+
             int status = rt.status();
             switch(status) {
             case ThreadReference.THREAD_STATUS_MONITOR:
-                return "at monitor";
+                return statusMonitor;
             case ThreadReference.THREAD_STATUS_NOT_STARTED:
-                return "not started";
+                return statusNotStarted;
             case ThreadReference.THREAD_STATUS_RUNNING:
-                return "running";
+                return statusRunning;
             case ThreadReference.THREAD_STATUS_SLEEPING:
-                return "sleeping";
+                return statusSleeping;
             case ThreadReference.THREAD_STATUS_UNKNOWN:
-                return "unknown status";
+                return statusUnknown;
             case ThreadReference.THREAD_STATUS_WAIT:
-                return "waiting";
+                return statusWaiting;
             case ThreadReference.THREAD_STATUS_ZOMBIE:
-                return "zombie";
+                return statusZombie;
             }
-        } 
+        }
         catch(Exception e) {
             return "???";
         }
         return null; // to shut up compiler
     }
-   
+
     public boolean isSuspended()
     {
         return rt.isSuspended();
     }
-   
+
     public String getClass(int frameNo)
     {
         try {
             return rt.frame(frameNo).location().declaringType().name();
-        } 
+        }
         catch(Exception e) {
             return "<error finding type at frame " + frameNo +">";
         }
     }
-   
+
     public String getClassSourceName(int frameNo)
     {
         try {
             return rt.frame(frameNo).location().sourceName();
-        } 
+        }
         catch(Exception e) {
             return "<no source at frame no " + frameNo +">";
         }
     }
-   
+
     public int getLineNumber(int frameNo)
     {
         try {
             return rt.frame(frameNo).location().lineNumber();
-        } 
+        }
         catch(Exception e) {
             return 1;
         }
     }
-   
+
     // name of the threadgroup that contains user threads
     static final String MAIN_THREADGROUP = "main";
-   
+
     public boolean isKnownSystemThread()
     {
         if(! rt.threadGroup().name().equals(MAIN_THREADGROUP))
             return true;
-      
+
         String name = rt.name();
         if(name.startsWith("AWT-") ||
            name.equals("Timer Queue") ||
@@ -200,11 +212,11 @@ public final class JdiThread extends DebuggerThread
            name.startsWith("SunToolkit.") ||
            name.startsWith("Native Carbon"))
             return true;
-      
+
         return false;
     }
-   
-   
+
+
     /**
      * Get strings showing the current stack frames. Ignore everything
      * including the __SHELL class and below.
@@ -220,34 +232,38 @@ public final class JdiThread extends DebuggerThread
             if(rt.isSuspended()) {
                 List stack = new ArrayList();
                 List frames = rt.frames();
-            
+
                 for(int i = 0; i < frames.size(); i++) {
                     StackFrame f = (StackFrame)frames.get(i);
                     Location loc = f.location();
                     String classname = loc.declaringType().name();
-                    if(classname.startsWith("__SHELL"))
+
+                    //
+                    if(JavaNames.getBase(classname).startsWith("__SHELL"))
                         break;
 
                     String filename = loc.sourceName();
                     String methodname = loc.method().name();
                     int lineNumber = loc.lineNumber();
 
-                    stack.add(new SourceLocation(classname, filename, 
+                    stack.add(new SourceLocation(classname, filename,
                                                  methodname, lineNumber));
                 }
                 return stack;
             }
-        } 
+        }
         catch(IncompatibleThreadStateException e) {
+            e.printStackTrace();
             Debug.reportError("error while getting stack info");
         }
         catch(AbsentInformationException e) {
+            e.printStackTrace();
             Debug.reportError("error while getting stack info");
         }
         return new ArrayList();
     }
-   
-   
+
+
     /**
      * Return strings listing the local variables.
      *
@@ -262,24 +278,24 @@ public final class JdiThread extends DebuggerThread
                 StackFrame frame = rt.frame(frameNo);
                 List vars = frame.visibleVariables();
                 List localVars = new ArrayList();
-            
+
                 for(int i = 0; i < vars.size(); i++) {
                     LocalVariable var = (LocalVariable)vars.get(i);
                     String val = JdiObject.getValueString(
                                                           frame.getValue(var));
-                    localVars.add(JavaNames.stripPrefix(var.typeName()) + 
+                    localVars.add(JavaNames.stripPrefix(var.typeName()) +
                                   " " + var.name() + " = " + val);
-               
+
                 }
                 return localVars;
             }
-        } 
+        }
         catch(Exception e) {
             // nothing can be done...
         }
         return new ArrayList();
     }
-   
+
     /**
      * Return true if the identified slot on the stack contains an object.
      */
@@ -295,14 +311,14 @@ public final class JdiThread extends DebuggerThread
             }
             else
                 return false;
-        } 
+        }
         catch(Exception e) {
             // nothing can be done...
             Debug.reportError("could not get local variable info: " + e);
         }
         return false;
     }
-   
+
     /**
      * Return an object from this thread's stack. The variable must contain
      * an object.
@@ -319,14 +335,14 @@ public final class JdiThread extends DebuggerThread
             }
             else
                 return null;
-        } 
+        }
         catch(Exception e) {
             // nothing can be done...
             Debug.reportError("could not get local variable info: " + e);
         }
         return null;
     }
-   
+
     /**
      * Return the current object of this thread. May be null (if, for
      * example, the thread executed only static methods).
@@ -338,16 +354,16 @@ public final class JdiThread extends DebuggerThread
                 StackFrame frame = rt.frame(frameNo);
                 return JdiObject.getDebuggerObject(frame.thisObject());
             }
-        } 
+        }
         catch(Exception e) {
             // nothing to do...
         }
         return null;
     }
-   
-   
+
+
     /**
-     * Return the current class of this thread. 
+     * Return the current class of this thread.
      */
     public DebuggerClass getCurrentClass(int frameNo)
     {
@@ -356,34 +372,34 @@ public final class JdiThread extends DebuggerThread
                 StackFrame frame = rt.frame(frameNo);
                 return new JdiClass(frame.location().declaringType());
             }
-        } 
+        }
         catch(Exception e) {
             // nothing to do...
         }
         return null;
     }
-   
-   
+
+
     public void setSelectedFrame(int frame)
     {
         selectedFrame = frame;
     }
-   
+
     public int getSelectedFrame()
     {
         return selectedFrame;
     }
-   
+
     public void step()
     {
         doStep(StepRequest.STEP_OVER);
     }
-   
+
     public void stepInto()
     {
         doStep(StepRequest.STEP_INTO);
     }
-   
+
     private void doStep(int depth)
     {
         clearPreviousStep(rt);
@@ -391,13 +407,13 @@ public final class JdiThread extends DebuggerThread
                                              StepRequest.STEP_LINE, depth);
         //if(depth == StepRequest.STEP_INTO)
         addExcludesToRequest(request);
-      
+
         // Make sure the step event is done only once
         request.addCountFilter(1);
         request.enable();
         Debugger.debugger.cont();
     }
-   
+
     public void terminate()
     {
         //if(! isKnownSystemThread()) {
@@ -411,7 +427,7 @@ public final class JdiThread extends DebuggerThread
         }
         //}
     }
-   
+
     /**
      * A previously set step may not have completed yet - find out and
      * if it is so, remove it.
@@ -420,13 +436,13 @@ public final class JdiThread extends DebuggerThread
     {
         if(eventReqMgr == null)
             getEventRequestManager();
-      
+
         List requests = eventReqMgr.stepRequests();
         Iterator iter = requests.iterator();
-      
+
         while (iter.hasNext()) {
             StepRequest request = (StepRequest)iter.next();
-         
+
             if (request != null && request.thread() != null) {
                 if (request.thread().equals(thread)) {
                     eventReqMgr.deleteEventRequest(request);
@@ -435,10 +451,10 @@ public final class JdiThread extends DebuggerThread
             }
         }
     }
-   
+
     private void getEventRequestManager()
     {
         eventReqMgr = rt.virtualMachine().eventRequestManager();
     }
-   
+
 }
