4fd018052fdf744b223f4061488f87ae582f4651
damiano <damiano@bluej.org>
2002-10-28 11:12:14 +0000

    Changed the code layout + forced user to select the info ICON to have the details
    (from subversion r1483)

--- bluej/src/bluej/extmgr/HelpDetailDialog.java
+++ bluej/src/bluej/extmgr/HelpDetailDialog.java
@@ -37,205 +37,217 @@ import java.awt.*;
  * bound to the HelpDialog and may be useful in the future.
  */
 class HelpDetailDialog extends JDialog implements ActionListener
-  {
-  private final String extensionsTag = Config.getString ("extmgr.extensions");
-  private final String detailsTag = Config.getString ("extmgr.details");
-  private final String installedString = Config.getString ("extmgr.installed");
-  private final String projectString = Config.getString ("extmgr.project");
-  private final String statusTag = Config.getString ("extmgr.details.status");
-  private final String nameTag = Config.getString ("extmgr.details.name");
-  private final String locationTag = Config.getString ("extmgr.details.location");
-  private final String typeTag = Config.getString ("extmgr.details.type");
-  private final String versionTag = Config.getString ("extmgr.details.version");
-  private final String dateTag = Config.getString ("extmgr.details.date");
-  private final String urlTag = Config.getString ("extmgr.details.url");
-  private final String menusTag = Config.getString ("extmgr.details.menus");
-  private final String preferencesTag = Config.getString ("extmgr.details.preferences");
-  private final String descriptionTag = Config.getString ("extmgr.details.description");
+{
+    private final String extensionsTag = Config.getString ("extmgr.extensions");
+    private final String detailsTag = Config.getString ("extmgr.details");
+    private final String installedString = Config.getString ("extmgr.installed");
+    private final String projectString = Config.getString ("extmgr.project");
+    private final String statusTag = Config.getString ("extmgr.details.status");
+    private final String nameTag = Config.getString ("extmgr.details.name");
+    private final String locationTag = Config.getString ("extmgr.details.location");
+    private final String typeTag = Config.getString ("extmgr.details.type");
+    private final String versionTag = Config.getString ("extmgr.details.version");
+    private final String dateTag = Config.getString ("extmgr.details.date");
+    private final String urlTag = Config.getString ("extmgr.details.url");
+    private final String menusTag = Config.getString ("extmgr.details.menus");
+    private final String preferencesTag = Config.getString ("extmgr.details.preferences");
+    private final String descriptionTag = Config.getString ("extmgr.details.description");
 
   
-  private JLabel statusField, nameField, locationField, typeField, versionField, 
+    private JLabel statusField, nameField, locationField, typeField, versionField, 
                    dateField, urlField, menusField, preferencesField;
-  private JTextArea descriptionField;
-  private URL url;
-  private JButton closeButton;
+    private JTextArea descriptionField;
+    private URL url;
+    private JButton closeButton;
         
-  public HelpDetailDialog (Dialog owner )
+    /**
+     * Constructor
+     */
+    public HelpDetailDialog (Dialog owner )
     {
-    super (owner, "Details");
+        super (owner, "Details");
 
-    JPanel panel = new JPanel();
-    panel.setLayout (new GridBagLayout());
+        JPanel panel = new JPanel();
+        panel.setLayout (new GridBagLayout());
 
-    GridBagConstraints tag = new GridBagConstraints();
-    tag.gridx = 0;
-    tag.anchor = GridBagConstraints.NORTHEAST;
+        GridBagConstraints tag = new GridBagConstraints();
+        tag.gridx = 0;
+        tag.anchor = GridBagConstraints.NORTHEAST;
 
-    GridBagConstraints value = new GridBagConstraints();
-    value.gridx = 1;
-    value.anchor = GridBagConstraints.NORTHWEST;
-    value.gridwidth = 3;
+        GridBagConstraints value = new GridBagConstraints();
+        value.gridx = 1;
+        value.anchor = GridBagConstraints.NORTHWEST;
+        value.gridwidth = 3;
                     
-    panel.add (new JLabel (statusTag+":  "), tag);
-    panel.add (new JLabel (nameTag+":  "), tag);
-    panel.add (new JLabel (locationTag+":  "), tag);
-    panel.add (new JLabel (typeTag+":  "), tag);
-    panel.add (new JLabel (versionTag+":  "), tag);
-    panel.add (new JLabel (dateTag+":  "), tag);
-    panel.add (new JLabel (urlTag+":  "), tag);
-    panel.add (new JLabel (menusTag+":  "), tag);
-    panel.add (new JLabel (preferencesTag+":  "), tag);
-    panel.add (new JLabel (descriptionTag+":  "), tag);
+        panel.add (new JLabel (statusTag+":  "), tag);
+        panel.add (new JLabel (nameTag+":  "), tag);
+        panel.add (new JLabel (locationTag+":  "), tag);
+        panel.add (new JLabel (typeTag+":  "), tag);
+        panel.add (new JLabel (versionTag+":  "), tag);
+        panel.add (new JLabel (dateTag+":  "), tag);
+        panel.add (new JLabel (urlTag+":  "), tag);
+        panel.add (new JLabel (menusTag+":  "), tag);
+        panel.add (new JLabel (preferencesTag+":  "), tag);
+        panel.add (new JLabel (descriptionTag+":  "), tag);
 
-    statusField = new JLabel();     
-    nameField = new JLabel();
-    locationField = new JLabel();
-    typeField = new JLabel();
-    versionField = new JLabel();
-    dateField = new JLabel();
-    dateField.setToolTipText ("yyyy/mm/dd hh:mm:ss");
+        statusField = new JLabel();     
+        nameField = new JLabel();
+        locationField = new JLabel();
+        typeField = new JLabel();
+        versionField = new JLabel();
+        dateField = new JLabel();
+        dateField.setToolTipText ("yyyy/mm/dd hh:mm:ss");
 
-    urlField = new JLabel();
-    urlField.setCursor (new Cursor (Cursor.HAND_CURSOR));
-    urlField.addMouseListener (new MouseAdapter() {
-        public void mouseClicked (MouseEvent e) {
-            openURL();
-        }
-    });
+        urlField = new JLabel();
+        urlField.setCursor (new Cursor (Cursor.HAND_CURSOR));
+        urlField.addMouseListener (new MouseAdapter() {
+            public void mouseClicked (MouseEvent e) {
+                openURL();
+            }
+        });
 
-    menusField = new JLabel();
-    preferencesField = new JLabel();
+        menusField = new JLabel();
+        preferencesField = new JLabel();
 
-    descriptionField = new JTextArea (4,40);
-    descriptionField.setLineWrap (true);
-    descriptionField.setWrapStyleWord (true);
-    descriptionField.setEnabled (false);
-    descriptionField.setDisabledTextColor (Color.black);
-    descriptionField.setBackground(preferencesField.getBackground());
-    // I need to put into a scrollbar in case the text is too much    
-    JScrollPane descriptionScroller = new JScrollPane (descriptionField);
-    descriptionScroller.setBorder (null);
+        descriptionField = new JTextArea (4,40);
+        descriptionField.setLineWrap (true);
+        descriptionField.setWrapStyleWord (true);
+        descriptionField.setEnabled (false);
+        descriptionField.setDisabledTextColor (Color.black);
+        descriptionField.setBackground(preferencesField.getBackground());
+        // I need to put into a scrollbar in case the text is too much    
+        JScrollPane descriptionScroller = new JScrollPane (descriptionField);
+        descriptionScroller.setBorder (null);
 
-    panel.add (statusField, value);
-    panel.add (nameField, value);
-    panel.add (locationField, value);
-    panel.add (typeField, value);
-    panel.add (versionField, value);
-    panel.add (dateField, value);
-    panel.add (urlField, value);
-    panel.add (menusField, value);
-    panel.add (preferencesField, value);
-    panel.add (descriptionScroller, value);
+        panel.add (statusField, value);
+        panel.add (nameField, value);
+        panel.add (locationField, value);
+        panel.add (typeField, value);
+        panel.add (versionField, value);
+        panel.add (dateField, value);
+        panel.add (urlField, value);
+        panel.add (menusField, value);
+        panel.add (preferencesField, value);
+        panel.add (descriptionScroller, value);
 
-    // I need to put this beast into a scroll pane
-    JScrollPane detailScroll = new JScrollPane(panel);
+        // I need to put this beast into a scroll pane
+        JScrollPane detailScroll = new JScrollPane(panel);
 
 
-    // The close button goes into a panel, to make it nice...
-    JPanel buttonPanel = new JPanel();
-    closeButton = new JButton (Config.getString ("close"));
-    closeButton.addActionListener (this);
-    buttonPanel.add (closeButton);
+        // The close button goes into a panel, to make it nice...
+        JPanel buttonPanel = new JPanel();
+        closeButton = new JButton (Config.getString ("close"));
+        closeButton.addActionListener (this);
+        buttonPanel.add (closeButton);
 
-    // TIme to put the two main panels into the root pane
-    JPanel rootPane = (JPanel)getContentPane();
-    rootPane.setLayout (new BorderLayout());
-    rootPane.setBorder (Config.dialogBorder);
+        // TIme to put the two main panels into the root pane
+        JPanel rootPane = (JPanel)getContentPane();
+        rootPane.setLayout (new BorderLayout());
+        rootPane.setBorder (Config.dialogBorder);
 
-    rootPane.add (detailScroll, BorderLayout.CENTER);
-    rootPane.add (buttonPanel, BorderLayout.SOUTH);
+        rootPane.add (detailScroll, BorderLayout.CENTER);
+        rootPane.add (buttonPanel, BorderLayout.SOUTH);
 
-    // save position when window is moved
-    addComponentListener(new ComponentAdapter() {
-        public void componentMoved(ComponentEvent event)
-        {
-            Config.putLocation("bluej.extmgr.helpdialog.details", getLocation());
-        }
-    });
+        // save position when window is moved
+        addComponentListener(new ComponentAdapter() {
+            public void componentMoved(ComponentEvent event)
+            {
+                Config.putLocation("bluej.extmgr.helpdialog.details", getLocation());
+            }
+        });
     
-    setLocation(Config.getLocation("bluej.extmgr.helpdialog.details"));
-
-    pack();
+        setLocation(Config.getLocation("bluej.extmgr.helpdialog.details"));
+        pack();
     }
 
         
-  public void actionPerformed (ActionEvent evt)
+    /**
+     * Called when the button is pressed
+     */
+    public void actionPerformed (ActionEvent evt)
     {
-    Object src = evt.getSource();
-    if ( src == null ) return;
-    if (src == closeButton) setVisible(false);
+        Object src = evt.getSource();
+        if ( src == null ) 
+            return;
+        if (src == closeButton) 
+            setVisible(false);
     }
 
-  /**
-   * Utility, to make code clean. Concatenate a series of strings separated by,
-   * We are just tryng to be quick, not that it really matters but still better nd clearer
-   */
-  private String commaList (String[] list)
+    /**
+     * Utility, to make code clean. Concatenate a series of strings separated by,
+     * We are just tryng to be quick, not that it really matters but still better nd clearer
+     */
+    private String commaList (String[] list)
     {
-    if (list == null) return "";
-    if (list.length < 1 ) return "";
+        if (list == null) 
+            return "";
+        if (list.length < 1 ) 
+            return "";
     
-    StringBuffer commaList = new StringBuffer(200);
+        StringBuffer commaList = new StringBuffer(200);
 
-    int lastIndex = list.length-1;     // We know that there is at least one element
-    for (int index=0; ; index++)
-      {
-      commaList.append (list[index]);
-      if ( index >= lastIndex ) break;
-      // There is another element in the list, add a comma
-      commaList.append(",");
-      }
+        int lastIndex = list.length-1;     // We know that there is at least one element
+        for (int index=0; ; index++)
+          {
+          commaList.append (list[index]);
+          if ( index >= lastIndex ) break;
+          // There is another element in the list, add a comma
+          commaList.append(",");
+          }
 
-    return commaList.toString();
+        return commaList.toString();
     }
 
 
-  /**
-   * Called with the index on what extension you want to update
-   * It would be better if I gave the extension.... Next one ?
-   */
-  public void updateInfo(int extensionIndex)
+    /**
+    * Called with the index on what extension you want to update
+    * It would be better if I gave the extension.... Next one ?
+    */
+    public void updateInfo(int extensionIndex)
     {
-    if (extensionIndex < 0 ) return;
+        if (extensionIndex < 0 )
+            return;
 
-    List exts = ExtensionsManager.getExtMgr().getExtensions();
-    ExtensionWrapper wrapper = (ExtensionWrapper) exts.get (extensionIndex);
+        List exts = ExtensionsManager.getExtMgr().getExtensions();
+        ExtensionWrapper wrapper = (ExtensionWrapper) exts.get (extensionIndex);
 
-    // You never know when you get strange errors...
-    if ( wrapper == null ) return;
+        // You never know when you get strange errors...
+        if ( wrapper == null ) 
+            return;
     
-    statusField.setText (wrapper.getStatus());
-    statusField.setForeground (wrapper.isValid()?Color.black:Color.red);
+        statusField.setText (wrapper.getStatus());
+        statusField.setForeground (wrapper.isValid()?Color.black:Color.red);
 
-    nameField.setText (wrapper.getName());
-    locationField.setText (wrapper.getLocation());
-    typeField.setText ((wrapper.getProject()!=null)?projectString:installedString);
-    versionField.setText (wrapper.getVersion());
-    dateField.setText (wrapper.getDate());
+        nameField.setText (wrapper.getName());
+        locationField.setText (wrapper.getLocation());
+        typeField.setText ((wrapper.getProject()!=null)?projectString:installedString);
+        versionField.setText (wrapper.getVersion());
+        dateField.setText (wrapper.getDate());
             
-    url = wrapper.getURL();
-    if (url == null) {
-        urlField.setText (null);
-    } else {
-        urlField.setText (url.toExternalForm());
-        urlField.setForeground (Color.blue);
-    }
+        url = wrapper.getURL();
+        if (url == null) {
+            urlField.setText (null);
+        } else {
+            urlField.setText (url.toExternalForm());
+            urlField.setForeground (Color.blue);
+        }
             
-    descriptionField.setText (wrapper.getDescription());
-    descriptionField.setCaretPosition (0);
+        descriptionField.setText (wrapper.getDescription());
+        descriptionField.setCaretPosition (0);
             
-    menusField.setText (commaList (wrapper.getMenuNames()));
-    preferencesField.setText (commaList (ExtPrefPanel.INSTANCE.getPreferenceNames (wrapper)));
+        menusField.setText (commaList (wrapper.getMenuNames()));
+        preferencesField.setText (commaList (ExtPrefPanel.INSTANCE.getPreferenceNames (wrapper)));
             
-    validate();
-    pack();
+        validate();
+        pack();
     }
         
-  private void openURL() 
+    private void openURL() 
     {
-    if (url == null) return;
-    Utility.openWebBrowser (url.toExternalForm());
+        if (url == null) 
+            return;
+        Utility.openWebBrowser (url.toExternalForm());
     }
-  }
+}
 
 
--- bluej/src/bluej/extmgr/HelpDialog.java
+++ bluej/src/bluej/extmgr/HelpDialog.java
@@ -36,171 +36,208 @@ import java.awt.*;
  * The Extensions Manager help panel allows the user to view current extensions.
  *
  * @author  Clive Miller, Damiano Bolla
- * @version $Id: HelpDialog.java 1471 2002-10-25 08:31:34Z damiano $
+ * @version $Id: HelpDialog.java 1483 2002-10-28 11:12:14Z damiano $
  */
 public class HelpDialog extends JDialog implements ActionListener
-  {
-  private final String installedString = Config.getString ("extmgr.installed");
-  private final String projectString = Config.getString ("extmgr.project");
-  private final ImageIcon infoIcon = Config.getImageAsIcon("image.extmgr.info");
+{
+    private final String installedString = Config.getString ("extmgr.installed");
+    private final String projectString = Config.getString ("extmgr.project");
+    private final ImageIcon infoIcon = Config.getImageAsIcon("image.extmgr.info");
   
-  private HelpDetailDialog detailDialog;
-  private JButton closeButton;
-  private ExtensionsTableModel extensionsTableModel;
-  private JTable extensionsTable;
+    private HelpDetailDialog detailDialog;
+    private JButton closeButton;
+    private ExtensionsTableModel extensionsTableModel;
+    private JTable extensionsTable;
 
-  /**
-   * Setup the UI for the dialog and event handlers for the dialog's buttons.
-   */
-  public HelpDialog (PkgMgrFrame parent)
+    /**
+    * Setup the UI for the dialog and event handlers for the dialog's buttons.
+    */
+    public HelpDialog (PkgMgrFrame parent)
     {
-    super (parent, Config.getString ("extmgr.title"), true);
+        super (parent, Config.getString ("extmgr.title"), true);
 
-    extensionsTable = getExtensionTable();
-    JScrollPane extensionsPane = new JScrollPane(extensionsTable);
+        extensionsTable = getExtensionTable();
+        JScrollPane extensionsPane = new JScrollPane(extensionsTable);
 
-    JPanel buttonPanel = new JPanel();
-    closeButton = new JButton (Config.getString ("close"));
-    closeButton.addActionListener (this);
-    buttonPanel.add (closeButton);
+        JPanel buttonPanel = new JPanel();
+        closeButton = new JButton (Config.getString ("close"));
+        closeButton.addActionListener (this);
+        buttonPanel.add (closeButton);
 
-    JPanel rootPane = (JPanel)getContentPane();
-    rootPane.setLayout (new BorderLayout());
-    rootPane.setBorder (Config.dialogBorder);
+        JPanel rootPane = (JPanel)getContentPane();
+        rootPane.setLayout (new BorderLayout());
+        rootPane.setBorder (Config.dialogBorder);
 
-    rootPane.add (extensionsPane, BorderLayout.CENTER);
-    rootPane.add (buttonPanel, BorderLayout.SOUTH);
-    DialogManager.centreDialog(this);
+        rootPane.add (extensionsPane, BorderLayout.CENTER);
+        rootPane.add (buttonPanel, BorderLayout.SOUTH);
+        DialogManager.centreDialog(this);
 
-    // save position when window is moved
-    addComponentListener(new ComponentAdapter() 
-      {
-      public void componentMoved(ComponentEvent event)
+        // save position when window is moved
+        addComponentListener(new ComponentAdapter() 
         {
-        Config.putLocation("bluej.extmgr.helpdialog", getLocation());
+            public void componentMoved(ComponentEvent event) 
+            {
+                Config.putLocation("bluej.extmgr.helpdialog", getLocation());
+            }
+        });
+
+        setLocation(Config.getLocation("bluej.extmgr.helpdialog"));
+        pack();
+        setVisible(true);
+    }
+
+    /**
+     * Just to manage the close button
+     */
+    public void actionPerformed (ActionEvent evt)
+    {
+        Object src = evt.getSource();
+        if ( src == null ) 
+            return;
+
+        if (src == closeButton) 
+            hide();
+    }
+
+    /**
+     * Utility, to make code cleaner
+     */
+    private void showDetails()
+    {
+        if (detailDialog == null) 
+            detailDialog = new HelpDetailDialog (this);
+
+        int selectedColumn = extensionsTable.getSelectedColumn();
+
+        // We want the user to click on the ICON !!!
+        if ( selectedColumn != 0 ) 
+            return;
+
+        detailDialog.updateInfo(extensionsTable.getSelectedRow());
+        detailDialog.setVisible(true);
+    }
+    
+
+    /**
+     * Utility, to make the code clean.
+     * Returns the table that describes the installed extensions
+     * I really would like not to set so many preferred values...
+     */
+    private JTable getExtensionTable ()
+    {
+        extensionsTableModel = new ExtensionsTableModel();
+
+        JTable aTable = new JTable (extensionsTableModel);
+        aTable.setSelectionMode (ListSelectionModel.SINGLE_SELECTION);
+        aTable.setPreferredScrollableViewportSize (new Dimension(400, 100));
+
+        TableColumnModel tcm = aTable.getColumnModel();
+        tcm.getColumn(0).setPreferredWidth(20);
+        tcm.getColumn(2).setPreferredWidth(250);
+
+        aTable.setRowHeight(18);
+        aTable.setRowSelectionAllowed(false);
+
+        aTable.addMouseListener (new myMouseAdapter() );
+        return aTable;
+    }
+
+    /**
+     * When a mouse is clicked I come here
+     * Maybe I can do it with the selected, it may be easier... next release ?
+     */
+    private class myMouseAdapter extends MouseAdapter
+    {
+    public void mouseClicked(MouseEvent e) 
+        {
+            if (e.getClickCount() == 1) 
+                showDetails();
         }
-      });
-
-    setLocation(Config.getLocation("bluej.extmgr.helpdialog"));
-    pack();
-    setVisible(true);
     }
 
-  /**
-   * Just to manage the close button
-   */
-  public void actionPerformed (ActionEvent evt)
+    /**
+     * This models the data of the table. Basically the JTable ask this class
+     * about the data to be displayed on the screen
+     */
+    private class ExtensionsTableModel extends AbstractTableModel
     {
-    Object src = evt.getSource();
-    if ( src == null ) return;
-    if (src == closeButton) hide();
-    }
+        // It does not matter very much if it is not static, it is created once only
+        private String columnNames[] = {
+        "",
+        Config.getString("extmgr.statuscolumn"),
+        Config.getString("extmgr.namecolumn"),
+        Config.getString("extmgr.typecolumn"),
+        Config.getString("extmgr.versioncolumn")};
 
-  /**
-   * Utility, to make code cleaner
-   */
-  private void showDetails()
-    {
-    if (detailDialog == null) detailDialog = new HelpDetailDialog (this);
+        public int getRowCount()     
+        {  
+            return getExtensions().size();    
+        }
 
-    detailDialog.updateInfo(extensionsTable.getSelectedRow());
-    detailDialog.setVisible(true);
-    }
-    
+        public int getColumnCount()  
+        { 
+            return columnNames.length;         
+        }
+            
+        public String getColumnName(int col) 
+        {  
+            return columnNames [col]; 
+        }
 
-  /**
-   * Utility, to make the code clean.
-   * Returns the table that describes the installed extensions
-   * I really would like not to set so many preferred values...
-   */
-  private JTable getExtensionTable ()
-    {
-    extensionsTableModel = new ExtensionsTableModel();
-
-    JTable aTable = new JTable (extensionsTableModel);
-    aTable.setSelectionMode (ListSelectionModel.SINGLE_SELECTION);
-    aTable.setPreferredScrollableViewportSize (new Dimension(400, 100));
-
-    TableColumnModel tcm = aTable.getColumnModel();
-    tcm.getColumn(0).setPreferredWidth(20);
-    tcm.getColumn(2).setPreferredWidth(250);
-
-    aTable.setRowHeight(18);
-    aTable.setRowSelectionAllowed(false);
-
-    aTable.addMouseListener (new myMouseAdapter() );
-  
-
-    return aTable;
-    }
-
-  /**
-   * When a mouse is clicked I come here
-   * Maybe I can do it with the selected, it may be easier... next release ?
-   */
-  private class myMouseAdapter extends MouseAdapter
-    {
-    public void mouseClicked (MouseEvent e) 
-      {
-      if (e.getClickCount() == 1) showDetails();
-      }
-    }
-
-  /**
-   * This models the data of the table. Basically the JTable ask this class
-   * about the data to be displayed on the screen
-   */
-  private class ExtensionsTableModel extends AbstractTableModel
-    {
-    // It does not matter very much if it is not static, it is created once only
-    private String columnNames[] = {
-    "",
-    Config.getString("extmgr.statuscolumn"),
-    Config.getString("extmgr.namecolumn"),
-    Config.getString("extmgr.typecolumn"),
-    Config.getString("extmgr.versioncolumn")};
-
-    public int getRowCount()     {  return getExtensions().size();    }
-    public int getColumnCount()  { return columnNames.length;         }
-    public String getColumnName(int col) {  return columnNames [col]; }
-    public boolean isCellEditable(int row, int col) { return false;   }
+        public boolean isCellEditable(int row, int col) 
+        { 
+            return false;   
+        }
         
-    public Object getValueAt(int row, int col)
-      {
-      if (col == 0) return infoIcon;
+        public Object getValueAt(int row, int col)
+        {
+            if (col == 0) 
+                return infoIcon;
 
-      // I really need to sort this out... Do I need to call it every time ?
-      ExtensionWrapper wrapper = getWrapper (row);
-      if ( wrapper == null ) return "getValueAt: ERROR: no wrapper at row="+row+" col="+col;
+            // I really need to sort this out... Do I need to call it every time ?
+            ExtensionWrapper wrapper = getWrapper (row);
+            if ( wrapper == null ) 
+                return "getValueAt: ERROR: no wrapper at row="+row+" col="+col;
       
-      if (col == 1) return wrapper.getStatus();
-      if (col == 2) return wrapper.getName();
-      if (col == 3) return (wrapper.getProject()!=null)?projectString:installedString;
-      if (col == 4) return wrapper.getVersion(); 
+            if (col == 1) 
+                return wrapper.getStatus();
+
+            if (col == 2) 
+                return wrapper.getName();
+                
+            if (col == 3) 
+                return (wrapper.getProject()!=null)?projectString:installedString;
+                
+            if (col == 4) 
+                return wrapper.getVersion(); 
       
-      // If I trow an exception all will stop. This instead keeps going
-      return "getValueAt: ERROR at row="+row+" col="+col;
-      }
+            // If I trow an exception all will stop. This instead keeps going
+            return "getValueAt: ERROR at row="+row+" col="+col;
+        }
 
-    public Class getColumnClass(int col) 
-      {
-      if ( col == 0 ) return new ImageIcon().getClass();
-      return new String().getClass();
-      }
+        public Class getColumnClass(int col) 
+        {
+            if ( col == 0 ) 
+                return new ImageIcon().getClass();
 
-    private ExtensionWrapper getWrapper (int index)
-      {
-      // Every time I am called I reload this ?
-      List exts = getExtensions(); // of ExtensionWrapper
-      if (index > exts.size()) return null;
-      return (ExtensionWrapper) exts.get (index);
-      }
+            return new String().getClass();
+        }
+
+        private ExtensionWrapper getWrapper (int index)
+        {
+            // Every time I am called I reload this ?
+            List exts = getExtensions(); // of ExtensionWrapper
+            if (index > exts.size()) 
+                return null;
+
+            return (ExtensionWrapper) exts.get (index);
+        }
         
     
-    private List getExtensions()
-      {
-      // Just wondering if there is a thread sync issue here
-      return ExtensionsManager.getExtMgr().getExtensions();
-      }
+        private List getExtensions()
+        {
+            // Just wondering if there is a thread sync issue here
+            return ExtensionsManager.getExtMgr().getExtensions();
+        }
     }
-  }
+}
