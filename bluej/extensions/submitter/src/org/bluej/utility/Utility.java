bd6d14d1313d5bbe2cb731ccf0fe61fb45e6ef81
damiano <damiano@bluej.org>
2003-01-27 09:41:52 +0000

    General utilities go here
    (from subversion r1605)

---
+++ bluej/extensions/submitter/src/org/bluej/utility/Utility.java
@@ -0,0 +1,60 @@
+package org.bluej.utility;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * This is just a Utility class, functions here are quite general ones.
+ */
+ 
+public class Utility 
+  {
+
+  /**
+   * CLoses a stream without trowing errors here and there
+   */
+  public static void inputStreamClose ( InputStream iStream )
+    {
+    try
+      {
+      if ( iStream != null ) iStream.close();
+      }
+    catch ( Exception exc )
+      {
+      // NOthing, really...  
+      }
+    }
+
+  /**
+   * CLoses a stream without trowing errors here and there
+   */
+  public static void outputStreamClose ( OutputStream oStream )
+    {
+    try
+      {
+      if ( oStream != null ) oStream.close();
+      }
+    catch ( Exception exc )
+      {
+      // NOthing, really...  
+      }
+    }
+
+
+  /**
+   * Convert a string into an int with a default and without complaining...
+   */
+  public static int convStringToInt ( String input, int defVal )
+    {
+    try 
+      {
+      Integer risul = Integer.decode(input);
+      return risul.intValue();
+      }
+    catch ( Exception eee )
+      {
+      return defVal;
+      }
+    }
+
+  }
