d7c156152ae99a35629ba2ca018e3a70b48f43ee
damiano <damiano@bluej.org>
2003-04-10 11:13:50 +0000

    Comment cleaning
    (from subversion r1815)

--- bluej/src/bluej/extensions/BArray.java
+++ bluej/src/bluej/extensions/BArray.java
@@ -6,7 +6,11 @@ import com.sun.jdi.*;
  * This is an equivalent to java.lang.reflect.Array for Arrays objects in BlueJ.
  * It allows you to get items of an array Object.
  * 
- * @version $Id: BArray.java 1726 2003-03-24 13:33:06Z damiano $
+ * @version $Id: BArray.java 1815 2003-04-10 11:13:50Z damiano $
+ */
+
+/*
+ * Author Damiano Bolla, University of Kent at Canterbury, 2003
  */
 public class BArray 
 {
@@ -14,10 +18,10 @@ public class BArray
    * Given an array object contained inside a BObject, returns the item at the given index.
    * This returns an Object since this can return primitive types wrappers for most cases.
    * Ex: in case you have arrays of int, long, boolean and so on.
-   * In case the array is composed of real OBJECTS then what will be returned is 
+   * In case the array is composed of real objects then what will be returned is 
    * a BObject itself, this is also in the case that it is a nested array.
    * 
-   * @param thisArray This MUST be an array object of which you want the given item
+   * @param thisArray This must be an array object of which you want the given item.
    * @param itemIndex The index in the array where you want to peek.
    * 
    * @return an Object that encapsulate the specific item or null if not an array.
