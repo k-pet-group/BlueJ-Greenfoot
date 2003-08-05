64bf5d05848c705b0f9e87eca95fc5dcf6d84790
mik <mik@bluej.org>
2003-08-05 14:50:41 +0000

    *** empty log message ***
    (from subversion r2153)

--- bluej/boot/src/bluej/SplashWindow.java
+++ bluej/boot/src/bluej/SplashWindow.java
@@ -11,7 +11,7 @@ import java.net.URL;
  * is starting up.
  *
  * @author  Michael Kolling
- * @version $Id: SplashWindow.java 2152 2003-08-05 14:47:36Z mik $
+ * @version $Id: SplashWindow.java 2153 2003-08-05 14:50:41Z mik $
  */
 
 public class SplashWindow extends JWindow
@@ -65,7 +65,7 @@ public class SplashWindow extends JWindow
         Graphics2D g = (Graphics2D)getGlassPane().getGraphics();
         g.setColor(new Color(51,102,153));
         g.setFont(new Font("SansSerif", Font.PLAIN, 16));
-        g.drawString("Version " + Boot.BLUEJ_VERSION, 24, 
+        g.drawString("Version " + Boot.BLUEJ_VERSION, 26, 
                      image.getHeight()-20);
     }
 
