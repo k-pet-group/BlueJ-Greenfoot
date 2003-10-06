4b99fdec5ceeedab0e2b5e86850e68bc209308fa
fisker <fisker@bluej.org>
2003-10-06 12:57:53 +0000

    part of multiselection
    (from subversion r2201)

---
+++ bluej/src/bluej/graph/GraphElementManager.java
@@ -0,0 +1,152 @@
+/*
+ * Created on Sep 17, 2003
+ *
+ */
+package bluej.graph;
+
+import java.awt.event.MouseEvent;
+import java.util.*;
+
+
+/**
+ * 
+ * @author fisker
+ *
+ */
+public class GraphElementManager 
+{
+    private GraphEditor graphEditor;
+    private List graphElements = new LinkedList();
+    
+    
+    /**
+     * 
+     * @param graphEditor
+     */
+    public GraphElementManager(GraphEditor graphEditor)
+    {
+        this.graphEditor = graphEditor;
+    }
+
+    /**
+     * Add an unselected selectable graphElement to the GraphElementManager and 
+     * set it's 'selected' flag true.
+     * If the graphElement is not implementing Selectable or is already 
+     * selected, nothing happens.  
+     * @param graphElement a GraphElement implementing Selectable which 
+     * returns false if it's 'isSelected' method is called.
+     */
+    public void add(GraphElement graphElement){
+        if (graphElement instanceof Selectable && !((Selectable)graphElement).isSelected()){
+            ((Selectable)graphElement).setSelected(true);
+            graphElements.add(graphElement);            
+        }
+    }
+    
+    /**
+     * Move all the elements from another graphElementManager to this one.
+     * @param graphElementManager the other graphElementManager
+     */
+    public void moveAll(GraphElementManager graphElementManager){
+        GraphElement graphElement;
+        for(Iterator i=graphElementManager.graphElements.iterator();i.hasNext();){
+            graphElement = (GraphElement) i.next();
+            i.remove();
+            graphElements.add(graphElement);
+        }
+    }
+    
+    /**
+     * Remove the graphElement and set it's 'selected' flag false.
+     * @param graphElement
+     */
+    public void remove(GraphElement graphElement){
+        if (graphElement!=null && graphElement instanceof Selectable){
+            ((Selectable)graphElement).setSelected(false);
+        }
+        graphElements.remove(graphElement);
+    }
+    
+    /**
+     * Remove all the graphElements from the list. 
+     * Set each removed grahpElement 'selected' flag to false.
+     * Does NOT selfuse remove method.
+     */
+    public void clear(){
+        GraphElement graphElement;
+        for(Iterator i=graphElements.iterator();i.hasNext();){
+            graphElement = (GraphElement) i.next();
+            if(graphElement instanceof Selectable){
+                ((Selectable)graphElement).setSelected(false);
+            } 
+            i.remove();
+        }
+    }
+    
+    /**
+     * Invoke 'mouseDragged' on all the GraphElements in the list
+     * @param evt
+     */
+    public void mouseDragged(MouseEvent evt){
+        GraphElement graphElement = null;
+        int x = evt.getX();
+        int y = evt.getY();
+        for(Iterator i=graphElements.iterator(); i.hasNext();){
+            graphElement = (GraphElement) i.next();
+            graphElement.mouseDragged(evt, x, y, graphEditor);
+        }
+    }
+    
+    
+    /**
+     * Invoke 'mouseMoved' on all the GraphElements in the list
+     * @param evt the mouseEvent
+     */
+    public void mouseMoved(MouseEvent evt){
+        GraphElement graphElement;
+        int x = evt.getX();
+        int y = evt.getY();
+        for(Iterator i=graphElements.iterator(); i.hasNext();){
+            graphElement = (GraphElement) i.next();
+            graphElement.mouseMoved(evt, x, y, graphEditor);
+        }
+    }
+    
+    /**
+     * Invoke 'mousePressed' on all the GraphElements in the list
+     * @param evt the mouseEvent
+     */
+    public void mousePressed(MouseEvent evt){
+        GraphElement graphElement;
+        int x = evt.getX();
+        int y = evt.getY();
+        for(Iterator i=graphElements.iterator(); i.hasNext();){
+            graphElement = (GraphElement) i.next();
+            graphElement.mousePressed(evt, x, y, graphEditor);
+        }
+    }
+
+    /**
+     * Invoke 'mouseReleased' on all the GraphElements in the list
+     * @param evt the mouseEvent
+     */
+    public void mouseReleased(MouseEvent evt){
+        GraphElement graphElement;
+        int x = evt.getX();
+        int y = evt.getY();
+        for(Iterator i=graphElements.iterator(); i.hasNext();){
+            graphElement = (GraphElement) i.next();
+            graphElement.mouseReleased(evt, x, y, graphEditor);
+        }
+    }
+    
+    /**
+     * Get the number of graphElements in this graphElementManager
+     * @return the number of elements
+     */
+    public int getSize(){
+        return graphElements.size();
+    }
+    
+    
+}
---
+++ bluej/src/bluej/graph/Marquee.java
@@ -0,0 +1,121 @@
+/*
+ * Created on Sep 17, 2003
+ *
+ */
+package bluej.graph;
+
+import java.awt.*;
+import java.awt.Graphics2D;
+import java.util.Iterator;
+
+import bluej.Config;
+
+/**
+ * @author fisker
+ *
+ */
+public class Marquee
+{
+    static final Color graphbg = Config.getItemColour("colour.graph.background");
+    static final float alpha = (float)0.2;
+    private Graph graph;
+    private GraphEditor graphEditor;
+    private int drag_start_x, drag_start_y;
+    private Rectangle oldRect;
+    private GraphElementManager graphElementManger;
+    private AlphaComposite alphaComposite;
+    private Composite oldComposite;
+    
+    /**
+     * Create a Marquee
+     * @param graph
+     * @param graphEditor
+     */
+    public Marquee(Graph graph, GraphEditor graphEditor){
+        this.graph = graph;
+        this.graphEditor = graphEditor;
+        this.graphElementManger = new GraphElementManager(graphEditor);
+        this.alphaComposite = 
+                AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha);
+    }
+    
+    /**
+     * start the marquee at point x, y
+     * @param x
+     * @param y
+     */
+    public void start(int x, int y){
+           drag_start_x = x;
+           drag_start_y = y;
+           graphElementManger.clear();
+    }
+    
+    /**
+     * Place the marquee from its starting point to the 
+     * coordinate (drag_x, drag_y).
+     * The marquee must have been started before this method is called.
+     * @param drag_x
+     * @param drag_y
+     */
+    public void move(int drag_x, int drag_y){
+        int x = drag_start_x;
+        int y = drag_start_y;
+        int w = drag_x- drag_start_x;
+        int h = drag_y - drag_start_y;
+        //Rectangle can't handle negative numbers, modify coordinates
+        if(w<0) x = x+w;
+        if(h<0) y = y+h;
+        w = Math.abs(w);
+        h = Math.abs(h);
+        Rectangle newRect = new Rectangle(x, y, w, h);  
+        oldRect = newRect;
+        graphEditor.repaint();
+        findSelectedVertices(x,y,w,h);
+    }
+    
+    
+    private void findSelectedVertices(int x, int y, int w, int h){
+        //clear the currently selected
+       graphElementManger.clear();
+        //find the intersecting vertices
+       Vertex v;
+       for (Iterator it = graph.getVertices(); it.hasNext();){
+           v = (Vertex)it.next();
+           if(v.getRectangle().intersects(x, y, w, h)){
+               graphElementManger.add(v);
+           }
+       }
+    }
+    
+    public void stop(){
+        oldRect = null;
+            graphEditor.repaint();
+    }
+        
+    public GraphElementManager getGraphElementManager(){
+        return graphElementManger;
+    }
+    
+    public void draw(Graphics g){
+        Graphics2D g2D = (Graphics2D) g.create();
+        oldComposite = g2D.getComposite();
+                
+        if(oldRect != null){
+            g2D.setColor(Color.BLACK);
+            g2D.draw(oldRect);
+            g2D.setComposite(alphaComposite);
+            g2D.setColor(Color.GRAY);
+            g2D.fill(oldRect);
+            g2D.setComposite(oldComposite);     
+        }
+    }
+    
+    /**
+     * Get the GraphElementManger
+     * @return
+     */
+    public GraphElementManager getGraphElementManger()
+    {
+        return graphElementManger;
+    }
+}
