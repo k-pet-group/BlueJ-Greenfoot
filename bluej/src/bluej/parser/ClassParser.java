94ef34365f379e237a2cc4c52fad764be2ba2118
ajp <ajp@bluej.org>
2003-11-13 04:07:56 +0000

    removed eclipse warnings. will need to be redone each time the parser is regenerated
    (from subversion r2327)

--- bluej/src/bluej/parser/ClassParser.java
+++ bluej/src/bluej/parser/ClassParser.java
@@ -2,31 +2,12 @@
 
     package bluej.parser;
 
-import antlr.TokenBuffer;
-import antlr.TokenStreamException;
-import antlr.TokenStreamIOException;
-import antlr.ANTLRException;
-import antlr.LLkParser;
-import antlr.Token;
-import antlr.TokenStream;
-import antlr.RecognitionException;
-import antlr.NoViableAltException;
-import antlr.MismatchedTokenException;
-import antlr.SemanticException;
-import antlr.ParserSharedInputState;
+import java.io.*;
+import java.util.Vector;
+
+import antlr.*;
 import antlr.collections.impl.BitSet;
-
-    import bluej.utility.Debug;
-    import bluej.parser.symtab.SymbolTable;
-    import bluej.parser.symtab.JavaVector;
-    import bluej.parser.symtab.DummyClass;
-    import bluej.parser.symtab.ClassInfo;
-    import bluej.parser.symtab.Selection;
-
-    import antlr.*;
-
-    import java.util.Vector;
-    import java.io.*;
+import bluej.parser.symtab.*;
 
     class JavaBitSet extends java.util.BitSet
     {
