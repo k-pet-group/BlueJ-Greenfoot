1cc0308feeb67914d3824e95836594d2ff79b7f6
ajp <ajp@bluej.org>
2003-11-13 03:06:51 +0000

    test of jdk 1.4 syntax
    (from subversion r2326)

---
+++ bluej/test/src/bluej/parser/ast/Java14ParseTest.java
@@ -0,0 +1,59 @@
+package bluej.parser.ast;
+
+import java.io.*;
+import java.net.URL;
+
+import antlr.*;
+
+/**
+ * Run a whole directory of sample source files through our parser.
+ *
+ * @author  Andrew Patterson
+ * @version (a version number or a date)
+ */
+public class Java14ParseTest extends junit.framework.TestCase
+{
+    /**
+     * Get a data or result file from our hidden stash..
+     * 
+     * @param name
+     * @return
+     */
+    private File getFile(String name)
+    {
+        URL url = getClass().getResource("/bluej/parser/ast/data/" + name);
+        
+        if (url == null || url.getFile().equals(""))
+            return null;
+        else
+            return new File(url.getFile());
+    }
+    
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    protected void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    protected void tearDown()
+    {
+    }
+
+    /**
+     * A sample test case method
+     */
+    public void testStandardAssertion()
+        throws RecognitionException, TokenStreamException, FileNotFoundException
+    {
+        JavaAnalyzer ja = new JavaAnalyzer(
+            new BufferedReader(new FileReader(getFile("14_assertion.dat"))));
+    } 
+}
