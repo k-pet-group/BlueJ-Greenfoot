866cdfad6e3e7bd44ee9283bd7554f7e7023b8ad
polle <polle@bluej.org>
2004-06-01 13:07:17 +0000

    isVarArgs() is now correctly implemented. The bug with the isVarArgs has been fixed with JDK 1.5 beta 2.
    (from subversion r2564)

--- bluej/src/bluej/views/ConstructorView.java
+++ bluej/src/bluej/views/ConstructorView.java
@@ -7,7 +7,7 @@ import bluej.utility.JavaNames;
 /**
  * A representation of a Java constructor in BlueJ
  * 
- * @version $Id: ConstructorView.java 2560 2004-05-27 14:32:19Z polle $
+ * @version $Id: ConstructorView.java 2564 2004-06-01 13:07:17Z polle $
  * @author Michael Cahill
  * @author Michael Kolling
  */
@@ -79,12 +79,8 @@ public final class ConstructorView extends CallableView
 
     /**
      * Whether this method has a var arg.
-     * 
-     * Due to a bug in java 1.5 this doesn't work right now, and it always returns false.
-     * 
      */
     public boolean isVarArgs() {
-        //System.out.println("  isVarArgs  1.5: " + Config.isJava15() + " cons.isVarArgs" +  cons.isVarArgs());
         return (Config.isJava15() && cons.isVarArgs());
     }
 }
