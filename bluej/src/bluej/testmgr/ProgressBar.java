db86443ad0bec477e3a14451b89d48c2d9a7fe04
bquig <bquig@bluej.org>
2005-12-01 01:37:08 +0000

    Removed confusing and unnecessary method. Removed tabs.
    (from subversion r3725)

--- bluej/src/bluej/testmgr/ProgressBar.java
+++ bluej/src/bluej/testmgr/ProgressBar.java
@@ -6,48 +6,43 @@ import javax.swing.JProgressBar;
 
 /**
  * A progress bar showing the green/red status.
- *
- * @author  Andrew Patterson (derived from JUnit src)
- * @version $Id: ProgressBar.java 2997 2004-09-09 12:29:52Z mik $
+ * 
+ * @author Andrew Patterson (derived from JUnit src)
+ * @version $Id: ProgressBar.java 3725 2005-12-01 01:37:08Z bquig $
  */
 class ProgressBar extends JProgressBar
 {
     public static final Color redBarColour = new Color(208, 16, 16);
     public static final Color greenBarColour = new Color(32, 192, 32);
-    
-	private boolean fError = false;
-	
-	public ProgressBar()
-	{
-		super(); 
-		setForeground(getStatusColor());
-	}
-	
-	private Color getStatusColor()
-	{
-		if (fError)
-			return redBarColour;
-		return greenBarColour;
-	}
-		
-	public void reset()
-	{
-		fError= false;
-		setForeground(getStatusColor());
-		setValue(0);
-	}
-	
-	public void setmaximum(int total)
-	{
-		setMaximum(total);
-	}
 
-	public void step(int value, boolean successful)
-	{
-		setValue(value);
-		if (!fError && !successful) {
-			fError= true;
-			setForeground(getStatusColor());
-		}
-	}
+    private boolean fError = false;
+
+    public ProgressBar() 
+    {
+        super();
+        setForeground(getStatusColor());
+    }
+
+    private Color getStatusColor()
+    {
+        if(fError)
+            return redBarColour;
+        return greenBarColour;
+    }
+
+    public void reset()
+    {
+        fError = false;
+        setForeground(getStatusColor());
+        setValue(0);
+    }
+
+    public void step(int value, boolean successful)
+    {
+        setValue(value);
+        if(!fError && !successful) {
+            fError = true;
+            setForeground(getStatusColor());
+        }
+    }
 }
