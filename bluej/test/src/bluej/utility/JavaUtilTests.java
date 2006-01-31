270fa727f06eb6a70be4deebc9d9cdcd51ca835f
davmac <davmac@bluej.org>
2006-01-31 00:11:40 +0000

    Add unit test for signature generation
    (from subversion r3752)

--- bluej/test/src/bluej/utility/JavaUtilTests.java
+++ bluej/test/src/bluej/utility/JavaUtilTests.java
@@ -1,5 +1,7 @@
 package bluej.utility;
 
+import java.lang.reflect.Method;
+
 import junit.framework.TestCase;
 
 public class JavaUtilTests extends TestCase
@@ -21,8 +23,60 @@ public class JavaUtilTests extends TestCase
     public void test1()
     {
         JavaUtils ju = JavaUtils.getJavaUtils();
-        ju.getTypeParams(Enum.class);
+        try {
+            Class enumClass = getClass().getClassLoader().loadClass("java.lang.Enum");
+            ju.getTypeParams(enumClass);
+        }
+        catch (ClassNotFoundException cnfe) {}
         // ok test passed
+    }
+    
+    // This is just a method for testing signature creation
+    public void sampleMethod(int arg1, int arg2)
+    {
+        
+    }
+    
+    /**
+     * Test that method/constructor signatures are constructed correctly.
+     */
+    public void testSignatures()
+    {
+        JavaUtils jutils = JavaUtils.getJavaUtils();
+        JavaUtils jutils14 = new JavaUtils14();
+        boolean onjava5 = ! (jutils instanceof JavaUtils14);
+        
+        Method sampleMeth = null;
+        
+        Class thisClass = getClass();
+        try {
+            sampleMeth = thisClass.getMethod("sampleMethod", new Class [] {int.class, int.class});
+        }
+        catch (NoSuchMethodException nsme) {
+            fail();
+        }
+        
+        String sig = jutils.getSignature(sampleMeth);
+        assertEquals(sig, "void sampleMethod(int, int)");
+        
+        if (onjava5) {
+            // repeat the same test using the java 1.4 javautils
+            sig = jutils14.getSignature(sampleMeth);
+            assertEquals(sig, "void sampleMethod(int, int)");
+            
+            // test a varargs method
+            Class clazz = Class.class;
+            try {
+                sampleMeth = clazz.getMethod("getConstructor", new Class [] {Class [].class});
+            }
+            catch (NoSuchMethodException nsme) {
+                fail();
+            }
+            
+            sig = jutils.getSignature(sampleMeth);
+            assertEquals(sig, "Constructor<T> getConstructor(Class ...)");
+        }
+        
     }
     
 }
