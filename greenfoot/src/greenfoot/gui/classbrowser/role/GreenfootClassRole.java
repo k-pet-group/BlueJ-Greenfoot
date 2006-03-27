e254f7f7bf2b9a8a436a6672d028fa5716ab9227
davmac <davmac@bluej.org>
2006-03-27 03:49:56 +0000

    Use the class image cache in the class views.
    (from subversion r3884)

--- greenfoot/src/greenfoot/gui/classbrowser/role/GreenfootClassRole.java
+++ greenfoot/src/greenfoot/gui/classbrowser/role/GreenfootClassRole.java
@@ -1,11 +1,10 @@
 package greenfoot.gui.classbrowser.role;
 
-import bluej.Config;
-import bluej.prefmgr.PrefMgr;
 import greenfoot.GreenfootImage;
 import greenfoot.actions.DragProxyAction;
 import greenfoot.actions.SelectImageAction;
 import greenfoot.core.GClass;
+import greenfoot.core.Greenfoot;
 import greenfoot.core.ObjectDragProxy;
 import greenfoot.gui.classbrowser.ClassView;
 import greenfoot.util.GreenfootUtil;
@@ -13,25 +12,25 @@ import greenfoot.util.GreenfootUtil;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import javax.imageio.ImageIO;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.ImageIcon;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 
+import bluej.Config;
+import bluej.prefmgr.PrefMgr;
+
 /**
  * 
  * @author Poul Henriksen
- * @version $Id: GreenfootClassRole.java 3867 2006-03-24 04:51:45Z davmac $
+ * @version $Id: GreenfootClassRole.java 3884 2006-03-27 03:49:56Z davmac $
  * 
  */
 public class GreenfootClassRole extends ClassRole
@@ -40,7 +39,6 @@ public class GreenfootClassRole extends ClassRole
     protected final Color envOpColour = Config.getItemColour("colour.menu.environOp");
 
     private GClass gClass;
-    private Image image;
     private ClassView classView;
 
     /*
@@ -59,7 +57,7 @@ public class GreenfootClassRole extends ClassRole
         classView.setText(name);
         
         // Add the image label
-        image = getImage();
+        Image image = getImage();
         if (image != null) {
             Image scaledImage = GreenfootUtil.getScaledImage(image, iconSize.width, iconSize.height);
             classView.setIcon(new ImageIcon(scaledImage));
@@ -71,8 +69,8 @@ public class GreenfootClassRole extends ClassRole
      */
     public void changeImage()
     {
-        image = null;
-        getImage();
+        Greenfoot.getInstance().removeCachedImage(classView.getClassName());
+        Image image = getImage();
         if (image != null) {
             Image scaledImage = GreenfootUtil.getScaledImage(image, iconSize.width, iconSize.height);
             classView.setIcon(new ImageIcon(scaledImage));
@@ -80,11 +78,18 @@ public class GreenfootClassRole extends ClassRole
     }
     
     public GreenfootImage getGreenfootImage() {
-        Image im = getImage();
-        if(im == null) {
-            return null;
+        GClass gclass = classView.getGClass();
+        String className = gclass.getQualifiedName();
+        
+        while (gclass != null) {
+            GreenfootImage gfImage = Greenfoot.getInstance().getClassImage(className);
+            if (gfImage != null) {
+                return gfImage;
+            }
+            gclass = gclass.getSuperclass();
         }
-        return new GreenfootImage(im);
+        
+        return null;
     }
     
     /**
@@ -94,34 +99,13 @@ public class GreenfootClassRole extends ClassRole
      */
     public Image getImage()
     {
-        if (image == null) {
-            
-            String imageName = null;
-            GClass imageClass = gClass;
-            
-            // go up the class hierarchy until we find a class with an image
-            while (imageClass != null) {
-                imageName = imageClass.getClassProperty("image");
-                if (imageName != null) {
-                    break;
-                }
-                imageClass = imageClass.getSuperclass();
-            }
-            
-            // If we can't find any image, use the greenfoot logo
-            if (imageName == null) {
-                imageName = "greenfoot-logo.png";
-            }
-            
-            try {
-                image = ImageIO.read(new File(new File("images"), imageName));
-            }
-            catch (IOException ioe) {
-                image = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
-            }
+        GreenfootImage gfImage = getGreenfootImage();
+        if (gfImage != null) {
+            return gfImage.getAWTImage();
         }
-        
-        return image;
+        else {
+            return null;
+        }
     }
 
     /**
