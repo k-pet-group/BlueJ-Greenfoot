2a28c7fa6fde5e4cd2733a43a8c223209ab40b6f
mik <mik@bluej.org>
2006-04-25 16:17:27 +0000

    new layout manager to center world
    (from subversion r4014)

---
+++ greenfoot/src/greenfoot/gui/CenterLayout.java
@@ -0,0 +1,54 @@
+package greenfoot.gui;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Insets;
+
+/*
+ * This is a layout class for a container that has exactly one child component.
+ * The layout places that child in the centre of the container.
+ *
+ * @author mik
+ * @version 0.1
+ */
+
+public class CenterLayout extends FlowLayout {
+    
+    /**
+     * Creates a new instance of CenterLayout.
+     */
+    public CenterLayout() {
+        super(FlowLayout.LEFT, 20, 20);
+        
+    }
+    
+    
+    /**
+     * Layout the target container.
+     */
+    public void layoutContainer(Container target) {
+      synchronized (target.getTreeLock()) {
+	Insets insets = target.getInsets();
+	int maxwidth = target.getWidth() - (insets.left + insets.right + getHgap()*2);
+	int maxheight = target.getHeight() - (insets.top + insets.bottom + getVgap()*2);
+        int y = insets.top + getVgap();
+
+        Component m = target.getComponent(0);
+        if (m.isVisible()) {
+            Dimension d = m.getPreferredSize();
+            m.setSize(d.width, d.height);
+            
+            int hspace = maxwidth - d.width;
+            int xpos = insets.left + getHgap() + (hspace / 2);
+            
+            int vspace = maxheight - d.height;
+            int ypos = insets.top + getVgap() + (vspace / 2);
+            
+            m.setLocation(xpos, ypos);
+
+	}
+      }
+    }
+}
