3bb9130f57bb19a2c9ea0f6c6efcfa5f6ede680c
mik <mik@bluej.org>
2006-04-26 15:46:50 +0000

    updated ants to make better use of new add object method
    (from subversion r4025)

--- greenfoot/scenarios/ants/AntWorld.java
+++ greenfoot/scenarios/ants/AntWorld.java
@@ -22,40 +22,52 @@ public class AntWorld extends World
         super(SIZE/RESOLUTION,SIZE/RESOLUTION,RESOLUTION);       
         GreenfootImage background = new GreenfootImage("images/sand.jpg");
         setBackground(background);
+
+        scenario3();
     }
     
     public void scenario1()
     {
-        newObject(new AntHill(70), SIZE / 2, SIZE / 2);
-        newObject(new Food(), SIZE/2, SIZE/2 - 260);
-        newObject(new Food(), SIZE/2 + 215, SIZE/2 - 100);
-        newObject(new Food(), SIZE/2 + 215, SIZE/2 + 100);
-        newObject(new Food(), SIZE/2, SIZE/2 + 260);
-        newObject(new Food(), SIZE/2 - 215, SIZE/2 + 100);
-        newObject(new Food(), SIZE/2 - 215, SIZE/2 - 100);
+        addObject(new AntHill(70), SIZE / 2, SIZE / 2);
+        addObject(new Food(), SIZE/2, SIZE/2 - 260);
+        addObject(new Food(), SIZE/2 + 215, SIZE/2 - 100);
+        addObject(new Food(), SIZE/2 + 215, SIZE/2 + 100);
+        addObject(new Food(), SIZE/2, SIZE/2 + 260);
+        addObject(new Food(), SIZE/2 - 215, SIZE/2 + 100);
+        addObject(new Food(), SIZE/2 - 215, SIZE/2 - 100);
     }
 
     public void scenario2()
     {
-        newObject(new AntHill(40), 546, 356);
-        newObject(new AntHill(40), 95,267);
+        addObject(new AntHill(40), 546, 356);
+        addObject(new AntHill(40), 95,267);
         
-        newObject(new Food(), 80, 71);
-        newObject(new Food(), 291, 56);
-        newObject(new Food(), 516, 212);
-        newObject(new Food(), 311, 269);
-        newObject(new Food(), 318, 299);
-        newObject(new Food(), 315, 331);
-        newObject(new Food(), 141, 425);
-        newObject(new Food(), 378, 547);
-        newObject(new Food(), 566, 529);
+        addObject(new Food(), 80, 71);
+        addObject(new Food(), 291, 56);
+        addObject(new Food(), 516, 212);
+        addObject(new Food(), 311, 269);
+        addObject(new Food(), 318, 299);
+        addObject(new Food(), 315, 331);
+        addObject(new Food(), 141, 425);
+        addObject(new Food(), 378, 547);
+        addObject(new Food(), 566, 529);
     }
 
-    private void newObject(Actor obj, int x, int y)
+    public void scenario3()
     {
-        addObject(obj, x, y);
+        addObject(new AntHill(40), 576, 134);
+        addObject(new AntHill(40), 59, 512);
+        
+        addObject(new Food(), 182, 84);
+        addObject(new Food(), 39, 308);
+        addObject(new Food(), 249, 251);
+        addObject(new Food(), 270, 272);
+        addObject(new Food(), 291, 253);
+        addObject(new Food(), 339, 342);
+        addObject(new Food(), 593, 340);
+        addObject(new Food(), 487, 565);
     }
-    
+
     public int getResolution() {
         return RESOLUTION;
     }
