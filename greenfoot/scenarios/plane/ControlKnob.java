9cfce760d142eca4025528f0182c526a6bb87f4d
davmac <davmac@bluej.org>
2006-05-17 14:13:40 +0000

    Update Plane scenario documentation
    (from subversion r4297)

--- greenfoot/scenarios/plane/ControlKnob.java
+++ greenfoot/scenarios/plane/ControlKnob.java
@@ -1,19 +1,34 @@
 import greenfoot.World;
 import greenfoot.Actor;
 
+/**
+ * A control knob for a plane. A ControlKnob can be dragged around
+ * the world, and this will turn the associated plane.
+ */
 public class ControlKnob extends Actor
 {
     private Plane plane;
 
-    public void attachPlane(Plane plane) {
+    /**
+     * Associate a plane with this control knob.
+     */
+    public void attachPlane(Plane plane)
+    {
         this.plane = plane;
     }
     
-    public void setLocation(int x, int y) {
+    /**
+     * This method is called when the knob is dragged through
+     * the world.
+     */
+    public void setLocation(int x, int y)
+    {
         if(plane != null) {
             plane.setRotation(x);
         }
-        //ignore y location
+
+        // ignore y location - keep the knob at the same vertical
+        // position
         super.setLocation(x, 540);
     }
 
--- greenfoot/scenarios/plane/Plane.java
+++ greenfoot/scenarios/plane/Plane.java
@@ -1,19 +1,41 @@
 import greenfoot.World;
 import greenfoot.Actor;
 
+/**
+ * A Plane which flies above the field. The direction of the plane
+ * is controlled via a knob which is visible in the world. The knob
+ * will be created automatically when the plane is added to the
+ * world.
+ * 
+ * @author Michael Kolling
+ * @version $Id$
+ */
 public class Plane extends Actor
 {
     private static final double SPEED = 3.0;
 
-    public void addedToWorld(World world) {
+    /**
+     * This method will be called automatically when the plane
+     * is added to the world.
+     */
+    public void addedToWorld(World world)
+    {
         createControlKnob();
     }
 
+    /**
+     * Act is called repeatedly when the simulation is running.
+     * All a plane does is move forward a little.
+     */
     public void act()
     {
         move();
     }
     
+    /**
+     * Create the control knob which is used to control the
+     * direction of the plane.
+     */
     public void createControlKnob() 
     {
         ControlKnob ctrl = new ControlKnob();
@@ -21,6 +43,9 @@ public class Plane extends Actor
         ctrl.attachPlane(this);
     }
     
+    /**
+     * Move the plane forward in its current direction.
+     */
     public void move() {
         double angle = Math.toRadians(getRotation());
         int x = (int) Math.round(getX() + Math.cos(angle) * SPEED);
