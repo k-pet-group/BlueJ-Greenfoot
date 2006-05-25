cfa2670befd26f94b8e7f11ea10d89b41f42e33b
polle <polle@bluej.org>
2006-05-25 18:01:49 +0000

    Asteroids. Shoot them!
    (from subversion r4338)

---
+++ greenfoot/scenarios/asteroids/Space.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
+import java.awt.Color;
+
+/**
+ * ... the final frontier
+ * 
+ * @author Poul Henriksen
+ */
+public class Space extends World
+{
+    public Space() {
+        super(600, 400, 1);
+        getBackground().setColor(Color.BLACK);
+        getBackground().fill();
+    }
+}
