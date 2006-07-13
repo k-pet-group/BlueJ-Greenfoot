8258e7eb2d3d771d978ba482a70cc966cde9430e
polle <polle@bluej.org>
2006-07-13 14:29:11 +0000

    New scenario: maze builder
    (from subversion r4501)

---
+++ greenfoot/scenarios/maze/Wall.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
+import java.awt.Color;
+
+/**
+ * A wall in a maze.
+ * 
+ * @author Poul Henriksen
+ */
+public class Wall extends Actor
+{
+    public Wall()
+    {        
+    }
+}
