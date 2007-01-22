324e8981513c4b617ed8b7ae39cf424b0e716dbe
polle <polle@bluej.org>
2007-01-22 12:37:45 +0000

    Added distant stars to the background image of the world.
    (from subversion r4802)

--- greenfoot/scenarios/asteroids/Space.java
+++ greenfoot/scenarios/asteroids/Space.java
@@ -12,5 +12,21 @@ public class Space extends World
         super(600, 400, 1);
         getBackground().setColor(Color.BLACK);
         getBackground().fill();
+        createStars(300);
+        
+        addObject(new Rocket(), 300,200);
+        addObject(new Asteroid(), 100 ,100);
+        addObject(new Asteroid(), 500 ,300);        
+    }
+        
+    private void createStars(int number) {
+        GreenfootImage background = getBackground();             
+        for(int i=0; i < number; i++) {            
+             int x = (int) (Math.random() * getWidth());          
+             int y = (int) (Math.random() * getHeight());
+             int color = 255 - (int) (Math.random() * 220);
+             background.setColor(new Color(color,color,color));
+             background.fillOval(x,y,1,1);
+        }
     }
 }
