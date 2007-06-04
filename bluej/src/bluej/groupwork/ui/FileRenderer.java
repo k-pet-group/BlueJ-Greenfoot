c7603128981a512b21cd30fb071520259de6b291
bquig <bquig@bluej.org>
2007-06-04 04:30:31 +0000

    Refactoring to support use of ResourceDescriptor class
    (from subversion r5083)

--- bluej/src/bluej/groupwork/ui/FileRenderer.java
+++ bluej/src/bluej/groupwork/ui/FileRenderer.java
@@ -1,8 +1,5 @@
 package bluej.groupwork.ui;
 
-import bluej.Config;
-import bluej.groupwork.TeamStatusInfo;
-import bluej.pkgmgr.Package;
 import bluej.pkgmgr.Project;
 import java.awt.Component;
 import javax.swing.DefaultListCellRenderer;
@@ -15,7 +12,7 @@ import javax.swing.JList;
  * 
  * @author Bruce Quig
  * @author Davin McCall
- * @version $Id: FileRenderer.java 5074 2007-05-31 04:59:43Z davmac $
+ * @version $Id: FileRenderer.java 5083 2007-06-04 04:30:31Z bquig $
  */
 public class FileRenderer extends DefaultListCellRenderer
 {
@@ -28,35 +25,7 @@ public class FileRenderer extends DefaultListCellRenderer
         
     public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus)
     {
-        String status = value.toString();
-        if(value instanceof TeamStatusInfo) {
-            TeamStatusInfo info = (TeamStatusInfo)value;
-            boolean isPkgFile = info.getFile().getName().equals(Package.pkgfileName);
-            
-            if (isPkgFile) {
-                status = Config.getString("team.commit.layout") + " " + project.getPackageForFile(info.getFile());
-            }
-            
-            // file has been deleted
-            if(info.getStatus() == TeamStatusInfo.STATUS_DELETED) {
-                status += " (" + Config.getString("team.status.delete") + ")";
-            }
-            else if (info.getStatus() == TeamStatusInfo.STATUS_NEEDSADD) {
-                status += " (" + Config.getString("team.status.add") + ")";
-            }
-            else if (info.getStatus() == TeamStatusInfo.STATUS_NEEDSCHECKOUT) {
-                status += " (" + Config.getString("team.status.new") + ")";
-            }
-            else if (info.getStatus() == TeamStatusInfo.STATUS_REMOVED) {
-                status += " (" + Config.getString("team.status.removed") + ")";
-            }
-            else if (info.getStatus() == TeamStatusInfo.STATUS_NEEDSMERGE) {
-                if (! isPkgFile) {
-                    status += " (" + Config.getString("team.status.needsmerge") + ")";
-                }
-            }
-        }
-       
+        String status = ResourceDescriptor.getResource(project, value, true);       
         JLabel label = new JLabel(status);
         return label;
     }
