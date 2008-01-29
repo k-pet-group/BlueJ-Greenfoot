82348b3e14f861e3172afdbac89c43db455f5b7b
davmac <davmac@bluej.org>
2008-01-29 04:59:32 +0000

    Fix update/commit with packages and package layouts.
    (from subversion r5501)

--- bluej/src/bluej/groupwork/UpdateFilter.java
+++ bluej/src/bluej/groupwork/UpdateFilter.java
@@ -1,11 +1,11 @@
 package bluej.groupwork;
 
 /**
- * Class to filter CVS StatusInformation to calculate thos classes that will 
+ * Class to filter TeamStatusInfo objects to calculate those classes that will 
  * be changed when we next update. 
  *
  * @author Davin McCall
- * @version $Id: UpdateFilter.java 5096 2007-06-15 05:04:26Z davmac $
+ * @version $Id: UpdateFilter.java 5501 2008-01-29 04:59:32Z davmac $
  */
 public class UpdateFilter
 {
@@ -15,23 +15,47 @@ public class UpdateFilter
      */
     public boolean accept(TeamStatusInfo statusInfo)
     {
+        boolean isDir = statusInfo.getFile().isDirectory();
+        int stat = statusInfo.getStatus();
+        
+        if (stat == TeamStatusInfo.STATUS_NEEDSCHECKOUT) {
+            return true;
+        }
+        if (stat == TeamStatusInfo.STATUS_NEEDSMERGE) {
+            return ! isDir;
+        }
+        if (stat == TeamStatusInfo.STATUS_NEEDSUPDATE) {
+            return ! isDir;
+        }
+        if (stat == TeamStatusInfo.STATUS_REMOVED) {
+            return true;
+        }
+        if (stat == TeamStatusInfo.STATUS_CONFLICT_LDRM) {
+            // Locally deleted, remotely modified. Update pulls the repository version
+            return true;
+        }
+        if (stat == TeamStatusInfo.STATUS_CONFLICT_LMRD) {
+            // Update will succeed if forced (for bluej.pkg files)
+            return true;
+        }
+    
+        return false;
+    }
+    
+    /**
+     * For layout files, checks whether the file should be updated unconditionally.
+     */
+    public boolean updateAlways(TeamStatusInfo statusInfo)
+    {
         if (statusInfo.getStatus() == TeamStatusInfo.STATUS_NEEDSCHECKOUT) {
-            return true;
-        }
-        if (statusInfo.getStatus() == TeamStatusInfo.STATUS_NEEDSMERGE) {
-            return true;
-        }
-        if (statusInfo.getStatus() == TeamStatusInfo.STATUS_NEEDSUPDATE) {
             return true;
         }
         if (statusInfo.getStatus() == TeamStatusInfo.STATUS_REMOVED) {
             return true;
         }
-        if (statusInfo.getStatus() == TeamStatusInfo.STATUS_CONFLICT_LDRM) {
-            // Locally deleted, remotely modified. Update pulls the repository version
+        if (statusInfo.getStatus() == TeamStatusInfo.STATUS_CONFLICT_LMRD) {
             return true;
         }
-    
         return false;
     }
 }
