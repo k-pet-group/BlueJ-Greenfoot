3d78d71a6833867a2d9591308f2290cb0aeeceac
polle <polle@bluej.org>
2008-06-22 23:43:57 +0000

    updated Greenfoot version number from 1.4.5 BETA to 1.4.5
    (from subversion r5778)

--- bluej/boot/src/bluej/Boot.java
+++ bluej/boot/src/bluej/Boot.java
@@ -20,7 +20,7 @@ import java.util.Properties;
  * @author  Damiano Bolla
  * @author  Michael Kolling
  * @author  Bruce Quig
- * @version $Id: Boot.java 5744 2008-05-03 13:59:07Z polle $
+ * @version $Id: Boot.java 5778 2008-06-22 23:43:57Z polle $
  */
 public class Boot
 {
@@ -40,7 +40,7 @@ public static final int BLUEJ_VERSION_MAJOR = 2;
 
     public static final String BLUEJ_VERSION_TITLE = "BlueJ " + BLUEJ_VERSION;
     
-    public static String GREENFOOT_VERSION = "1.4.5 beta";
+    public static String GREENFOOT_VERSION = "1.4.5";
     public static String GREENFOOT_API_VERSION = "1.4.5";
 
     
