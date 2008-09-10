5ed745eec56921176c57d3f6c3058d56b3006faa
polle <polle@bluej.org>
2008-09-10 15:29:05 +0000

    Fixed bug #175: Exporting allowed with no world
    A dialog with an error message is displayed if export is attempted without a world class.
    
    (from subversion r5865)

--- greenfoot/src/greenfoot/actions/ExportProjectAction.java
+++ greenfoot/src/greenfoot/actions/ExportProjectAction.java
@@ -1,21 +1,27 @@
 package greenfoot.actions;
 
 import bluej.Config;
+import greenfoot.core.WorldHandler;
 import greenfoot.gui.GreenfootFrame;
+import greenfoot.gui.MessageDialog;
 import greenfoot.gui.export.ExportDialog;
 
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
+import javax.swing.JButton;
 
 /**
  * Action to export a project to a standalone program.
  * 
  * @author Poul Henriksen, Michael Kolling
- * @version $Id: ExportProjectAction.java 5284 2007-10-04 04:09:40Z bquig $
+ * @version $Id: ExportProjectAction.java 5865 2008-09-10 15:29:05Z polle $
  */
 public class ExportProjectAction extends AbstractAction 
 {
+    private static final String dialogTitle = Config.getString("export.noworld.dialog.title");
+    private static final String dialogMsg = Config.getString("export.noworld.dialog.msg");
+    
     private ExportDialog exportDialog;
     private GreenfootFrame gfFrame;
     
@@ -28,6 +34,13 @@ public class ExportProjectAction extends AbstractAction
 
     public void actionPerformed(ActionEvent event)
     {
+        if(WorldHandler.getInstance().getLastWorldClass() == null) {
+            JButton[] buttons = new JButton[]{new JButton(Config.getString("greenfoot.continue"))};
+            MessageDialog errorDialog = new MessageDialog(gfFrame, dialogMsg, dialogTitle, 50 , buttons);
+            errorDialog.display();
+            return;
+        }
+       
         if(exportDialog == null) {
             exportDialog = new ExportDialog(gfFrame);
         }
