95f1e6a865eea730b64140dd83c9addaaf709ea6
polle <polle@bluej.org>
2009-01-26 12:13:29 +0000

    Updated Greenfoot version to 1.5.1
    (from subversion r6072)

--- bluej/boot/src/bluej/Boot.java
+++ bluej/boot/src/bluej/Boot.java
@@ -20,7 +20,7 @@ import java.util.Properties;
  * @author  Damiano Bolla
  * @author  Michael Kolling
  * @author  Bruce Quig
- * @version $Id: Boot.java 6071 2009-01-26 12:03:19Z polle $
+ * @version $Id: Boot.java 6072 2009-01-26 12:13:29Z polle $
  */
 public class Boot
 {
@@ -45,7 +45,7 @@ public class Boot
     
     // The version numbers for Greenfoot are changed in the Greenfoot build.xml
     // and then the update-version target should be executed.
-    public static String GREENFOOT_VERSION = "1.5.0";
+    public static String GREENFOOT_VERSION = "1.5.1";
     public static String GREENFOOT_API_VERSION = "2.0.1";
     
     // A singleton boot object so the rest of BlueJ can pick up args etc.
