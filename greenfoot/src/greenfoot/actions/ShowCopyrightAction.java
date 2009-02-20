0d5c5da4e3d47ca6ddd8cecd2c5feb47ae51d7ee
polle <polle@bluej.org>
2009-02-20 13:38:53 +0000

    Updated Greenfoot copyright notice in the Greenfoot interface.
    (from subversion r6171)

--- greenfoot/src/greenfoot/actions/ShowCopyrightAction.java
+++ greenfoot/src/greenfoot/actions/ShowCopyrightAction.java
@@ -66,19 +66,11 @@ public class ShowCopyrightAction extends AbstractAction
     public void actionPerformed(ActionEvent e)
     {
             JOptionPane.showMessageDialog(parent, new String[]{
-                "Greenfoot \u00a9 2005-2008 Michael K\u00F6lling, Poul Henriksen.", " ",
-                "Greenfoot is available 'as is' free of charge for use and non-commercial",
-                "redistribution. Disassembly of the system is prohibited. Greenfoot may",
-                "not be sold for profit or included in other packages which are sold for",
-                "profit without written authorisation.",
-                " ",
-                "The Greenfoot library used in exported Greenfoot scenarios",
-                "('GreenfootScenarioViewer') is exempt from the not-for-profit clause.",
-                "Thus, this license imposes no commercial restrictions on scenarios",
-                "created and exported with Greenfoot. Licenses for individual scenarios",
-                "may be defined by scenario authors as they see fit, including",
-                "commercial and for-profit licenses. Disassembly of the Greenfoot",
-                "library is prohibited."
+                "Greenfoot \u00a9 2005-2009 Michael K\u00F6lling, Poul Henriksen.", " ",
+                "Greenfoot is available under the GNU General Public License",
+                "version 2 with Classpath exception.",
+                "For more information please see the files LICENSE.txt",
+                "and THIRDPARTYLICENSE.txt."
                 }, 
                 "Copyright, License and Redistribution", JOptionPane.INFORMATION_MESSAGE);
     }
