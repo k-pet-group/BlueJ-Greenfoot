5fc8ca65892489a6aa2535d4d88b65d84cc96e01
polle <polle@bluej.org>
2009-04-30 14:29:18 +0000

    Sound stuff: changed size of the cache.
    (from subversion r6294)

--- greenfoot/src/greenfoot/sound/SoundCache.java
+++ greenfoot/src/greenfoot/sound/SoundCache.java
@@ -28,46 +28,54 @@
 
 package greenfoot.sound;
 
-
-public class SoundCache 
+/**
+ * Cache for holding SoundClips.
+ * 
+ * @author Poul Henriksen
+ * 
+ */
+public class SoundCache
 {
-    private static final int CACHE_SIZE = 4; //20
+	private static final int CACHE_SIZE = 20;
 
-    private SoundClip[] cache = new SoundClip[CACHE_SIZE];
-    private int nextEntry;
-    
-    /**
-     * Create an empty SoundCache.
-     */
-    public SoundCache() 
-    {
-        nextEntry = 0;
-    }
-    
-    /**
-     * Add a sound to the cache.
-     */
-    public void put(SoundClip sound)
-    {
-    	if(cache[nextEntry] != null) {
-    		cache[nextEntry].setCloseWhenFinished(true);
-    	}
-        cache[nextEntry] = sound;
-        nextEntry = (nextEntry+1) % CACHE_SIZE;
-    }
-    
-    /**
-     * Try to find a sound in the cache. Returns the sound or null.
-     */
-    public SoundClip get(String name) 
-    {
-        int i = 0;
-        while(i < CACHE_SIZE) {
-            if(cache[i] != null && name.equals(cache[i].getName()) && !cache[i].isPlaying()) {
-                return cache[i];
-            }
-            i++;
-        }
-        return null;
-    }
+	private SoundClip[] cache = new SoundClip[CACHE_SIZE];
+	private int nextEntry;
+
+	/**
+	 * Create an empty SoundCache.
+	 */
+	public SoundCache()
+	{
+		nextEntry = 0;
+	}
+
+	/**
+	 * Add a sound to the cache.
+	 */
+	public void put(SoundClip sound)
+	{
+		if (cache[nextEntry] != null) {
+			// Make sure the discarded clip is closed when it has finished
+			// pĺaying.
+			cache[nextEntry].setCloseWhenFinished(true);
+		}
+		cache[nextEntry] = sound;
+		nextEntry = (nextEntry + 1) % CACHE_SIZE;
+	}
+
+	/**
+	 * Try to find a sound in the cache. Returns the sound or null.
+	 */
+	public SoundClip get(String name)
+	{
+		int i = 0;
+		while (i < CACHE_SIZE) {
+			if (cache[i] != null && name.equals(cache[i].getName())
+					&& !cache[i].isPlaying()) {
+				return cache[i];
+			}
+			i++;
+		}
+		return null;
+	}
 }
