b17df21388c04ecc747de99c754a3ffe1f040c68
davmac <davmac@bluej.org>
2009-07-01 06:15:00 +0000

    Add a couple more parser tests for the new parser.
    (from subversion r6412)

--- bluej/test/src/bluej/parser/NewParserTest.java
+++ bluej/test/src/bluej/parser/NewParserTest.java
@@ -34,8 +34,48 @@ public class NewParserTest extends TestCase
 		InfoParser ip = new InfoParser(sr);
 		List ll = new LinkedList();
 		assertTrue(ip.parseTypeSpec(false, ll));
-		// 6 tokens: LinkedList, '<', String, '[', ']', '>'
-		assertEquals(9, ll.size());
+		// 8 tokens: LinkedList, '<', List, '<', String, '[', ']', '>>'
+		assertEquals(8, ll.size());
+	}
+	
+	/**
+	 * Test multiple type parameters
+	 */
+	public void test3()
+	{
+		StringReader sr = new StringReader(
+				"Map<String,Integer> v1; "
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseStatement();
+	}
+	
+	/**
+	 * Test generic inner class of generic outer class
+	 */
+	public void test4()
+	{
+		StringReader sr = new StringReader(
+				"Outer<String>.Inner<String> v8; "
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseStatement();
+	}
+	
+	/**
+	 * Test wildcard type parameters
+	 */
+	public void test5()
+	{
+		StringReader sr = new StringReader(
+				"A<?> v8; " +
+				"A<? extends String> v9; " +
+				"A<? super String> v10;"
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseStatement();
+		ip.parseStatement();
+		ip.parseStatement();
 	}
 	
 }
