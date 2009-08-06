dc0bc03d623b265a4d035089ad33a270f0e7a3a6
davmac <davmac@bluej.org>
2009-08-06 23:41:29 +0000

    Restore new parser tests.
    (from subversion r6496)

---
+++ bluej/test/src/bluej/parser/NewParserTest.java
@@ -0,0 +1,335 @@
+package bluej.parser;
+
+import java.io.StringReader;
+import java.util.LinkedList;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+public class NewParserTest extends TestCase
+{
+	/**
+	 * Test array as type parameter
+	 */
+	public void test1()
+	{
+		StringReader sr = new StringReader(
+				"LinkedList<String[]>"
+		);
+		InfoParser ip = new InfoParser(sr);
+		List ll = new LinkedList();
+		assertTrue(ip.parseTypeSpec(false, ll));
+		// 6 tokens: LinkedList, '<', String, '[', ']', '>'
+		assertEquals(6, ll.size());
+	}
+	
+	/**
+	 * Test handling of '>>' sequence in type spec
+	 */
+	public void test2()
+	{
+		StringReader sr = new StringReader(
+				"LinkedList<List<String[]>>"
+		);
+		InfoParser ip = new InfoParser(sr);
+		List ll = new LinkedList();
+		assertTrue(ip.parseTypeSpec(false, ll));
+		// 8 tokens: LinkedList, '<', List, '<', String, '[', ']', '>>'
+		assertEquals(8, ll.size());
+	}
+	
+	/**
+	 * Test multiple type parameters
+	 */
+	public void test3()
+	{
+		StringReader sr = new StringReader(
+				"Map<String,Integer> v1; "
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseStatement();
+	}
+	
+	/**
+	 * Test generic inner class of generic outer class
+	 */
+	public void test4()
+	{
+		StringReader sr = new StringReader(
+				"Outer<String>.Inner<String> v8; "
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseStatement();
+	}
+	
+	/**
+	 * Test wildcard type parameters
+	 */
+	public void test5()
+	{
+		StringReader sr = new StringReader(
+				"A<?> v8; " +
+				"A<? extends String> v9; " +
+				"A<? super String> v10;"
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseStatement();
+		ip.parseStatement();
+		ip.parseStatement();
+	}
+	
+	/**
+	 * Test less-than operator.
+	 */
+	public void test6()
+	{
+		StringReader sr = new StringReader(
+				"b = (i < j);"
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseStatement();
+	}
+	
+	/**
+	 * Test a funky statement.
+	 */
+	public void test7()
+	{
+		StringReader sr = new StringReader(
+				"boolean.class.equals(T.class);"
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseStatement();
+	}
+	
+	/**
+	 * Test a class declaration with a single type parameter.
+	 */
+	public void test8()
+	{
+		StringReader sr = new StringReader(
+				"class A<T>{}"
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseTypeDef();
+	}
+	
+	/**
+	 * Test a class declaration containing a semi-colon
+	 */
+	public void test9()
+	{
+		StringReader sr = new StringReader(
+				"class A{;}"
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseTypeDef();
+	}
+	
+	/**
+	 * Test a simple enum
+	 */
+	public void test10()
+	{
+		StringReader sr = new StringReader(
+				"enum A {" +
+				"    one, two, three;" +
+				"    private int x;" +
+				"}"
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseTypeDef();
+	}
+	
+	/**
+	 * Test array declarators after a variable name.
+	 */
+	public void test11()
+	{
+		StringReader sr = new StringReader(
+				"int a[] = {1, 2, 3};"
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseStatement();
+	}
+	
+	/**
+	 * Test array declarators after a method parameter name.
+	 */
+	public void test12()
+	{
+		StringReader sr = new StringReader(
+				"int a[], int[] b);"
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseMethodParamsBody();
+	}
+	
+	/** 
+	 * Test array declarators after a field name.
+	 */
+	public void test13()
+	{
+		StringReader sr = new StringReader(
+				"class A { int x[] = {1,2,3}, y = 5; }"
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseTypeDef();
+	}
+	
+	/**
+	 * Test multiple variable declaration in a single statement.
+	 */
+	public void test14()
+	{
+		StringReader sr = new StringReader(
+				"int x[], y = 3, z, q;"
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseStatement();
+	}
+		
+	/**
+	 * Test annotation declaration
+	 */
+	public void test15()
+	{
+		StringReader sr = new StringReader(
+				"public @interface Copyright{  String value();}"
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseTypeDef();
+	}
+	
+	/**
+	 * Test use of marker annotation
+	 */
+	public void test16()
+	{
+		StringReader sr = new StringReader(
+				"@Preliminary public class TimeTravel { }"
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseTypeDef();
+	}
+	
+	/**
+	 * Test the use of an annotation.
+	 */
+	public void test17()
+	{
+		StringReader sr = new StringReader(
+				"@Copyright(\"2002 Yoyodyne Propulsion Systems\")"+
+				"public class NewParserTest { }"
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseTypeDef();
+	}
+	
+	/**
+	 * Test the '?:' operator.
+	 */
+	public void testQuestionOperator()
+	{
+		StringReader sr = new StringReader(
+				"Object g = (x<y) ? null : null;"
+				);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseStatement();
+	}
+	
+	/**
+	 * Test a static method call.
+	 */
+	public void testStaticMethodCall()
+	{
+		StringReader sr = new StringReader(
+				"AAA.bbb(1,2,3);"
+				);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseStatement();
+	}
+
+	/**
+	 * Test the declaration of an annotation.
+	 */
+	public void test18()
+	{
+		StringReader sr = new StringReader(
+				"public @interface RequestForEnhancement { " +
+					"int id();" +
+					"String synopsis();"+
+					"String engineer();  default \"[unassigned]\"; "+
+					"String date();      default \"[unimplemented]\"; "+
+				"}"
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseTypeDef();
+	}
+	
+		
+	/**
+	 * Test the use of an annotation.
+	 */
+	public void test19()
+	{
+		StringReader sr = new StringReader(
+				"public @RequestForEnhancement(" +
+					"id       = 2868724," +
+					"synopsis = \"Enable time-travel\","+
+					"engineer = \"Mr. Peabody\", "+
+					"date     = \"4/1/3007\""+
+				")"+
+				"static void travelThroughTime(Date destination) { } }"
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseClassBody();
+	}
+	
+	/**
+	 * Test the use of an annotation for a method.
+	 */
+	public void test20()
+	{
+		StringReader sr = new StringReader(
+				"@Test public static void m1() { } }"
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseClassBody();
+	}
+	
+	/**
+	 * Test the use of a package annotation
+	 */
+	public void test21()
+	{
+		StringReader sr = new StringReader(
+				"@Test.RequestForEnhancement req;"
+		);
+		InfoParser ip = new InfoParser(sr);
+		ip.parseStatement();
+	}
+	
+	
+
+	public void test22(){
+	    StringReader sr = new StringReader(
+	            "@Expression(\"execution(* com.mypackage.Target.*(..))\") "+
+	            "Pointcut pc1; "
+	    );
+	    InfoParser ip = new InfoParser(sr);
+        ip.parseStatement();
+
+	}
+	
+    public void test23(){
+        StringReader sr = new StringReader(
+                "@Expression(\"execution(* com.mypackage.Target.*(..))\") "+
+                "volatile Pointcut pc1; "
+        );
+        InfoParser ip = new InfoParser(sr);
+        ip.parseStatement();
+
+    }
+	
+}
---
+++ bluej/test/src/bluej/parser/NodeTreeTest.java
@@ -0,0 +1,121 @@
+package bluej.parser;
+
+import org.syntax.jedit.tokenmarker.Token;
+
+import junit.framework.TestCase;
+import bluej.parser.NodeTree.NodeAndPosition;
+
+public class NodeTreeTest extends TestCase
+{
+    private NodeTree nt;
+    private ParsedNode pn1;
+    private ParsedNode pn2;
+    private ParsedNode pn3;
+    private ParsedNode pn4;
+    private ParsedNode pn5;
+    
+    
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    protected void setUp()
+    {
+        nt = new NodeTree();
+        pn1 = new ColourNode(null, Token.KEYWORD1);
+        pn2 = new ColourNode(null, Token.KEYWORD1);
+        pn3 = new ColourNode(null, Token.KEYWORD1);
+        pn4 = new ColourNode(null, Token.KEYWORD1);
+        pn5 = new ColourNode(null, Token.KEYWORD1);
+
+        nt.insertNode(pn1, 0, 10);
+        nt.insertNode(pn2, 20, 10);
+        nt.insertNode(pn3, 40, 10);
+        nt.insertNode(pn4, 60, 10);
+        nt.insertNode(pn5, 80, 10);
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    protected void tearDown()
+    {
+    }
+
+    public void testBasic()
+    {        
+        NodeAndPosition np = nt.findNode(5);
+        assertNotNull(np);
+        assertTrue(np.getNode() == pn1);
+        assertTrue(np.getPosition() == 0);
+
+        np = nt.findNode(25);
+        assertNotNull(np);
+        assertTrue(np.getNode() == pn2);
+        assertTrue(np.getPosition() == 20);
+
+        np = nt.findNode(45);
+        assertNotNull(np);
+        assertTrue(np.getNode() == pn3);
+        assertTrue(np.getPosition() == 40);
+
+        np = nt.findNode(65);
+        assertNotNull(np);
+        assertTrue(np.getNode() == pn4);
+        assertTrue(np.getPosition() == 60);
+
+        np = nt.findNode(85);
+        assertNotNull(np);
+        assertTrue(np.getNode() == pn5);
+        assertTrue(np.getPosition() == 80);
+    }
+    
+    public void testRemoval()
+    {
+        NodeAndPosition np = nt.findNode(65);
+        np.getNode().getContainingNodeTree().remove();
+        
+        np = nt.findNode(5);
+        assertNotNull(np);
+        assertTrue(np.getNode() == pn1);
+        assertTrue(np.getPosition() == 0);
+        
+        np = nt.findNode(65);
+        assertNull(np);
+        
+        np = nt.findNode(85);
+        assertNotNull(np);
+        assertTrue(np.getNode() == pn5);
+        assertTrue(np.getPosition() == 80);
+    }
+    
+    public void testRemoval2()
+    {
+        // Remove all nodes
+        NodeAndPosition np = nt.findNode(65);
+        np.getNode().getContainingNodeTree().remove();
+        np = nt.findNode(45);
+        np.getNode().getContainingNodeTree().remove();
+        np = nt.findNode(85);
+        np.getNode().getContainingNodeTree().remove();
+        np = nt.findNode(25);
+        np.getNode().getContainingNodeTree().remove();
+        np = nt.findNode(5);
+        np.getNode().getContainingNodeTree().remove();
+
+        np = nt.findNode(5);
+        assertNull(np);
+        np = nt.findNode(25);
+        assertNull(np);
+        np = nt.findNode(45);
+        assertNull(np);
+        np = nt.findNode(65);
+        assertNull(np);
+        np = nt.findNode(85);
+        assertNull(np);
+    }
+    
+}
