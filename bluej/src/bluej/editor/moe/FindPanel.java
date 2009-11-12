56f0c997c54f40cb2dcd73f1ca9c564b26fc866d
marionz <marionz@bluej.org>
2009-11-12 04:08:42 +0000

    Added license info Removed unused method Changed some documentation
    (from subversion r6828)

--- bluej/src/bluej/editor/moe/FindPanel.java
+++ bluej/src/bluej/editor/moe/FindPanel.java
@@ -1,3 +1,24 @@
+/*
+ This file is part of the BlueJ program. 
+ Copyright (C) 1999-2009  Michael Kolling and John Rosenberg 
+ 
+ This program is free software; you can redistribute it and/or 
+ modify it under the terms of the GNU General Public License 
+ as published by the Free Software Foundation; either version 2 
+ of the License, or (at your option) any later version. 
+ 
+ This program is distributed in the hope that it will be useful, 
+ but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ GNU General Public License for more details. 
+ 
+ You should have received a copy of the GNU General Public License 
+ along with this program; if not, write to the Free Software 
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. 
+ 
+ This file is subject to the Classpath exception as provided in the  
+ LICENSE.txt file that accompanied this code.
+ */
 package bluej.editor.moe;
 
 import java.awt.BorderLayout;
@@ -360,46 +381,13 @@ public class FindPanel extends JPanel implements ActionListener, KeyListener {
     }
 
     /**
-     * Replace all instances of the search String with a replacement.
+     *  HighlightAll instances of the search String with a replacement.
      * -check for valid search criteria
      * - TODO: get initial cursor pos
      * -start at beginning
      * -do initial find
      * -replace until not found, no wrapping!
-     * -print out number of replacements (?)
-     * -TODO: return cursor/caret to original place
-     */
-    private void replaceAll(String replaceString)
-    {
-        String searchString = getSearchString();
-
-        int count = 0;
-
-        while(editor.doFind(searchString, false, false, false)) {
-            editor.insertText(smartFormat(editor.getSelectedText(), replaceString), true);
-            count++;
-        }
-
-        if(count > 0)
-            //editor.writeMessage("Replaced " + count + " instances of " + searchString);
-            editor.writeMessage(Config.getString("editor.replaceAll.replaced") +
-                    count + Config.getString("editor.replaceAll.intancesOf") + 
-                    searchString);
-        else
-            //editor.writeMessage("String " + searchString + " not found. Nothing replaced.");
-            editor.writeMessage(Config.getString("editor.replaceAll.string") + 
-                    searchString + Config.getString("editor.replaceAll.notFoundNothingReplaced"));
-    }
-
-
-    /**
-     * Replace all instances of the search String with a replacement.
-     * -check for valid search criteria
-     * - TODO: get initial cursor pos
-     * -start at beginning
-     * -do initial find
-     * -replace until not found, no wrapping!
-     * -print out number of replacements (?)
+     * -print out number of highlights (?)
      * -TODO: return cursor/caret to original place
      */
     private void highlightAll(boolean ignoreCase, boolean wholeWord, boolean wrap, boolean next)
