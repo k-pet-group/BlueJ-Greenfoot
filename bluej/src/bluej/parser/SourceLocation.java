7af468a8142135ee6b523caa492ca11922f387ae
davmac <davmac@bluej.org>
2010-01-13 00:28:47 +0000

    Fix SourceLocation comments (lines/columns start from 0 not 1) and constructor check.
    (from subversion r6994)

--- bluej/src/bluej/parser/SourceLocation.java
+++ bluej/src/bluej/parser/SourceLocation.java
@@ -24,11 +24,9 @@ package bluej.parser;
 /**
  * A line/column location in a source file.
  *
- * Note that all line/column numbers start counting from
- * 1 (not 0).
+ * Note that all line/column numbers start counting from 0.
  *
  * @author  Andrew Patterson
- * @version $Id: SourceLocation.java 6215 2009-03-30 13:28:25Z polle $
  */
 public class SourceLocation
 {
@@ -37,8 +35,8 @@ public class SourceLocation
     
     public SourceLocation(int line, int column)
     {
-        if (line < 1 || column < 1)
-            throw new IllegalArgumentException("line/column numbers must be greater than 0");
+        if (line < 0 || column < 0)
+            throw new IllegalArgumentException("line/column numbers must be >= 0");
 
         this.line = line;
         this.column = column;
