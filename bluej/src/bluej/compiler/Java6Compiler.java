6f23fcb829c12e467f271d01e61919a1b4ddac04
marionz <marionz@bluej.org>
2010-02-04 03:34:08 +0000

    The new JavaCompiler is now using Diagnostics to retrieve its error and warning messages
    (from subversion r7093)

--- bluej/src/bluej/compiler/Java6Compiler.java
+++ bluej/src/bluej/compiler/Java6Compiler.java
@@ -27,6 +27,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.tools.Diagnostic;
+import javax.tools.DiagnosticCollector;
 import javax.tools.JavaCompiler;
 import javax.tools.JavaFileObject;
 import javax.tools.StandardJavaFileManager;
@@ -58,11 +60,12 @@ public class Java6Compiler extends Compiler {
             boolean internal) {
         boolean result = true;
         JavaCompiler jc = ToolProvider.getSystemJavaCompiler();
-        JavacErrorWriter output = new JavacErrorWriter(internal);
         String[] options = new String[]{}; 
+        DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
         try
-        {            
-            StandardJavaFileManager sjfm = jc.getStandardFileManager(null, null, null);
+        {  
+            //setup the filemanager
+            StandardJavaFileManager sjfm = jc.getStandardFileManager(diagnostics, null, null);
             List<File> pathList = new ArrayList<File>();
             List<File> outputList= new ArrayList<File>();
             outputList.add(getDestDir());
@@ -70,21 +73,21 @@ public class Java6Compiler extends Compiler {
             sjfm.setLocation(StandardLocation.SOURCE_PATH, pathList);
             sjfm.setLocation(StandardLocation.CLASS_PATH, pathList);
             sjfm.setLocation(StandardLocation.CLASS_OUTPUT, outputList);
-
+            //get the source files for compilation  
             Iterable<? extends JavaFileObject> compilationUnits1 =
                 sjfm.getJavaFileObjectsFromFiles(Arrays.asList(sources));
-
+            //add any options
             if(isDebug())
                 options[0]="-g";
-
             if(isDeprecation())
                 options[1]="-deprecation"; 
             List<String> optionsList=new ArrayList<String>();
             optionsList.addAll(Arrays.asList(options));
             addUserSpecifiedOptions(optionsList, COMPILER_OPTIONS);
+            //compile
+            jc.getTask(null, sjfm, diagnostics, optionsList, null, compilationUnits1).call();
+            sjfm.close();            
 
-            jc.getTask(output, sjfm, null, optionsList, null, compilationUnits1).call();
-            sjfm.close();
         }
         catch(IOException e)
         {
@@ -92,20 +95,39 @@ public class Java6Compiler extends Compiler {
             return false;
         }
 
-        if (output.hasError()) 
+        //Query diagnostics for error/warning messages
+        List<Diagnostic<? extends JavaFileObject>> diagnosticList = diagnostics.getDiagnostics();        
+        String src=null;
+        int pos=0;
+        String msg=null;
+        boolean error=false;
+        boolean warning=false;
+
+        if (diagnosticList!=null && diagnosticList.size()>0){
+            if (diagnosticList.get(0).getKind().equals(Diagnostic.Kind.ERROR))
+                error=true;
+            if (diagnosticList.get(0).getKind().equals(Diagnostic.Kind.WARNING)||
+                    diagnosticList.get(0).getKind().equals(Diagnostic.Kind.MANDATORY_WARNING))
+                warning=true;
+            src= ((Diagnostic<?>)diagnosticList.get(0)).getSource().toString();
+            pos= (int)((Diagnostic<?>)diagnosticList.get(0)).getLineNumber();
+            msg=((Diagnostic<?>)diagnosticList.get(0)).getMessage(null);
+            //the message is in this format 
+            //path:line number:message
+            //i.e includes the path and line number so need to strip that off
+            msg=msg.substring(msg.indexOf(':', src.length()+1)+1);
+        }
+       
+        // Handle compiler error messages 
+        if (error) 
         {
             result=false;
-            observer.errorMessage(output.getFilename(),
-                    output.getLineNo(),
-                    output.getMessage());
+            observer.errorMessage(src, pos, msg);
         }
-
         // Handle compiler warning messages        
-        if (output.hasWarnings()) 
+        if (warning) 
         {
-            observer.warningMessage(output.getFilename(),
-                    output.getLineNo(),
-                    output.getWarning());
+            observer.warningMessage(src, pos, msg);
         }
         return result;
     }
