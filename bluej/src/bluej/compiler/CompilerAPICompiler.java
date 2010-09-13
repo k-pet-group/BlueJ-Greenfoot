fac402f9c5d75c9b1cd963990d804d812903bfa3
marionz <marionz@bluej.org>
2010-09-13 03:49:50 +0000

    A cleanup and improvement of code Added a java 7 compiler method for handling error messages (which is incomplete obviously and will need to be enhanced when it is released)
    (from subversion r8302)

--- bluej/src/bluej/compiler/CompilerAPICompiler.java
+++ bluej/src/bluej/compiler/CompilerAPICompiler.java
@@ -35,6 +35,8 @@ import javax.tools.StandardJavaFileManager;
 import javax.tools.StandardLocation;
 import javax.tools.ToolProvider;
 
+import bluej.Config;
+
 /**
  * A compiler implementation using the Compiler API introduced in Java 6.
  * 
@@ -112,65 +114,71 @@ public class CompilerAPICompiler extends Compiler
 
         //Query diagnostics for error/warning messages
         List<Diagnostic<? extends JavaFileObject>> diagnosticList = diagnostics.getDiagnostics();        
-        String src=null;
-        int pos=0;
-        String msg=null;
-        boolean error=false;
-        boolean warning=false;
-        int diagnosticErrorPosition=-1;
-        int diagnosticWarningPosition=-1;
+        String src = null;
+        int pos = 0;
+        String msg = null;
+        boolean error = false;
+        boolean warning = false;
+        int diagnosticErrorPosition = -1;
+        int diagnosticWarningPosition = -1;
         //ensure an error is printed if there is one, else use the warning/s; note/s
         //(errors should have priority in the diagnostic list, but this is just in case not)
-        for (int i=0; i< diagnosticList.size(); i++){
+        for (int i = 0; i< diagnosticList.size(); i++){
             if (diagnosticList.get(i).getKind().equals(Diagnostic.Kind.ERROR))
             {
-                diagnosticErrorPosition=i;
-                error=true;
-                warning=false;
+                diagnosticErrorPosition = i;
+                error = true;
+                warning = false;
                 break;
             }
             if (diagnosticList.get(i).getKind().equals(Diagnostic.Kind.WARNING)||
                     diagnosticList.get(i).getKind().equals(Diagnostic.Kind.NOTE))
             {
-                warning=true;
+                warning = true;
                 //just to ensure the first instance of the warning position is recorded 
                 //(not the last position)
-                if (diagnosticWarningPosition==-1){
-                    diagnosticWarningPosition=i;
+                if (diagnosticWarningPosition == -1){
+                    diagnosticWarningPosition = i;
                 }
             }
         }
         //diagnosticErrorPosition can either be the warning/error
-        if (diagnosticErrorPosition<0)
-            diagnosticErrorPosition=diagnosticWarningPosition;
+        if (diagnosticErrorPosition < 0)
+        {
+            diagnosticErrorPosition = diagnosticWarningPosition;
+        }
         //set the necessary values
-        if (warning||error){
-            if (((Diagnostic<?>)diagnosticList.get(diagnosticErrorPosition)).getSource()!=null)
-                src= ((Diagnostic<?>)diagnosticList.get(diagnosticErrorPosition)).getSource().toString();
-            pos= (int)((Diagnostic<?>)diagnosticList.get(diagnosticErrorPosition)).getLineNumber();
-            
+        if (warning || error) 
+        {
+            Diagnostic<? extends JavaFileObject> diagnostic = diagnosticList.get(diagnosticErrorPosition);
+            if (diagnostic.getSource() != null)
+            {
+                src = diagnostic.getSource().toUri().getPath();       
+            }
+            pos = (int)diagnostic.getLineNumber();
+
             // Handle compiler error messages 
             if (error) 
             {
-                result=false;
-                msg=((Diagnostic<?>)diagnosticList.get(diagnosticErrorPosition)).getMessage(null);
-                msg=processMessage(src, pos, msg);
+                result = false;          
+                msg = processDiagnosticMessage(src, pos, diagnostic.getMessage(null));  
                 observer.errorMessage(src, pos, msg);
             }
             // Handle compiler warning messages  
             // If it is a warning message, need to get all the messages
             if (warning) 
             {
-                for (int i=diagnosticErrorPosition; i< diagnosticList.size(); i++){
+                for (int i = diagnosticErrorPosition; i < diagnosticList.size(); i++)
+                {
                     //'display unchecked warning messages' in the preferences dialog is unchecked
                     //therefore notes should not be displayed
                     //warnings can still be displayed
-                    if (internal && ((Diagnostic<?>)diagnosticList.get(i)).getKind().equals(Diagnostic.Kind.NOTE)){
+                    if (internal && diagnosticList.get(i).getKind().equals(Diagnostic.Kind.NOTE)){
                         continue;
                     }
                     else
                     {
-                        msg=((Diagnostic<?>)diagnosticList.get(i)).getMessage(null);
+                        msg = diagnosticList.get(i).getMessage(null);
                         observer.warningMessage(src, pos, msg);
                     }
                 }              
@@ -181,16 +189,46 @@ public class CompilerAPICompiler extends Compiler
 
     /**
      * @param  String msg representing the message retrieved from the diagnostic tool
-     * processMessage tidies up the message returned from the diagnostic tool
+     * processDiagnosticMessage tidies up the message returned from the diagnostic tool 
      * @return message String
      */
+    protected String processDiagnosticMessage(String src, int pos, String msg)
+    {
+        if (Config.isJava17())
+        {
+            int lastIndex = msg.lastIndexOf('\n');            
+            if (lastIndex == -1)
+            {
+                return msg;
+            }            
+            int index = 0; 
+            String rMsg = "";
+            while (index >= 0 ) 
+            {
+                lastIndex = Math.max(msg.indexOf('\n', index+1), msg.indexOf('\r', index+1));
+                if (lastIndex < 0){
+                    rMsg = rMsg + msg.substring(index, msg.length()).trim();
+                }
+                else {
+                    rMsg = rMsg + msg.substring(index, lastIndex).trim() +";\n";
+                }
+                index = msg.indexOf('\n', index + 1);
+            }  
+            return rMsg;
+        }
+        else {
+            return processMessage(src, pos, msg);
+        }
+    }
+
     protected String processMessage(String src, int pos, String msg)
     {
         // The message is in this format: 
         //   path and filename:line number:message
         // i.e includes the path and line number; so we need to strip that off
         String expected = src + ":" + pos + ": ";
-        if (! msg.startsWith(expected)) {
+        if (! msg.startsWith(expected)) 
+        {
             // Hmm, it's not a format we recgonize
             return src;
         }
@@ -198,18 +236,21 @@ public class CompilerAPICompiler extends Compiler
         String message = msg.substring(expected.length());
         if (message.contains("cannot resolve symbol")
                 || message.contains("cannot find symbol")
-                || message.contains("incompatible types")) {
+                || message.contains("incompatible types")) 
+        {
             // divide the message into lines so we can retrieve necessary values
             int index1, index2;
             String line2, line3;
             index1=message.indexOf('\n');
-            if (index1 == -1) {
+            if (index1 == -1) 
+            {
                 // We don't know how to handle this.
                 return msg;
             }
             index2=message.indexOf('\n',index1+1);
             //i.e there are only 2 lines not 3
-            if (index2 < index1) {
+            if (index2 < index1) 
+            {
                 line2 = message.substring(index1).trim();
                 line3 = "";
             }
@@ -222,7 +263,8 @@ public class CompilerAPICompiler extends Compiler
             //e.g incompatible types
             //found   : int
             //required: java.lang.String
-            if (line2.startsWith("found") && line2.indexOf(':') != -1) {
+            if (line2.startsWith("found") && line2.indexOf(':') != -1) 
+            {
                 message= message +" - found "+line2.substring(line2.indexOf(':')+2, line2.length());
             }
             if (line3.startsWith("required") && line3.indexOf(':') != -1) {
@@ -230,7 +272,8 @@ public class CompilerAPICompiler extends Compiler
             }
             //e.g cannot find symbol
             //symbol: class Persons
-            if (line2.startsWith("symbol") && line2.indexOf(':') != -1) {
+            if (line2.startsWith("symbol") && line2.indexOf(':') != -1) 
+            {
                 message= message +" - "+line2.substring(line2.indexOf(':')+2, line2.length());
             }
         }
