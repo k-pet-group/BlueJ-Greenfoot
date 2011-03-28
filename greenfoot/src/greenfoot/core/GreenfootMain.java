7a5f6eafd042ca3fe5e7a49f6156080ab52b8d7e
marionz <marionz@bluej.org>
2011-03-28 00:11:22 +0000

    Unfortunately, this does fix #314, but breaks something else. Reverting change.
    (from subversion r8772)

--- greenfoot/src/greenfoot/core/GreenfootMain.java
+++ greenfoot/src/greenfoot/core/GreenfootMain.java
@@ -73,7 +73,7 @@ import bluej.views.View;
  * but each will be in its own JVM so it is effectively a singleton.
  * 
  * @author Poul Henriksen <polle@mip.sdu.dk>
- * @version $Id: GreenfootMain.java 8749 2011-03-22 05:06:14Z marionz $
+ * @version $Id: GreenfootMain.java 8772 2011-03-28 00:11:22Z marionz $
  */
 public class GreenfootMain extends Thread implements CompileListener, RProjectListener
 {
@@ -297,6 +297,10 @@ public class GreenfootMain extends Thread implements CompileListener, RProjectLi
                 catch (PackageNotFoundException e) { }
             }
 
+            // if this is the dummy startup project, close it now.
+            if (frame.getProject() == null) {
+                project.close();
+            }
         }
     }
 
