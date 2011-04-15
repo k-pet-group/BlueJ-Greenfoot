92e616b43288ef4055292dc19a272a34a9466f79
davmac <davmac@bluej.org>
2011-04-15 09:13:21 +0000

    Add missing license header and class comment.
    (from subversion r8871)

--- bluej/src/bluej/editor/moe/PrintDialog.java
+++ bluej/src/bluej/editor/moe/PrintDialog.java
@@ -1,3 +1,24 @@
+/*
+ This file is part of the BlueJ program. 
+ Copyright (C) 2010  Michael Kolling and John Rosenberg 
+ 
+ This program is free software; you can redistribute it and/or 
+ modify it under the terms of the GNU General Public License 
+ as published by the Free Software Foundation; either version 2 
+ of the License, or (at your option) any later version. 
+ 
+ This program is distributed in the hope that it will be useful, 
+ but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ GNU General Public License for more details. 
+ 
+ You should have received a copy of the GNU General Public License 
+ along with this program; if not, write to the Free Software 
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. 
+ 
+ This file is subject to the Classpath exception as provided in the  
+ LICENSE.txt file that accompanied this code.
+ */
 package bluej.editor.moe;
 
 import java.awt.FlowLayout;
@@ -18,6 +39,9 @@ import bluej.Config;
 import bluej.utility.DialogManager;
 import bluej.utility.EscapeDialog;
 
+/**
+ * A print dialog with options specific to the editor.
+ */
 public class PrintDialog extends EscapeDialog
 {
     private boolean ok; // result: which button?
@@ -62,13 +86,16 @@ public class PrintDialog extends EscapeDialog
 
         JButton okButton = BlueJTheme.getOkButton();
         okButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent evt) { doOK(); }        		
+                public void actionPerformed(ActionEvent evt) { doOK(); }
         });
         
         JButton cancelButton = BlueJTheme.getCancelButton();
         cancelButton.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent evt) { doCancel(); }        		
-		});
+            public void actionPerformed(ActionEvent evt)
+            {
+                doCancel();
+            }
+        });
 
         DialogManager.addOKCancelButtons(buttonPanel, okButton, cancelButton);
 
