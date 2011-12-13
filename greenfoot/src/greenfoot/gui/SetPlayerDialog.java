10bb08e6138fe76b3229d5129b9f29c3fe3f273a
nccb <nccb@bluej.org>
2011-12-13 11:38:13 +0000

    Added missing licence text to some recently added classes, after some feckless programmer repeatedly failed to include it...
    (from subversion r9361)

--- greenfoot/src/greenfoot/gui/SetPlayerDialog.java
+++ greenfoot/src/greenfoot/gui/SetPlayerDialog.java
@@ -1,8 +1,26 @@
+/*
+ This file is part of the Greenfoot program. 
+ Copyright (C) 2011  Poul Henriksen and Michael Kolling 
+ 
+ This program is free software; you can redistribute it and/or 
+ modify it under the terms of the GNU General Public License 
+ as published by the Free Software Foundation; either version 2 
+ of the License, or (at your option) any later version. 
+ 
+ This program is distributed in the hope that it will be useful, 
+ but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ GNU General Public License for more details. 
+ 
+ You should have received a copy of the GNU General Public License 
+ along with this program; if not, write to the Free Software 
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. 
+ 
+ This file is subject to the Classpath exception as provided in the  
+ LICENSE.txt file that accompanied this code.
+ */
 package greenfoot.gui;
 
-import greenfoot.core.GPackage;
-import greenfoot.event.ValidityEvent;
-import greenfoot.event.ValidityListener;
 import greenfoot.util.GreenfootUtil;
 
 import java.awt.Color;
