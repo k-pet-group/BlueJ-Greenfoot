a768ccc7c9534ae647bbbe33d47f255a6df1b6c1
nccb <nccb@bluej.org>
2012-03-19 10:11:10 +0000

    Changed the Counter class to use Michael's version, then quickly added prefixing and horizontal scaling.
    (from subversion r9577)

--- greenfoot/common/Counter.java
+++ greenfoot/common/Counter.java
@@ -28,65 +28,96 @@ import java.awt.Color;
  * }
  * </pre>
  * 
- * @author Neil Brown 
+ * @author Neil Brown and Michael Kölling 
  * @version 1.0
  */
 public class Counter extends Actor
 {
-    private final int HEIGHT = 25;
-    private final Color FOREGROUND = Color.RED;
-    
-    private String prefix;
+    private static final Color transparent = new Color(0,0,0,0);
+    private GreenfootImage background;
     private int value;
-    
+    private int target;
+    private String prefix;
+
     /**
-     * Creates a new Counter.
+     * Create a new counter, initialised to 0.
      */
     public Counter()
     {
-        this("");
-    }
-    
-    /**
-     * Creates a new Counter that will use the given String as a prefix.
-     * 
-     * @param prefix What to write before the score, e.g. "Score: " or "Eaten: "
-     */
-    public Counter(String prefix)
-    {
-        this.prefix = prefix;
-        this.value = 0;
+        background = getImage();  // get image from class
+        value = 0;
+        target = 0;
+        prefix = "";
         updateImage();
     }
     
-    private void updateImage()
-    {
-        setImage(new GreenfootImage(prefix + value, HEIGHT, FOREGROUND, new java.awt.Color(0, true)));
-    }
-    
     /**
-     * Sets a new value for the counter.  The display will update to reflect the new value.
-     * 
-     * If the new value is longer (has more digits), the counter will keep its left edge in the same place.
+     * Animate the display to count up (or down) to the current target value.
      */
-    public void setValue(int newValue)
+    public void act() 
     {
-        value = newValue;
-        
-        // Keep the left-edge in the same place after the score changes:
-        int left = getX() - getImage().getWidth()/2;
-        updateImage();
-        if (getWorld() != null)
-        {
-            setLocation(getImage().getWidth()/2 + left, getY());
+        if (value < target) {
+            value++;
+            updateImage();
+        }
+        else if (value > target) {
+            value--;
+            updateImage();
         }
     }
-    
+
     /**
-     * Gets the current value of the counter 
+     * Add a new score to the current counter value.  This will animate
+     * the counter over consecutive frames until it reaches the new value.
+     */
+    public void add(int score)
+    {
+        target += score;
+    }
+
+    /**
+     * Return the current counter value.
      */
     public int getValue()
     {
         return value;
+    }
+
+    /**
+     * Set a new counter value.  This will not animate the counter.
+     */
+    public void setValue(int newValue)
+    {
+        target = newValue;
+        value = newValue;
+        updateImage();
+    }
+    
+    /**
+     * Sets a text prefix that should be displayed before
+     * the counter value (e.g. "Score: ").
+     */
+    public void setPrefix(String prefix)
+    {
+        this.prefix = prefix;
+        updateImage();
+    }
+
+    /**
+     * Update the image on screen to show the current value.
+     */
+    private void updateImage()
+    {
+        GreenfootImage image = new GreenfootImage(background);
+        GreenfootImage text = new GreenfootImage(prefix + value, 22, Color.BLACK, transparent);
+        
+        if (text.getWidth() > image.getWidth() - 20)
+        {
+            image.scale(text.getWidth() + 20, image.getHeight());
+        }
+        
+        image.drawImage(text, (image.getWidth()-text.getWidth())/2, 
+                        (image.getHeight()-text.getHeight())/2);
+        setImage(image);
     }
 }
