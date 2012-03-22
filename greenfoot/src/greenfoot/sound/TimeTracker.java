d2189ae9d25bb2adb8cf5accfb3236515d7bac42
davmac <davmac@bluej.org>
2012-03-22 13:25:43 +0000

    Add some javadoc, and convert tabs to spaces
    (from subversion r9606)

--- greenfoot/src/greenfoot/sound/TimeTracker.java
+++ greenfoot/src/greenfoot/sound/TimeTracker.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the Greenfoot program. 
- Copyright (C) 2005-2009  Poul Henriksen and Michael Kolling 
+ Copyright (C) 2005-2009,2012  Poul Henriksen and Michael Kolling 
  
  This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License 
@@ -21,65 +21,73 @@
  */
 package greenfoot.sound;
 
+/**
+ * A controllable timer, keeps track of time since a mark.
+ */
 public class TimeTracker 
 {
-	private long startTime;
-	private boolean tracking;
-	private long timeElapsed;
-	
-	
-	public void start() 
-	{
-		if(tracking) {
-			return;
-		}
-		startTime = System.currentTimeMillis();	
-		tracking = true;
+    private long startTime;
+    private boolean tracking;
+    private long timeElapsed;
+    
+    public void start() 
+    {
+        if(tracking) {
+            return;
+        }
+        startTime = System.currentTimeMillis();    
+        tracking = true;
+    }
+    
+    public void pause()
+    {
+        if(!tracking) {
+            return;
+        }
+        long timeSincestart = getTimeSinceStart();
+        timeElapsed += timeSincestart;
+        tracking = false;
+    }
 
-	}
-	
-	public void pause()
-	{
-		if(!tracking) {
-			return;
-		}
-		long timeSincestart = getTimeSinceStart();
-		timeElapsed += timeSincestart;
-		tracking = false;
-	}
-
-	public void reset()
-	{
-		startTime = 0;
-		tracking = false;
-		timeElapsed = 0;
-	}
-	
-	private long getTimeSinceStart()
-	{
-		if(tracking) {
-			return System.currentTimeMillis() - startTime;
-		}
-		else {
-			return 0;
-		}
-	}	
-	
-	public long getTimeTracked()
-	{
-		return timeElapsed + getTimeSinceStart();
-	}
-	
-	/**
-	 * Reset the time tracked to the given time.
-	 */
-	public void setTimeTracked(long newTime) 
-	{
-		if(tracking) {
-			startTime = System.currentTimeMillis() - newTime;
-			timeElapsed = 0;
-		} else {
-			timeElapsed = newTime;
-		}
-	}
+    /**
+     * Reset time to 0 and stop the timer.
+     */
+    public void reset()
+    {
+        startTime = 0;
+        tracking = false;
+        timeElapsed = 0;
+    }
+    
+    private long getTimeSinceStart()
+    {
+        if(tracking) {
+            return System.currentTimeMillis() - startTime;
+        }
+        else {
+            return 0;
+        }
+    }
+    
+    /**
+     * Get the time tracked, in milliseconds, since the mark.
+     */
+    public long getTimeTracked()
+    {
+        return timeElapsed + getTimeSinceStart();
+    }
+    
+    /**
+     * Reset the time tracked to the given time. The time tracked continues
+     * to count up if the TimeTracker is started.
+     */
+    public void setTimeTracked(long newTime) 
+    {
+        if(tracking) {
+            startTime = System.currentTimeMillis() - newTime;
+            timeElapsed = 0;
+        } else {
+            timeElapsed = newTime;
+        }
+    }
 }
