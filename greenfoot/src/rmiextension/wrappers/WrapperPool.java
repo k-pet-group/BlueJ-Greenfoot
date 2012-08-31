50e01c6b9889200d46472081b9348a8852f1aeb1
davmac <davmac@bluej.org>
2012-08-31 14:31:05 +0000

    Weak hash map values were hard-referencing their own keys, causing a memory leak. Fix it by inserting weak references as map values.
    (from subversion r9899)

--- greenfoot/src/rmiextension/wrappers/WrapperPool.java
+++ greenfoot/src/rmiextension/wrappers/WrapperPool.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the Greenfoot program. 
- Copyright (C) 2005-2009,2010  Poul Henriksen and Michael Kolling 
+ Copyright (C) 2005-2009,2010,2012  Poul Henriksen and Michael Kolling 
  
  This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License 
@@ -21,6 +21,7 @@
  */
 package rmiextension.wrappers;
 
+import java.lang.ref.WeakReference;
 import java.rmi.RemoteException;
 import java.util.WeakHashMap;
 
@@ -43,9 +44,11 @@ import bluej.extensions.BProject;
  * @author Poul Henriksen <polle@mip.sdu.dk>
  */
 public class WrapperPool
-{
+{    
     static private WrapperPool instance = new WrapperPool();
 
+    // Note, the value must generally be a weak reference to the wrapper, because
+    // the wrapper maintains a hard reference back to the key.
     private WeakHashMap<Object,Object> pool = new WeakHashMap<Object,Object>();
 
     private WrapperPool()
@@ -61,11 +64,18 @@ public class WrapperPool
     public synchronized RProjectImpl getWrapper(BProject wrapped)
         throws RemoteException
     {
-        RProjectImpl wrapper = (RProjectImpl) pool.get(wrapped);
+        RProjectImpl wrapper = null;
+        WeakReference<?> wrProj = (WeakReference<?>) pool.get(wrapped);
+        
+        if (wrProj != null) {
+            wrapper = (RProjectImpl) wrProj.get();
+        }
+        
         if (wrapper == null) {
             wrapper = new RProjectImpl(wrapped);
-            pool.put(wrapped, wrapper);
+            pool.put(wrapped, new WeakReference<RProjectImpl>(wrapper));
         }
+        
         return wrapper;
     }
 
@@ -75,10 +85,16 @@ public class WrapperPool
         if (wrapped == null) {
             return null;
         }
-        RPackage wrapper = (RPackage) pool.get(wrapped);
+        
+        RPackage wrapper = null;
+        WeakReference<?> wrPkg = (WeakReference<?>) pool.get(wrapped);
+        if (wrPkg != null) {
+            wrapper = (RPackage) wrPkg.get();
+        }
+        
         if (wrapper == null) {
             wrapper = new RPackageImpl(wrapped);
-            pool.put(wrapped, wrapper);
+            pool.put(wrapped, new WeakReference<RPackage>(wrapper));
         }
         return wrapper;
     }
@@ -89,10 +105,16 @@ public class WrapperPool
         if (wrapped == null) {
             return null;
         }
-        RClass wrapper = (RClass) pool.get(wrapped);
+        
+        RClass wrapper = null;
+        WeakReference<?> wrClass = (WeakReference<?>) pool.get(wrapped);
+        if (wrClass != null) {
+            wrapper = (RClass) wrClass.get();
+        }
+        
         if (wrapper == null) {
             wrapper = new RClassImpl(wrapped);
-            pool.put(wrapped, wrapper);
+            pool.put(wrapped, new WeakReference<RClass>(wrapper));
         }
         return wrapper;
     }
@@ -106,11 +128,18 @@ public class WrapperPool
         if (wrapped == null) {
             return null;
         }
-        RConstructor wrapper = (RConstructor) pool.get(wrapped);
+        
+        RConstructor wrapper = null;
+        WeakReference<?> wrCons = (WeakReference<?>) pool.get(wrapped);
+        if (wrCons != null) {
+            wrapper = (RConstructor) wrCons.get();
+        }
+        
         if (wrapper == null) {
             wrapper = new RConstructorImpl(wrapped);
-            pool.put(wrapped, wrapper);
+            pool.put(wrapped, new WeakReference<RConstructor>(wrapper));
         }
+        
         return wrapper;
     }
 
@@ -123,11 +152,18 @@ public class WrapperPool
         if (wrapped == null) {
             return null;
         }
-        RObject wrapper = (RObject) pool.get(wrapped);
+        
+        RObject wrapper = null;
+        WeakReference<?> wrObj = (WeakReference<?>) pool.get(wrapped);
+        if (wrObj != null) {
+            wrapper = (RObject) wrObj.get();
+        }
+
         if (wrapper == null) {
             wrapper = new RObjectImpl(wrapped);
-            pool.put(wrapped, wrapper);
+            pool.put(wrapped, new WeakReference<RObject>(wrapper));
         }
+        
         return wrapper;
     }
 
@@ -140,10 +176,16 @@ public class WrapperPool
         if (wrapped == null) {
             return null;
         }
-        RField wrapper = (RField) pool.get(wrapped);
+        
+        RField wrapper = null;
+        WeakReference<?> wrField = (WeakReference<?>) pool.get(wrapped);
+        if (wrField != null) {
+            wrapper = (RField) wrField.get();
+        }
+        
         if (wrapper == null) {
             wrapper = new RFieldImpl(wrapped);
-            pool.put(wrapped, wrapper);
+            pool.put(wrapped, new WeakReference<RField>(wrapper));
         }
         return wrapper;
     }
@@ -155,5 +197,4 @@ public class WrapperPool
     {
         pool.remove(wrapped);        
     }
-
 }
