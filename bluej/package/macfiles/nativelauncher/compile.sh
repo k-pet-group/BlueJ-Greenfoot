1bcbc07689f5059ad637337f9d4caf11f1814ed4
davmac <davmac@bluej.org>
2012-09-12 17:05:38 +0000

    Add source for a native launcher for Mac OS. This is based on the native launcher (JavaAppLauncher) included with Oracle's AppBundler, with some modifications to make it work a bit more like the old Apple launcher (making it more suitable for use with BlueJ/Greenfoot).
    (from subversion r9915)

---
+++ bluej/package/macfiles/nativelauncher/compile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# You need to download the "Command line tools for XCode" from developer.apple.com,
+# as well as XCode itself.
+#
+# JAVA_HOME should be set to something like:
+#     /Library/Java/JavaVirtualMachines/jdk1.7.0_07.jdk/Contents/Home
+
+gcc -I "$JAVA_HOME/include" -I "$JAVA_HOME/include/darwin" -o JavaAppLauncher \
+	-framework Cocoa -arch x86_64 \
+	-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk \
+	-mmacosx-version-min=10.7 \
+	-Wall \
+	main.m
