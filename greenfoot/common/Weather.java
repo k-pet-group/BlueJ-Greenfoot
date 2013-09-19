fe7b37d14388501361789fb78c86ae0f0d2cc131
amjad <amjad@bluej.org>
2013-09-19 12:16:57 +0000

    New Weather Helper class which doesn't need API key, and it is under GNU license.
    (from subversion r10872)

--- greenfoot/common/Weather.java
+++ greenfoot/common/Weather.java
@@ -1,130 +1,99 @@
 
-import weather.util.Locator;
 import weather.util.WeatherGrabber;
 
 /**
- * A helper class that fetch weather status from weather stations
- * for a specific location. 
- * 
- * <p>It uses "The Weather Underground LLC" API.
- * <img src="../images/Weather.png" width="175" height="24"/> 
- * 
- * <p>As it is based on service from online provider,
- * each individual user needs an API key in order to use this class.
- * 
- * <p>An API key can be obtained from: http://api.wunderground.com/api/ 
- * 
- * <p>Example of use:</p>
+ * A helper class that fetch weather status for a specific location
+ * <br> It uses "The Weather Underground Core" API. <br>
  * <pre>
  * class WeatherQuery
  * {
- *     private Weather weather = new Weather("your generated key");
+ *     private Weather weather = new Weather("Germany", "Berlin");
  *     
  *     public exampleMethod()
  *     {
- *         String localCondition   = weather.getLocalCondition();
- *         // Canterbury Coordinates: 51.2786 N, 1.0794 E
- *         String conditionAtPoint = weather.getCondition(51.2786, 1.0794);
- *         double temperature      = weather.getCelsiusTemperature("France", "Paris");
+ *         double temperature = weather.getTemperature();
+ *         String windDirection = weather.getWindDirection();
+ *         int humidity = weather.getHumidity();
+ *         
+ *         System.out.println("The current temperature in " + 
+ *             weather.getCity() + " is " + temperature + " degrees");
  *     }
  * }
  * </pre>
  * 
- * @author Amjad Altadmri
- * @version 1.0
+ * @author amjad
+ * @version 2.0
  */
 public class Weather 
 {
-    private String apiUrl = "http://api.wunderground.com/api/";
-    private static WeatherGrabber weatherGrabber;
-    
+    private WeatherGrabber weatherGrabber;
+
     /**
-     * Create an instance of the Weather class, using the specified
-     * API Key. You can get a key from:
-     * http://api.wunderground.com/api/ 
-     * 
-     * @param apiKey the unique individual user key
+     * construct an object to get local weather status.
      */
-    public Weather(String apiKey)
+    public Weather()
     {
-        weatherGrabber = WeatherGrabber.getInstance(apiKey, apiUrl);
+        weatherGrabber = new WeatherGrabber();
     }
-    
+
     /**
-     * Return the local weather condition.
-     * Location will be decided based on the device IP address.
+     * Construct an object to get weather status in a specific city.
      * 
-     * @return A string describes the weather condition
+     *  @param country  the country name
+     *  @param city     the city name
      */
-    public String getLocalCondition()
+    public Weather(String country, String city)
     {
-        return weatherGrabber.getCondition(Locator.getLocalLocation());
+        weatherGrabber = new WeatherGrabber(country, city);
     }
-    
+
     /**
-     * Return the weather condition for a specific city in a specific country.
-     * 
-     * @param country     The country name
-     * @param city         The city name
-     * 
-     * @return            A string describing the weather condition
+     * Fetch the current temperature in Celsius.
      */
-    public String getCondition(String country, String city)
-    {
-         return weatherGrabber.getCondition(Locator.getLocation(country, city));
-    }
-    
-    /**
-     * Return the weather condition for a specific location on earth
-     * described by the latitude and longitude values.
-     * 
-     * @param latitude        The latitude value of the location
-     * @param longitude     The longitude value of the location
-     * 
-     * @return                A string describing the weather condition
-     */
-    public String getCondition(double latitude, double longitude)
-    {
-         return weatherGrabber.getCondition(Locator.getLocation(latitude, longitude));
-    }
-    
-    
-    /**
-     * Return the local temperature in celsius.
-     * Location will be decided based on the device IP address.
-     * 
-     * @return The temperature in celsius. 
-     */
-    public double getLocalCelsiusTemperature()
+    public double getTemperature()
     {                            
-        return weatherGrabber.getTemperature(Locator.getLocalLocation());
+        return weatherGrabber.getDataset().getTemperature();
+    }
+
+    /**
+     * Fetch the current speed of the wind in km/h.
+     */
+    public double getWindSpeed() {
+        return weatherGrabber.getDataset().getWindSpeedKmh();
+    }
+
+    /**
+     * Fetch the direction of the wind.
+     */
+    public String getWindDirection() {
+        return weatherGrabber.getDataset().getWindDirection();
+    }
+
+    /**
+     * Fetch the current humidity.
+    */
+    public int getHumidity() {
+        return weatherGrabber.getDataset().getHumidity();
+    }
+
+    /**
+     * Fetch the current atmospheric air pressure in hectopascal (hPa).
+    */
+    public double getPressure() {
+        return weatherGrabber.getDataset().getPressurehPa();
     }
     
     /**
-     * Return the temperature in celsius for a specific city
-     * in a specific country.
-     * 
-     * @param country     The country name
-     * @param city         The city name
-     * 
-     * @return            The temperature in celsius.
-     */
-    public double getCelsiusTemperature(String country, String city)
-    {
-        return weatherGrabber.getTemperature(Locator.getLocation(country, city));
+     * get the city name.
+    */
+    public String getCity() {
+        return weatherGrabber.getCity();
     }
     
     /**
-     * Return the temperature in celsius for a specific location on earth
-     * described by the latitude and longitude values.
-     * 
-     * @param latitude        The latitude value of the location
-     * @param longitude     The longitude value of the location
-     * 
-     * @return                The temperature in celsius.
-     */
-    public double getCelsiusTemperature(double latitude, double longitude)
-    {
-        return weatherGrabber.getTemperature(Locator.getLocation(latitude, longitude));
+     * get the country name.
+    */
+    public String getCountry() {
+        return weatherGrabber.getCountry();
     }
 }
