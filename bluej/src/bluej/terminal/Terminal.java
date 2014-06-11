3e5fc19ae5b21dc4a07938e4273c55c296714c69
amjad <amjad@bluej.org>
2014-06-11 13:44:34 +0000

    Solving Ticket #511 Extra semi-colon in Terminal window
    (from subversion r11902)

--- bluej/src/bluej/terminal/Terminal.java
+++ bluej/src/bluej/terminal/Terminal.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the BlueJ program. 
- Copyright (C) 1999-2013  Michael Kolling and John Rosenberg 
+ Copyright (C) 1999-2013,2014  Michael Kolling and John Rosenberg 
  
  This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License 
@@ -348,12 +348,8 @@ public final class Terminal extends JFrame
             clear();
         }
         if(recordMethodCalls) {
-            if (isVoid) {
-                text.appendMethodCall(callString + ";\n");
-            }
-            else {
-                text.appendMethodCall(callString + "\n");
-            }
+            // If isVoid, it will have a ';' anyway.
+            text.appendMethodCall(callString + "\n");
         }
         newMethodCall = true;
     }
