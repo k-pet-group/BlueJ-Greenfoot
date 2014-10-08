bea09bdc48e486e262abe925123c33c11669e2a8
davmac <davmac@bluej.org>
2014-10-08 15:50:45 +0000

    Fix #534, printing source documents containing tabs fails
    (from subversion r12511)

--- bluej/src/bluej/editor/moe/MoePrinter.java
+++ bluej/src/bluej/editor/moe/MoePrinter.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the BlueJ program. 
- Copyright (C) 1999-2009,2011,2013  Michael Kolling and John Rosenberg 
+ Copyright (C) 1999-2009,2011,2013,2014  Michael Kolling and John Rosenberg 
  
  This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License 
@@ -357,7 +357,10 @@ public class MoePrinter
                     }
                     else {
                         TabExpander tab = Utility.makeTabExpander(lineSeg.toString(), tabSize, fontMetrics);
-                        x = Utilities.drawTabbedText(lineSeg,x,position,g,tab,offset);
+                        
+                        // Draw the text.
+                        // The final parameter, 'start offset', affects what is passed to the tab expander; we need it to be 0:
+                        x = Utilities.drawTabbedText(lineSeg, x, position, g, tab, 0);
                     }
                     offset += length;
                     tokens = tokens.next;
