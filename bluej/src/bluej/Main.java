d3632de5f7c400ba4c5d48bcd7b7fb7d7c9c92ce
fdlh <fdlh@bluej.org>
2015-01-05 12:02:23 +0000

    Added missing import for IOException
    (from subversion r12928)

--- bluej/src/bluej/Main.java
+++ bluej/src/bluej/Main.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the BlueJ program. 
- Copyright (C) 1999-2009,2010,2011,2012,2013,2014  Michael Kolling and John Rosenberg 
+ Copyright (C) 1999-2009,2010,2011,2012,2013,2014,2015  Michael Kolling and John Rosenberg 
  
  This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License 
@@ -28,6 +28,7 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.util.Properties;
 import java.util.UUID;
+import java.io.IOException;
 
 import com.apple.eawt.Application;
 import com.apple.eawt.AppEvent;
