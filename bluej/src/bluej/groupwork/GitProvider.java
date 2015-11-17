f3632a775c0153b9a4adbe50c010a9dda306de21
fdlh <fdlh@bluej.org>
2015-11-17 11:47:55 +0000

    -check of host's fingerprint key doesn't seem to be working properly. Disable it when using ssh connection.
    (from subversion r15024)

--- bluej/src/bluej/groupwork/GitProvider.java
+++ bluej/src/bluej/groupwork/GitProvider.java
@@ -23,12 +23,22 @@ package bluej.groupwork;
 
 import bluej.groupwork.git.GitRepository;
 import bluej.utility.Debug;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
 import java.io.File;
 import java.io.IOException;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.LsRemoteCommand;
+import org.eclipse.jgit.api.TransportConfigCallback;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.transport.JschConfigSessionFactory;
+import org.eclipse.jgit.transport.OpenSshConfig;
+import org.eclipse.jgit.transport.SshSessionFactory;
+import org.eclipse.jgit.transport.SshTransport;
+import org.eclipse.jgit.transport.Transport;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.eclipse.jgit.util.FS;
 
 /**
  * Teamwork provider for Git.
@@ -61,16 +71,44 @@ public class GitProvider implements TeamworkProvider {
     public TeamworkCommandResult checkConnection(TeamSettings settings) {
 
         try {
+            String gitUrl = makeGitUrl(settings);
 
             //perform a lsRemote on the remote git repo.
             LsRemoteCommand lsRemoteCommand = Git.lsRemoteRepository();
             UsernamePasswordCredentialsProvider cp = new UsernamePasswordCredentialsProvider(settings.getUserName(), settings.getPassword()); // set a configuration with username and password.
-            lsRemoteCommand.setRemote(makeGitUrl(settings)); //configure remote repository address.
+            lsRemoteCommand.setRemote(gitUrl); //configure remote repository address.
             lsRemoteCommand.setCredentialsProvider(cp); //associate the repository to the username and password.
             lsRemoteCommand.setTags(false); //disable refs/tags in reference results
             lsRemoteCommand.setHeads(false); //disable refs/heads in reference results
-            lsRemoteCommand.call(); //executes the lsRemote commnand.
 
+            //It seems that ssh host fingerprint check is not working properly. 
+            //Disable it in a ssh connection.
+            if (gitUrl.startsWith("ssh")) {
+                SshSessionFactory sshSessionFactory = new JschConfigSessionFactory() {
+                    @Override
+                    protected void configure(OpenSshConfig.Host host, Session sn) {
+                        java.util.Properties config = new java.util.Properties();
+                        config.put("StrictHostKeyChecking", "no");
+                        sn.setConfig(config);
+                    }
+
+                    @Override
+                    protected JSch createDefaultJSch(FS fs) throws JSchException {
+                        return super.createDefaultJSch(fs);
+                    }
+                };
+                
+                lsRemoteCommand.setTransportConfigCallback(new TransportConfigCallback() {
+                    @Override
+                    public void configure(Transport t) {
+                        SshTransport sshTransport = (SshTransport) t;
+                        sshTransport.setSshSessionFactory(sshSessionFactory);
+                    }
+
+                });
+            }
+            
+            lsRemoteCommand.call(); //executes the lsRemote commnand.
         } catch (GitAPIException ex) {
             return new TeamworkCommandError(ex.getMessage(), ex.getLocalizedMessage());
         } catch (UnsupportedSettingException ex) {
