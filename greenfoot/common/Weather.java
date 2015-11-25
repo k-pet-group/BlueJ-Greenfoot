03e7a0828bd07624ac5126413bbe02b6c08966cc
davmac <davmac@bluej.org>
2015-11-25 16:51:42 +0000

    The "Weather" class uses a GPLv3 library and so must be licensed under the GPLv3. Add a copyright notice to reflect this.
    (from subversion r15066)

--- greenfoot/common/Weather.java
+++ greenfoot/common/Weather.java
@@ -1,8 +1,26 @@
 import weather.util.WeatherGrabber;
 
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
- * A helper class that fetch weather status for a specific location
- * <br> It uses "The Weather Underground Core" API. <br>
+ * A helper class that fetch weather status for a specific location.
+ * It uses "The Weather Underground Core" API. This class is licensed
+ * under the terms of the GNU General Public License version 3.
+ * 
+ * <p>Example usage:</p>
  * <pre>
  * class WeatherQuery
  * {
