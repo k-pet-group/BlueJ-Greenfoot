31e238969c29ef5667b9c8f59548bdd8fbb61c55
amjad <amjad@bluej.org>
2016-01-12 16:37:29 +0000

    Adds hasCode to InfixExpression class.
    (from subversion r15262)

--- bluej/src/bluej/stride/framedjava/slots/InfixExpression.java
+++ bluej/src/bluej/stride/framedjava/slots/InfixExpression.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the BlueJ program. 
- Copyright (C) 2014,2015 Michael Kölling and John Rosenberg 
+ Copyright (C) 2014,2015,2016 Michael Kölling and John Rosenberg
  
  This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License 
@@ -3045,6 +3045,13 @@ class InfixExpression implements TextFieldDelegate<ExpressionSlotField>
                 return false;
             return true;
         }
+
+        @Override
+        public int hashCode()
+        {
+            return startIndex % 31;
+        }
+
         // For debugging:
         @Override
         public String toString()
