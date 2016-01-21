ca05bdc7b235e33dd5859b5a908bb1cc7ea4a459
nccb <nccb@bluej.org>
2016-01-21 12:44:33 +0000

    Added recording identifier panel back into preferences panel if in Greenfoot but in a research build
    (from subversion r15327)

--- bluej/src/bluej/prefmgr/MiscPrefPanel.java
+++ bluej/src/bluej/prefmgr/MiscPrefPanel.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the BlueJ program. 
- Copyright (C) 1999-2009,2011,2012,2013  Michael Kolling and John Rosenberg 
+ Copyright (C) 1999-2009,2011,2012,2013,2016  Michael Kolling and John Rosenberg
  
  This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License 
@@ -40,6 +40,7 @@ import javax.swing.JTextField;
 import javax.swing.KeyStroke;
 
 import bluej.BlueJTheme;
+import bluej.Boot;
 import bluej.Config;
 import bluej.collect.DataCollector;
 import bluej.pkgmgr.PkgMgrFrame;
@@ -88,6 +89,10 @@ public class MiscPrefPanel extends JPanel
         
         if (Config.isGreenfoot()) {
             box.add(makePlayerNamePanel());
+            if (Boot.isTrialRecording())
+            {
+                box.add(makeDataCollectionPanel());
+            }
         }
         else {
             box.add(makeVMPanel());
