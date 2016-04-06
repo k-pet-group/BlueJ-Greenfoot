9e9bece3efd58a1f7209e80c20efd688ffbbab2a
nccb <nccb@bluej.org>
2016-04-06 12:32:13 +0000

    Added license text to the rest of the BlueJ classes which were missing it.
    (from subversion r15679)

--- bluej/src/bluej/groupwork/TeamworkCommandUnsupportedSetting.java
+++ bluej/src/bluej/groupwork/TeamworkCommandUnsupportedSetting.java
@@ -1,3 +1,24 @@
+/*
+ This file is part of the BlueJ program. 
+ Copyright (C) 2010,2016 Michael Kölling and John Rosenberg 
+ 
+ This program is free software; you can redistribute it and/or 
+ modify it under the terms of the GNU General Public License 
+ as published by the Free Software Foundation; either version 2 
+ of the License, or (at your option) any later version. 
+ 
+ This program is distributed in the hope that it will be useful, 
+ but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ GNU General Public License for more details. 
+ 
+ You should have received a copy of the GNU General Public License 
+ along with this program; if not, write to the Free Software 
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. 
+ 
+ This file is subject to the Classpath exception as provided in the  
+ LICENSE.txt file that accompanied this code.
+ */
 package bluej.groupwork;
 
 /**
@@ -5,23 +26,23 @@ package bluej.groupwork;
  */
 public class TeamworkCommandUnsupportedSetting extends TeamworkCommandResult
 {
-	private String message;
+    private String message;
 
-	public TeamworkCommandUnsupportedSetting(String message)
-	{
-		this.message = message;
-	}
+    public TeamworkCommandUnsupportedSetting(String message)
+    {
+        this.message = message;
+    }
 
-	@Override
-	public String getErrorMessage()
-	{
-		return message;
-	}
+    @Override
+    public String getErrorMessage()
+    {
+        return message;
+    }
 
-	@Override
-	public boolean isError()
-	{
-		return true;
-	}
-	
+    @Override
+    public boolean isError()
+    {
+        return true;
+    }
+    
 }
