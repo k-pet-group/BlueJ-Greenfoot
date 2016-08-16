5e21f1be7107c6a4130a4cff3ec43a441af0f98e
nccb <nccb@bluej.org>
2016-08-16 13:42:02 +0000

    Changed the list of child projects in the open failure dialog to be a list with a single open button, rather than a grid of items each with their own open button.
    (from subversion r16361)

--- bluej/src/bluej/utility/NotAProjectDialog.java
+++ bluej/src/bluej/utility/NotAProjectDialog.java
@@ -23,16 +23,22 @@ package bluej.utility;
 
 import bluej.Config;
 import bluej.utility.javafx.JavaFXUtil;
-import javafx.geometry.Pos;
+
+import javafx.collections.FXCollections;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.cell.TextFieldListCell;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Window;
+import javafx.util.StringConverter;
+
 import threadchecker.OnThread;
 import threadchecker.Tag;
 
@@ -46,6 +52,9 @@ import java.util.List;
 @OnThread(Tag.FXPlatform)// package-visible
 class NotAProjectDialog
 {
+    private static final ButtonType OPEN_BUTTON = ButtonType.NEXT;
+    private final ListView<File> subDirList;
+
     private static enum Choice { CANCEL, CHOOSE_AGAIN, IMPORT, SELECTED_FILE }
     private static class ChoiceAndFile
     {
@@ -70,38 +79,63 @@ class NotAProjectDialog
         final String labelRoot = "notAProject." + (Config.isGreenfoot() ? "greenfoot" : "bluej");
         dialog.setTitle(Config.getString(labelRoot + ".title"));
         dialog.getDialogPane().getButtonTypes().setAll(ButtonType.CANCEL, ButtonType.OK);
-        ((Button)dialog.getDialogPane().lookupButton(ButtonType.OK)).setText(Config.getString(labelRoot + ".button"));
         Config.addDialogStylesheets(dialog.getDialogPane());
         VBox content = new VBox(new Label(Config.getString(labelRoot + ".message") + "\n    " + original.getAbsolutePath()));
         JavaFXUtil.addStyleClass(content, "not-a-project");
         content.setFillWidth(true);
         if (possibilities != null && !possibilities.isEmpty())
         {
+            dialog.getDialogPane().getButtonTypes().add(OPEN_BUTTON);
+            Button openButton = (Button) dialog.getDialogPane().lookupButton(OPEN_BUTTON);
+            openButton.setText(Config.getString(labelRoot + ".subDirButton"));
             content.getChildren().add(new Label(Config.getString(labelRoot + ".subDirs")));
-            GridPane subDirPanel = new GridPane();
-            JavaFXUtil.addStyleClass(subDirPanel, "subDirs");
-            int row = 0;
-            for (File f : possibilities)
-            {
-                subDirPanel.add(new Label(f.getAbsolutePath()), 0, row);
-                Button button = new Button(Config.getString(labelRoot + ".subDirButton"));
-                button.setOnAction(e -> {
-                    dialog.setResult(new ChoiceAndFile(Choice.SELECTED_FILE, f));
-                    dialog.close();
+            subDirList = new ListView<>(FXCollections.observableArrayList(possibilities));
+            subDirList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+            subDirList.setEditable(false);
+            subDirList.setCellFactory(v -> {
+                ListCell<File> cell = new TextFieldListCell<>(new StringConverter<File>()
+                {
+                    @Override
+                    public String toString(File object)
+                    {
+                        return object.getAbsolutePath();
+                    }
+
+                    @Override
+                    public File fromString(String string)
+                    {
+                        throw new UnsupportedOperationException();
+                    }
                 });
-                subDirPanel.add(button, 1, row);
-                row += 1;
-            }
-            subDirPanel.setAlignment(Pos.CENTER);
-            content.getChildren().add(new BorderPane(subDirPanel));
+                cell.setOnMouseClicked(e -> {
+                    if (e.getClickCount() == 2 && e.getButton() == MouseButton.PRIMARY)
+                    {
+                        subDirList.getSelectionModel().select(cell.getItem());
+                        openButton.fire();
+                    }
+                });
+                return cell;
+            });
+            // From http://stackoverflow.com/questions/17429508/how-do-you-get-javafx-listview-to-be-the-height-of-its-items
+            subDirList.prefHeightProperty().set(possibilities.size() * 26 + 20);
+
+            subDirList.setMaxHeight(300.0);
+            openButton.disableProperty().bind(subDirList.getSelectionModel().selectedItemProperty().isNull());
+            JavaFXUtil.addStyleClass(subDirList, "subDirs");
+            content.getChildren().add(subDirList);
         }
+        else
+            subDirList = null;
+        ((Button)dialog.getDialogPane().lookupButton(ButtonType.OK)).setText(Config.getString(labelRoot + ".button"));
         dialog.getDialogPane().setContent(content);
 
         dialog.setResultConverter(button -> {
             if (button == ButtonType.OK)
                 return new ChoiceAndFile(Choice.CHOOSE_AGAIN, null);
-            else if (button == ButtonType.NO)
-                return new ChoiceAndFile(Choice.IMPORT, original);
+            //else if (button == ButtonType.NO)
+                //return new ChoiceAndFile(Choice.IMPORT, original);
+            else if (button == OPEN_BUTTON)
+                return new ChoiceAndFile(Choice.SELECTED_FILE, subDirList.getSelectionModel().getSelectedItem());
             else if (button == ButtonType.CANCEL)
                 return new ChoiceAndFile(Choice.CANCEL, null);
             else
