c89a513f5438b79ad85bc0b81769b62ffd6d8b8f
amjad <amjad@bluej.org>
2017-02-06 10:16:35 +0000

    Fixes BLUEJ-752: Undo with formal parameters in methods definition not always right. It turns out that when the user type a character in a parameter name slot, the new UndoRedo state is recorded even before checking what is this character. In many cases, including comma, this character doesn't belong to the slot, but it adds a new formal parameter. This is when the state should be recorded.
    (from subversion r17119)

--- bluej/src/bluej/stride/slots/FormalParameters.java
+++ bluej/src/bluej/stride/slots/FormalParameters.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the BlueJ program. 
- Copyright (C) 2014,2015,2016 Michael Kölling and John Rosenberg 
+ Copyright (C) 2014,2015,2016,2017 Michael Kölling and John Rosenberg
  
  This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License 
@@ -177,7 +177,7 @@ public class FormalParameters
                 if (newValue.contains(",")) {
                     FormalParameter newFormal = addNewAfter(findFormal(slot));
                     if (Platform.isFxApplicationThread())
-                        JavaFXUtil.runNowOrLater(() -> newFormal.requestFocus(Focus.LEFT));
+                        JavaFXUtil.runPlatformLater(() -> newFormal.requestFocus(Focus.LEFT));
                     return false;
                 }
 
