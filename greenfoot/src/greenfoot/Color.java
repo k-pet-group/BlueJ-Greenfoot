cb66875011217917020f1f59ae5d21f193731233
davmac <davmac@bluej.org>
2017-02-09 23:03:42 +0000

    Update copyright year
    (from subversion r17142)

--- greenfoot/src/greenfoot/Color.java
+++ greenfoot/src/greenfoot/Color.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the Greenfoot program. 
- Copyright (C) 2016  Poul Henriksen and Michael Kolling
+ Copyright (C) 2016,2017  Poul Henriksen and Michael Kolling
  
  This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License 
