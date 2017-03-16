e8a85928869fb6bcb98a9c43e613b978dcd45577
amjad <amjad@bluej.org>
2017-03-16 13:33:58 +0000

    Changes the return type of createParameterPanel from HangingFlowPane to Pane, so that we have more freedom in controlling the layout of different dialogs taht contain them. Part of BLUEJ-547.
    (from subversion r17332)

--- bluej/src/bluej/debugmgr/CallDialog.java
+++ bluej/src/bluej/debugmgr/CallDialog.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the BlueJ program. 
- Copyright (C) 1999-2015,2016  Michael Kolling and John Rosenberg
+ Copyright (C) 1999-2015,2016,2017  Michael Kolling and John Rosenberg
  
  This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License 
@@ -205,8 +205,7 @@ public abstract class CallDialog extends Dialog<Void>
      * @param parameterList A list containing the components for the parameter panel
      * @return
      */
-    protected HangingFlowPane createParameterPanel(String startString, String endString,
-            ParameterList parameterList)
+    protected Pane createParameterPanel(String startString, String endString, ParameterList parameterList)
     {
         ObservableList<ObservableList<? extends Node>> contents = FXCollections.observableArrayList();
         
--- bluej/src/bluej/debugmgr/ConstructorDialog.java
+++ bluej/src/bluej/debugmgr/ConstructorDialog.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the BlueJ program. 
- Copyright (C) 2010,2013,2014,2015,2016  Michael Kolling and John Rosenberg
+ Copyright (C) 2010,2013,2014,2015,2016,2017  Michael Kolling and John Rosenberg
  
  This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License 
@@ -146,7 +146,7 @@ public class ConstructorDialog extends CallDialog
     /**
      * Creates a panel of type parameters for a new object
      */
-    private HangingFlowPane createTypeParameterPanel(String prefix)
+    private Pane createTypeParameterPanel(String prefix)
     {
         TypeParamView formalTypeParams[] = getFormalTypeParams(constructor);
 
