a802a4842ba8dfd28559018ba3f33b87f1dea3a4
amjad <amjad@bluej.org>
2017-05-09 18:10:09 +0100

    Adds the FX Abstract Action, written by Neil on the other branch, to replace the Swing Action class.

---
+++ bluej/src/bluej/utility/javafx/FXAbstractAction.java
@@ -0,0 +1,157 @@
+/*
+ This file is part of the BlueJ program.
+ Copyright (C) 1999-2009,2015,2016,2017  Michael Kolling and John Rosenberg
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+ This file is subject to the Classpath exception as provided in the
+ LICENSE.txt file that accompanied this code.
+ */
+package bluej.utility.javafx;
+
+import javafx.beans.binding.BooleanExpression;
+import javafx.beans.binding.ObjectBinding;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.KeyCombination;
+
+import threadchecker.OnThread;
+import threadchecker.Tag;
+
+
+/**
+ * An FX Abstract Action to replace the Swing Action class
+ *
+ * @author NCCB
+ */
+@OnThread(Tag.FXPlatform)
+public abstract class FXAbstractAction
+{
+    private final String name;
+    private boolean hasMenuItem = false;
+    private final BooleanProperty unavailable = new SimpleBooleanProperty(false);
+    private final BooleanProperty disabled = new SimpleBooleanProperty(false);
+    private /*final*/ ObjectBinding<KeyCombination> accelerator;
+    /*private final Category category;*/
+
+    public FXAbstractAction(String name/*, Category category*/)
+    {
+        this.name = name;
+//        this.category = category;
+//        this.accelerator = Bindings.createObjectBinding(() -> {
+//            return keymap.entrySet().stream().filter(e -> e.getValue().equals(this)).map(e -> e.getKey()).findFirst().orElse(null);
+//        }, keymap);
+    }
+
+    public abstract void actionPerformed();
+
+    public FXAbstractAction bindEnabled(BooleanExpression enabled)
+    {
+        if (enabled != null)
+            disabled.bind(enabled.not());
+        return this;
+    }
+
+    public void setEnabled(boolean enabled)
+    {
+        if (disabled.isBound())
+            disabled.unbind();
+        disabled.set(!enabled);
+    }
+
+    /**
+     * There's two ways in which actions become disabled.  One is that their inherent
+     * state in the editor changes, e.g. no undo if you haven't made any changes.
+     * The other is that they become what we call unavailable, which happens when
+     * the documentation view is showing, for example.  So their GUI enabled status
+     * is actually the conjunction of being enabled and available.
+     */
+    public void setAvailable(boolean available)
+    {
+        unavailable.set(!available);
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public Button makeButton()
+    {
+        Button button = new Button(name);
+        button.disableProperty().bind(disabled.or(unavailable));
+        button.setOnAction(e -> actionPerformed());
+        return button;
+    }
+
+    /**
+     * Note: calling this method indicates that the item will have
+     * a menu item with the accelerator available for use.  Actions with a menu item
+     * don't set a separate entry in the key map.  So don't call this
+     * unless you're actually going to have the menu item available on the menu.
+     */
+    public MenuItem makeMenuItem()
+    {
+        MenuItem menuItem = makeContextMenuItem();
+        hasMenuItem = true;
+        menuItem.acceleratorProperty().bind(accelerator);
+        return menuItem;
+    }
+
+    /**
+     * Makes a MenuItem which will run this action, but without an accelerator.
+     */
+    public MenuItem makeContextMenuItem()
+    {
+        MenuItem menuItem = new MenuItem(name);
+        menuItem.disableProperty().bind(disabled.or(unavailable));
+        menuItem.setOnAction(e -> actionPerformed());
+        return menuItem;
+    }
+
+    public String toString()
+    {
+        return name;
+    }
+
+    /*public Category getCategory()
+    {
+        return category;
+    }*/
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FXAbstractAction that = (FXAbstractAction) o;
+
+        return name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return name.hashCode();
+    }
+
+    public boolean hasMenuItem()
+    {
+        return hasMenuItem;
+    }
+}
