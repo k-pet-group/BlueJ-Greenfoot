k-pet-group/BlueJ-Greenfoot

commit a8ee3be875909b874a31a7d18b1bb71e2403bfe7

Made the scroll bar of the documentation pane work in Java/Stride code completion.
Fixes BLUEJ-883

bluej/src/bluej/stride/slots/SuggestionList.java
@@ -467,7 +467,6 @@ public class SuggestionList
         docPane.setMinWidth(400.0);
         docPane.setMaxHeight(300.0);
         docPane.setBackground(null);
-        docPane.setMouseTransparent(true);
         
         BorderPane listAndDocBorderPane = new BorderPane();
         JavaFXUtil.addStyleClass(listAndDocBorderPane, "suggestion-top-level");
@@ -608,7 +607,12 @@ public class SuggestionList
             }
             e.consume();
         });
-        listBox.addEventFilter(KeyEvent.KEY_PRESSED, e -> {
+        // Add listener to listAndDocBorderPane not listBox, so that
+        // keypresses when the doc pane scroll is focused are still used
+        // to move the list, not the scroll bar.  Otherwise, once you click on
+        // the doc scroll bar, there's no obvious way to make up/down change
+        // back to scrolling the list (which is what the user expects in this window):
+        listAndDocBorderPane.addEventFilter(KeyEvent.KEY_PRESSED, e -> {
             switch (e.getCode())
             {
                 case UP:

commit 71187cc5face187a8c6b607c2ca15d12e9b0e8a5

Fixed an issue where Ctrl-Space twice in Stride would not show all suggestions, as it is meant to.
This was caused by commit a8ee3be875909b874a31a7d18b1bb71e2403bfe7 (which fixed BLUEJ-883), which added a consuming key listener on a higher component in the hierarchy, thus preventing the keypress reaching the pre-existing listener.  The listeners have now been combined (on the higher element in the hierarchy).  This is not actually in any release, as that commit was since the last release, 4.1.0.

bluej/src/bluej/stride/slots/SuggestionList.java
@@ -570,24 +570,8 @@ public class SuggestionList
                 });
             }
         });
-        
-        // On Mac, we have to check for Ctrl-Space in KEY_PRESSED, not KEY_TYPED:
-        listBox.addEventFilter(KeyEvent.KEY_PRESSED, e -> {
-            if (e.getCode() == KeyCode.SPACE && e.isControlDown())
-            {
-                // Consume either way, because otherwise we get an invalid character:
-                e.consume();
 
-                if (shownState.get() == SuggestionShown.COMMON)
-                {
-                    shownState.set(SuggestionShown.RARE);
-                    calculateEligible(lastPrefix, lastAllowSimilar, false);
-                    updateVisual(lastPrefix);
-                }
-            }
-        });
-
-        listBox.addEventFilter(KeyEvent.KEY_TYPED, e -> {
+        listAndDocBorderPane.addEventFilter(KeyEvent.KEY_TYPED, e -> {
             if (e.getCharacter().equals(" ") && e.isControlDown())
             {
                 if (shownState.get() == SuggestionShown.COMMON)
@@ -633,6 +617,22 @@ public class SuggestionList
                 case END:
                     end();
                     break;
+                case SPACE:
+                    // On Mac, we have to check for Ctrl-Space in KEY_PRESSED, not KEY_TYPED:
+                    if (e.isControlDown())
+                    {
+                        // Consume either way, because otherwise we get an invalid character:
+                        e.consume();
+
+                        if (shownState.get() == SuggestionShown.COMMON)
+                        {
+                            shownState.set(SuggestionShown.RARE);
+                            calculateEligible(lastPrefix, lastAllowSimilar, false);
+                            updateVisual(lastPrefix);
+                        }
+                        break;
+                    }
+                    // Otherwise, fall through:
                 default:
                     int selected = getHighlighted();
                     if (selected == -1 && eligibleCount() == 1)