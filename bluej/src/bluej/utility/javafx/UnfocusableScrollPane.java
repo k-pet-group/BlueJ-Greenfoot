7dc71aa115800b4e064451edaded529668f32f5d
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2017-12-12 15:01:39 +0000

    Added some missing copyright comments and Javadoc comments.

--- bluej/src/bluej/utility/javafx/UnfocusableScrollPane.java
+++ bluej/src/bluej/utility/javafx/UnfocusableScrollPane.java
@@ -1,3 +1,24 @@
+/*
+ This file is part of the BlueJ program.
+ Copyright (C) 2017 Michael Kölling and John Rosenberg
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+ This file is subject to the Classpath exception as provided in the
+ LICENSE.txt file that accompanied this code.
+ */
 package bluej.utility.javafx;
 
 import javafx.scene.Node;
@@ -9,6 +30,9 @@ import javafx.scene.control.ScrollPane;
  */
 public class UnfocusableScrollPane extends ScrollPane
 {
+    /**
+     * Creates the scroll pane with the given content.
+     */
     public UnfocusableScrollPane(Node content)
     {
         super(content);
