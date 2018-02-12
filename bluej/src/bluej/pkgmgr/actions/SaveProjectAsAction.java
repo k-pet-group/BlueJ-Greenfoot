7f449dfaee078820f586c34c2be7359f73d15855
Davin McCall <davmac@bluej.org>
2018-02-12 12:58:11 +0000

    Factor out some save-as functionality, for use in Greenfoot.

--- bluej/src/bluej/pkgmgr/actions/SaveProjectAsAction.java
+++ bluej/src/bluej/pkgmgr/actions/SaveProjectAsAction.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the BlueJ program. 
- Copyright (C) 1999-2009,2012,2014,2016  Michael Kolling and John Rosenberg 
+ Copyright (C) 1999-2009,2012,2014,2016,2018  Michael Kolling and John Rosenberg 
  
  This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License 
@@ -21,16 +21,14 @@
  */
 package bluej.pkgmgr.actions;
 
-import javax.swing.*;
 import java.io.File;
-import java.nio.file.Path;
-
-import javafx.application.Platform;
+import java.io.IOException;
 
 import bluej.Config;
 import bluej.pkgmgr.Package;
 import bluej.pkgmgr.PkgMgrFrame;
 import bluej.pkgmgr.Project;
+import bluej.pkgmgr.ProjectUtils;
 import bluej.utility.Debug;
 import bluej.utility.DialogManager;
 import bluej.utility.FileUtility;
@@ -59,56 +57,39 @@ final public class SaveProjectAsAction extends PkgMgrAction
         // get a file name to save under
         File newName = FileUtility.getSaveProjectFX(frame.getFXWindow(), Config.getString("pkgmgr.saveAs.title"));
 
-        if (newName != null)
+        if (newName == null)
         {
-            Path newPath = newName.toPath().toAbsolutePath();
-            Path existingPath = project.getProjectDir().toPath().toAbsolutePath();
-            // We need to block the case where new project is subdirectory of old path, as that will cause
-            // infinite copy.  It's odd but fine if new project is parent of old path, as it won't cause
-            // new files in old path, so copy will complete.
-            if (newPath.startsWith(existingPath))
-            {
-                DialogManager.showErrorFX(frame.getFXWindow(), "cannot-save-inside-self");
-                return;
-            }
-
+            return;
+        }
+        
+        try {
             project.saveAll();
+            project.saveAllEditors();
+        }
+        catch (IOException ioe)
+        {
+            DialogManager.showErrorFX(frame.getFXWindow(), "cannot-save-project");
+            return;
+        }
+        
+        if (! ProjectUtils.saveProjectCopy(project, newName, frame.getFXWindow()))
+        {
+            return;
+        }
+        
+        // Copy successful: close the current project and open the new one
+        PkgMgrFrame.closeProject(project);
+        Project openProj = Project.openProject(newName.getAbsolutePath());
 
-            int result = FileUtility.copyDirectory(project.getProjectDir(),
-                    newName);
-
-            switch (result)
-            {
-                case FileUtility.NO_ERROR:
-                    break;
-
-                case FileUtility.DEST_EXISTS_NOT_DIR:
-                    DialogManager.showErrorFX(frame.getFXWindow(), "directory-exists-file");
-                    return;
-                case FileUtility.DEST_EXISTS_NON_EMPTY:
-                    DialogManager.showErrorFX(frame.getFXWindow(), "directory-exists-non-empty");
-                    return;
-
-                case FileUtility.SRC_NOT_DIRECTORY:
-                case FileUtility.COPY_ERROR:
-                    DialogManager.showErrorFX(frame.getFXWindow(), "cannot-save-project");
-                    return;
-            }
-
-            PkgMgrFrame.closeProject(project);
-
-            // open new project
-            Project openProj = Project.openProject(newName.getAbsolutePath());
-
-            if (openProj != null)
-            {
-                Package pkg = openProj.getPackage("");
-                PkgMgrFrame pmf = PkgMgrFrame.createFrame(pkg, null);
-                pmf.setVisible(true);
-            } else
-            {
-                Debug.message("Save as: could not open package under new name");
-            }
+        if (openProj != null)
+        {
+            Package pkg = openProj.getPackage("");
+            PkgMgrFrame pmf = PkgMgrFrame.createFrame(pkg, null);
+            pmf.setVisible(true);
+        }
+        else
+        {
+            Debug.message("Save as: could not open package under new name");
         }
     }
 
