104dcc1c650034d499e8ac423e264896a8be6a64
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2018-05-09 15:21:56 +0100

    Fixed the Application.start method to be tagged as FXPlatform by default (since it's always run by the FX runtime on the platform thread).

--- bluej/threadchecker/pluginsrc/threadchecker/TCScanner.java
+++ bluej/threadchecker/pluginsrc/threadchecker/TCScanner.java
@@ -203,6 +203,7 @@ class TCScanner extends TreePathScanner<Void, Void>
         methodAnns.add(new MethodRef("javafx.embed.swing.SwingNode", "getContent", new LocatedTag(Tag.Any, false, false, "<SwingNode>")));
 
         methodAnns.add(new MethodRef("javafx.application.Application", "launch", new LocatedTag(Tag.Any, false, false, "<FX launch>")));
+        methodAnns.add(new MethodRef("javafx.application.Application", "start", new LocatedTag(Tag.FXPlatform, false, false, "<FX application>")));
         methodAnns.add(new MethodRef("javafx.animation.AnimationTimer", "handle", new LocatedTag(Tag.FXPlatform, false, false, "<AnimationTimer>")));
         
         // This one isn't actually true!  But it's used during printing so let's live:
