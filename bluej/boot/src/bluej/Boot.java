cbfa10a1420a8ccdc06787401a931ea747fee2aa
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2018-08-31 11:52:33 +0100

    Updated the BlueJ version number to 4.1.3 ahead of building rc1

--- bluej/boot/src/bluej/Boot.java
+++ bluej/boot/src/bluej/Boot.java
@@ -56,7 +56,7 @@ public class Boot
     // and then the update-version target should be executed.
     public static final int BLUEJ_VERSION_MAJOR = 4;
     public static final int BLUEJ_VERSION_MINOR = 1;
-    public static final int BLUEJ_VERSION_RELEASE = 2;
+    public static final int BLUEJ_VERSION_RELEASE = 3;
     public static final String BLUEJ_VERSION_SUFFIX = "";
 
     // public static final int BLUEJ_VERSION_NUMBER = BLUEJ_VERSION_MAJOR * 1000 +
