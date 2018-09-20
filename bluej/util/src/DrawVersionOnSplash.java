2da1a686b1ddaef00a9bbe1cefe2efd901eaf967
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2018-09-20 15:55:35 +0100

    Changed DrawVersionOnSplash to also take the font file to use as an argument, and switched to using the SourceSansPro font (which we already have in the repository) for drawing the version number.

--- bluej/util/src/DrawVersionOnSplash.java
+++ bluej/util/src/DrawVersionOnSplash.java
@@ -1,6 +1,7 @@
 import javax.imageio.ImageIO;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
@@ -13,34 +14,42 @@ import java.io.IOException;
  */
 public class DrawVersionOnSplash
 {
-    // Takes five args: original image path, string to draw, size, X position, Y position, output image path
+    // Takes seven args: original image path, string to draw, font file, size, X position, Y position, output image path
     public static void main(String[] args)
     {
-        if (args.length != 6)
+        if (args.length != 7)
         {
             System.err.println("Wrong number of args (" + args.length + "), should be 6: original-image-path, string, size, X, Y, output-image-path");
             System.exit(-1);
         }
         try
         {
-            BufferedImage image = ImageIO.read(new File(args[0]));
+            File originalImage = new File(args[0]);
+            String stringToDraw = args[1];
+            File fontFile = new File(args[2]);
+            int fontSize = Integer.parseInt(args[3]);            
+            int xPosition = Integer.parseInt(args[4]);
+            int yPosition = Integer.parseInt(args[5]);
+            File destFile = new File(args[6]);
+            
+            BufferedImage image = ImageIO.read(originalImage);
             Graphics g = image.getGraphics();
             g.drawImage(image, 0, 0, null);
             g.setColor(new Color(255,255,255));
-            g.setFont(new Font("SansSerif", Font.BOLD, Integer.parseInt(args[2])));
+            g.setFont(Font.createFont(Font.TRUETYPE_FONT, fontFile).deriveFont((float)fontSize));
             if (g instanceof Graphics2D) {
                 Graphics2D g2d = (Graphics2D)g;
                 RenderingHints hints = new RenderingHints(RenderingHints.KEY_TEXT_ANTIALIASING,
                     RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
                 g2d.addRenderingHints(hints);
             }
-            g.drawString(args[1], Integer.parseInt(args[3]), Integer.parseInt(args[4]));
-            ImageIO.write(image, "png", new File(args[5]));
+            g.drawString(stringToDraw, xPosition, yPosition);
+            ImageIO.write(image, "png", destFile);
         }
-        catch (IOException | NumberFormatException e)
+        catch (IOException | NumberFormatException | FontFormatException e)
         {
             System.err.println("Exception: " + e.getLocalizedMessage());
-            System.exit(-1);
+            System.exit(-2);
         }
     }
 }
