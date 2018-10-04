e29eb9cabc8dc15bca564d6e4ecf895d88d1b7a2
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2018-10-04 15:21:51 +0100

    Added the JavaFX JARs to the starting class path of BlueJ, which is needed as Boot uses JavaFX.

--- bluej/package/winlaunch/bjlaunch.cc
+++ bluej/package/winlaunch/bjlaunch.cc
@@ -273,6 +273,15 @@ bool launchVMexternal(string jdkLocation)
 
     commandLine += TEXT("-classpath ");
     string classPathString = bluejPath + TEXT("\\lib\\bluej.jar");
+    string sep(TEXT(";"));
+    classPathString += sep + bluejPath + TEXT("\\lib\\javafx\\lib\\javafx.base.jar");
+    classPathString += sep + bluejPath + TEXT("\\lib\\javafx\\lib\\javafx.controls.jar");
+    classPathString += sep + bluejPath + TEXT("\\lib\\javafx\\lib\\javafx.fxml.jar");
+    classPathString += sep + bluejPath + TEXT("\\lib\\javafx\\lib\\javafx.graphics.jar");
+    classPathString += sep + bluejPath + TEXT("\\lib\\javafx\\lib\\javafx.media.jar");
+    classPathString += sep + bluejPath + TEXT("\\lib\\javafx\\lib\\javafx.properties.jar");
+    classPathString += sep + bluejPath + TEXT("\\lib\\javafx\\lib\\javafx.swing.jar");
+    classPathString += sep + bluejPath + TEXT("\\lib\\javafx\\lib\\javafx.web.jar");
     commandLine += escapeCmdlineParam(classPathString);
 
     commandLine += TEXT(" bluej.Boot");
@@ -373,6 +382,15 @@ bool launchVM(string jdkLocation)
 
     std::string classPathOpt = "-Djava.class.path=";
     (classPathOpt += bjDirACP) += "\\lib\\bluej.jar";
+    const char* sep = ";";
+    ((classPathOpt += sep) += bjDirACP) += "\\lib\\javafx\\lib\\javafx.base.jar";
+    ((classPathOpt += sep) += bjDirACP) += "\\lib\\javafx\\lib\\javafx.controls.jar";
+    ((classPathOpt += sep) += bjDirACP) += "\\lib\\javafx\\lib\\javafx.fxml.jar";
+    ((classPathOpt += sep) += bjDirACP) += "\\lib\\javafx\\lib\\javafx.graphics.jar";
+    ((classPathOpt += sep) += bjDirACP) += "\\lib\\javafx\\lib\\javafx.media.jar";
+    ((classPathOpt += sep) += bjDirACP) += "\\lib\\javafx\\lib\\javafx.properties.jar";
+    ((classPathOpt += sep) += bjDirACP) += "\\lib\\javafx\\lib\\javafx.swing.jar";
+    ((classPathOpt += sep) += bjDirACP) += "\\lib\\javafx\\lib\\javafx.web.jar";
 
     delete [] bjDirACP;
     delete [] jdkLocACP;
