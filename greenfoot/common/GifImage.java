023c3fe949bc4c34eafb7949910de8ade274b9fe
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2018-12-14 14:54:08 +0000

    Fix a bug in GifImage where it wasn't overwriting the previous image region correctly.
    As described on the Greenroom:  The bug was that the colour with which to overwrite the rectangle may be totally transparent. If you ask to fill a rectangle with transparency, it actually blends it over the top, thus not clearing the previous image. The previous GifImage used a different alpha blending mode to avoid this issue, but since we don’t expose that in GreenfootImage, I’ve instead used a loop that directly writes the colour value to each pixel.

--- greenfoot/common/GifImage.java
+++ greenfoot/common/GifImage.java
@@ -363,8 +363,13 @@ public class GifImage
                         } else {
                             c = lastBgColor; // use given background color
                         }
-                        image.setColor(c);
-                        image.fillRect(lastRect.x, lastRect.y, lastRect.width, lastRect.height);
+                        for (int x = 0; x < lastRect.width; x++)
+                        {
+                            for (int y = 0; y < lastRect.height; y++)
+                            {
+                                image.setColorAt(lastRect.x + x, lastRect.y + y, c);
+                            }
+                        }
                     }
                 }
             }
