611e0652a8e1d0b7c95096c6821b599e2057006c
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2019-05-28 15:46:10 +0100

    Fix copyright years.

--- bluej/src/bluej/pkgmgr/actions/PkgMgrAction.java
+++ bluej/src/bluej/pkgmgr/actions/PkgMgrAction.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the BlueJ program. 
- Copyright (C) 1999-2009,2016,2017  Michael Kolling and John Rosenberg 
+ Copyright (C) 1999-2009,2016,2017,2019  Michael Kolling and John Rosenberg 
  
  This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License 
