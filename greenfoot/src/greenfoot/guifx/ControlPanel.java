b7dada089b69a83c4323b48720f0719b12118050
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2019-06-07 15:49:12 +0100

    Tweaked the Greenfoot control panel to show the reset button when there is no world.  Sometimes this will not do anything, but it may also be useful to clear up a state in Greenfoot when there is no world and the user is not sure what to do.

--- greenfoot/src/greenfoot/guifx/ControlPanel.java
+++ greenfoot/src/greenfoot/guifx/ControlPanel.java
@@ -205,7 +205,7 @@ public class ControlPanel extends GridPane
         actDisabled.setValue(newState != State.PAUSED || atBreakpoint);
         runDisabled.setValue(newState != State.PAUSED || atBreakpoint);
         pauseDisabled.setValue(newState != State.RUNNING || atBreakpoint);
-        resetDisabled.setValue(newState == State.NO_PROJECT || newState == State.NO_WORLD);
+        resetDisabled.setValue(newState == State.NO_PROJECT);
         speedSlider.setDisable(newState == State.NO_PROJECT);
         speedLabel.setDisable(newState == State.NO_PROJECT);
 
