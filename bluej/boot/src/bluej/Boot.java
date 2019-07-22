e5cc75d0891bd20e3ecdcb582e8a46afdb59e993
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2019-07-22 15:17:08 +0100

    Bumped the version number ready to build an RC

--- bluej/boot/src/bluej/Boot.java
+++ bluej/boot/src/bluej/Boot.java
@@ -57,9 +57,9 @@ public class Boot
     // The version numbers for BlueJ are changed in the BlueJ build.xml
     // and then the update-version target should be executed.
     public static final int BLUEJ_VERSION_MAJOR = 4;
-    public static final int BLUEJ_VERSION_MINOR = 2;
-    public static final int BLUEJ_VERSION_RELEASE = 1;
-    public static final String BLUEJ_VERSION_SUFFIX = "";
+    public static final int BLUEJ_VERSION_MINOR = 5;
+    public static final int BLUEJ_VERSION_RELEASE = 0;
+    public static final String BLUEJ_VERSION_SUFFIX = "internal";
 
     // public static final int BLUEJ_VERSION_NUMBER = BLUEJ_VERSION_MAJOR * 1000 +
     //                                                BLUEJ_VERSION_MINOR * 100 +
