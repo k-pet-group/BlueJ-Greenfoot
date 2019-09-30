5fcb4268a64b0221098d2f3f74ae2645335302d0
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2019-09-30 12:59:29 +0100

    Update copyright years.

--- greenfoot/src/greenfoot/core/Simulation.java
+++ greenfoot/src/greenfoot/core/Simulation.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the Greenfoot program. 
- Copyright (C) 2005-2010,2011,2012,2013,2014,2016  Poul Henriksen and Michael Kolling 
+ Copyright (C) 2005-2010,2011,2012,2013,2014,2016,2019  Poul Henriksen and Michael Kolling 
  
  This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License 
