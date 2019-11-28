97f4e06094a1ba5971089848955ad2c24ce06e4e
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2019-11-28 16:01:52 +0000

    Fix copyright years

--- greenfoot/src/greenfoot/guifx/classes/GClassDiagram.java
+++ greenfoot/src/greenfoot/guifx/classes/GClassDiagram.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the Greenfoot program. 
- Copyright (C) 2017,2018  Poul Henriksen and Michael Kolling 
+ Copyright (C) 2017,2018,2019  Poul Henriksen and Michael Kolling 
  
  This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License 
