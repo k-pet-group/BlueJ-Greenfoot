21d2d93721aa61784dc524dd52881a49edd41c39
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2020-05-19 15:24:03 +0100

    Updated copyright years, and added a script for updating copyright years.

--- bluej/src/bluej/stride/operations/ToggleBooleanProperty.java
+++ bluej/src/bluej/stride/operations/ToggleBooleanProperty.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the BlueJ program.
- Copyright (C) 2016 Michael Kölling and John Rosenberg
+ Copyright (C) 2016,2020 Michael Kölling and John Rosenberg
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
