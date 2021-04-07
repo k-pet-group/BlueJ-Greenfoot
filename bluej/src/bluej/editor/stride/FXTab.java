1d2644db984c73b85661f6cc7e0475acc46873b5
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2021-04-07 16:45:47 +0100

    Fix copyright year.

--- bluej/src/bluej/editor/stride/FXTab.java
+++ bluej/src/bluej/editor/stride/FXTab.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the BlueJ program.
- Copyright (C) 2015,2016,2018,2020  Michael Kolling and John Rosenberg
+ Copyright (C) 2015,2016,2018,2020,2021  Michael Kolling and John Rosenberg
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
