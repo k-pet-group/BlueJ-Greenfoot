3ab57186a4dc68d03f7f18ba448afed2e7d34f31
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2021-04-19 12:17:44 +0100

    Fix copyright years

--- bluej/src/bluej/debugger/jdi/JdiDebugger.java
+++ bluej/src/bluej/debugger/jdi/JdiDebugger.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the BlueJ program. 
- Copyright (C) 1999-2009,2010,2011,2012,2014,2016,2017,2018,2019,2020  Michael Kolling and John Rosenberg
+ Copyright (C) 1999-2009,2010,2011,2012,2014,2016,2017,2018,2019,2020,2021  Michael Kolling and John Rosenberg
  
  This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License 
