519b40c7b041126c720354a3f8f11f16464357d5
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2021-05-17 17:21:25 +0100

    Fixed an exception which could occur when trying to calculate the bounds of a break overlay when loading (and thus off-thread).

--- bluej/src/bluej/stride/framedjava/frames/BreakFrame.java
+++ bluej/src/bluej/stride/framedjava/frames/BreakFrame.java
@@ -26,6 +26,7 @@
 package bluej.stride.framedjava.frames;
 
 
+import javafx.application.Platform;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
@@ -137,6 +138,9 @@ public class BreakFrame extends SingleLineFrame
     private void adjustOverlayBounds()
     {
         if (outer == null || rectangle == null || overlay == null)
+            return;
+        // Don't attempt to look up bounds while loading off-thread:
+        if (!Platform.isFxApplicationThread())
             return;
 
         final double ourX = getNode().localToScene(0, 0).getX();
