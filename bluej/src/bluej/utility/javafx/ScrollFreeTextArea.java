ece56181de5eca52220b9759976ac745ddd4df43
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2021-06-23 14:20:55 +0100

    Update copyright years.

--- bluej/src/bluej/utility/javafx/ScrollFreeTextArea.java
+++ bluej/src/bluej/utility/javafx/ScrollFreeTextArea.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the BlueJ program. 
- Copyright (C) 2015,2016 Michael Kölling and John Rosenberg 
+ Copyright (C) 2015,2016,2021 Michael Kölling and John Rosenberg 
  
  This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License 
