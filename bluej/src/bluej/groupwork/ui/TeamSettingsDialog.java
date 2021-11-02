2e049259dca25dab1e6a73eed9eb506b575bbc7d
Pierre Weill-Tessier <pierre.weill-tessier@kcl.ac.uk>
2021-11-02 17:11:47 +0000

    Fix BlueJ-1359 : (bug 3) option for saving the password (encrypted) on the computer for a project

--- bluej/src/bluej/groupwork/ui/TeamSettingsDialog.java
+++ bluej/src/bluej/groupwork/ui/TeamSettingsDialog.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the BlueJ program.
- Copyright (C) 1999-2009,2016,2017,2018,2019,2020  Michael Kolling and John Rosenberg
+ Copyright (C) 1999-2009,2016,2017,2018,2019,2020,2021  Michael Kolling and John Rosenberg
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
@@ -91,7 +91,7 @@ public class TeamSettingsDialog extends FXCustomizedDialog<TeamSettings>
         okButton = (Button) getDialogPane().lookupButton(ButtonType.OK);
         okButton.setOnAction(event -> {
             TeamSettings settings = teamSettingsPanel.getSettings();
-            teamSettingsController.updateSettings(settings, teamSettingsPanel.getUseAsDefault());
+            teamSettingsController.updateSettings(settings);
             if (teamSettingsController.hasProject()) {
                 teamSettingsController.writeToProject();
             }
