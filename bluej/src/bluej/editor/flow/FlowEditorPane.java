404b54c124386fe2f72ac846df6ec969370c3a3b
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2022-02-08 11:28:40 +0000

    Fix copyright year.

--- bluej/src/bluej/editor/flow/FlowEditorPane.java
+++ bluej/src/bluej/editor/flow/FlowEditorPane.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the BlueJ program. 
- Copyright (C) 2019,2020,2021  Michael Kolling and John Rosenberg
+ Copyright (C) 2019,2020,2021,2022  Michael Kolling and John Rosenberg
 
  This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License 
