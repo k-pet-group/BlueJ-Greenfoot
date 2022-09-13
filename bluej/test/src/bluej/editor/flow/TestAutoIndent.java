25a35be4e488c85475de26154212afd4cc6df212
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2022-09-13 13:23:15 +0100

    Added another test for comments, multiline strings and auto-indentation.

--- bluej/test/src/bluej/editor/flow/TestAutoIndent.java
+++ bluej/test/src/bluej/editor/flow/TestAutoIndent.java
@@ -1,24 +1,25 @@
 /*
- This file is part of the BlueJ program. 
- Copyright (C) 2022  Michael Kolling and John Rosenberg 
-
- This program is free software; you can redistribute it and/or 
- modify it under the terms of the GNU General Public License 
- as published by the Free Software Foundation; either version 2 
- of the License, or (at your option) any later version. 
-
- This program is distributed in the hope that it will be useful, 
- but WITHOUT ANY WARRANTY; without even the implied warranty of 
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- GNU General Public License for more details. 
-
- You should have received a copy of the GNU General Public License 
- along with this program; if not, write to the Free Software 
- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. 
-
- This file is subject to the Classpath exception as provided in the  
- LICENSE.txt file that accompanied this code.
+ * This file is part of the BlueJ program. 
+ * Copyright (C) 2022  Michael Kolling and John Rosenberg 
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation; either version 2 
+ * of the License, or (at your option) any later version. 
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. 
+ * 
+ * This file is subject to the Classpath exception as provided in the
+ * LICENSE.txt file that accompanied this code.
  */
+// Important we use stars above because this file is used as a test input for the auto indent
 package bluej.editor.flow;
 
 import bluej.parser.InitConfig;
@@ -83,6 +84,38 @@ public class TestAutoIndent
                               }
                               }
                 """;
+        runTest(correct, incorrect);
+        // Check correct is not modified:
+        runTest(correct, correct);
+    }
+
+    @Test
+    public void testCommentedString()
+    {
+        String correct = """
+                class Foo
+                {
+                    // \"\"\"
+                    int x = 6;
+                    /* \"\"\" */
+                    int y = 7;
+                    /*
+                    \"\"\" */
+                    int z = 8;
+                }   
+                """;
+        String incorrect = """
+            class Foo
+            {
+                // \"\"\"
+                    int x = 6;
+            /* \"\"\" */
+            int y = 7;
+                /*
+                \"\"\" */
+                        int z = 8;
+                }
+            """;
         runTest(correct, incorrect);
         // Check correct is not modified:
         runTest(correct, correct);
