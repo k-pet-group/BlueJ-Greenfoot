29d33e54382ef5c105e888f685a8b55640c0f371
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2023-08-23 16:29:08 +0100

    Increased the Greenfoot version number ready for the new build.

--- boot/src/main/java/bluej/Boot.java
+++ boot/src/main/java/bluej/Boot.java
@@ -60,7 +60,7 @@ public class Boot
     // version.properties file and then the :boot:updateVersionNumber task should be
     // executed to change them here and elsewhere where needed.
     public static final String BLUEJ_VERSION = "5.2.0";
-    public static final String GREENFOOT_VERSION = "3.7.1";
+    public static final String GREENFOOT_VERSION = "3.8.0";
     public static final String GREENFOOT_API_VERSION = "3.0.0";
 
     public static final String BLUEJ_VERSION_TITLE = "BlueJ " + BLUEJ_VERSION;
--- greenfoot/doc/Greenfoot-README.txt
+++ greenfoot/doc/Greenfoot-README.txt
@@ -1,4 +1,4 @@
-Greenfoot version: 3.7.1
+Greenfoot version: 3.8.0
 
 Information about Greenfoot
 -----------------------
