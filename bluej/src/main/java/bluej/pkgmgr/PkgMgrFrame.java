cdf27f9ac43e01528533c695c723d0200daeed3b
Neil Brown <neil.c.c.brown@kcl.ac.uk>
2024-01-11 12:12:45 +0000

    Moved Babis into the previous team members.

--- bluej/src/main/java/bluej/pkgmgr/PkgMgrFrame.java
+++ bluej/src/main/java/bluej/pkgmgr/PkgMgrFrame.java
@@ -1,6 +1,6 @@
 /*
  This file is part of the BlueJ program. 
- Copyright (C) 1999-2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023  Michael Kolling and John Rosenberg
+ Copyright (C) 1999-2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023,2024  Michael Kolling and John Rosenberg
  
  This program is free software; you can redistribute it and/or 
  modify it under the terms of the GNU General Public License 
@@ -1977,6 +1977,7 @@ public class PkgMgrFrame
                 "Hamza Hamza",
                 "Fabio Hedayioglu",
                 "Poul Henriksen",
+                "Charalampos Kyfonidis",
                 "Davin McCall",
                 "Clive Miller",
                 "Andrew Patterson",
