3ff8f8d3e58398fe23bfb20c3e41924b4705a644
Neil Brown <github@twistedsquare.com>
2024-02-08 17:15:29 +0000

    Fixed a bug where the backgrounds were not immediately fully cleared in the terminal when the terminal was cleared.

--- bluej/src/main/java/bluej/terminal/TerminalTextPane.java
+++ bluej/src/main/java/bluej/terminal/TerminalTextPane.java
@@ -603,6 +603,7 @@ public abstract class TerminalTextPane extends BaseEditorPane
         anchorPos = new Pos(0, 0, 0);
         setContent(Collections.singletonList(new ContentLine(new ArrayList<>())));
         currentSections.clear();
+        lineDisplay.applyScopeBackgrounds(Map.of());
     }
 
     /**
